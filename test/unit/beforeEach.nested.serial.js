7fb4ca91d8a02f87be70f376782639a6ed34decf
Tj Holowaychuk <tj@vision-media.ca>
2011-09-13 14:06:39 -0700

    nest describe()

--- test/unit/beforeEach.nested.serial.js
+++ test/unit/beforeEach.nested.serial.js
@@ -1,66 +1,68 @@
 
 describe('serial', function(){
-  var calls = [];
+  describe('nested', function(){
+    var calls = [];
 
-  beforeEach(function(test){
-    calls.push('parent before ' + test.title);
-  });
-
-  afterEach(function(test){
-    calls.push('parent after ' + test.title);
-  });
-
-  it('foo', function(){
-    calls.should.eql([
-        'before one'
-      , 'after one'
-      , 'before two'
-      , 'after two'
-      , 'before three'
-      , 'after three'
-      , 'parent before foo']);
-  });
-
-  it('bar', function(){
-    calls.should.eql([
-        'before one'
-      , 'after one'
-      , 'before two'
-      , 'after two'
-      , 'before three'
-      , 'after three'
-      , 'parent before foo'
-      , 'parent after foo'
-      , 'parent before bar']);
-  });
-
-  describe('beforeEach()', function(){
     beforeEach(function(test){
-      calls.push('before ' + test.title);
+      calls.push('parent before ' + test.title);
     });
 
-    it('one', function(){
-      calls.should.eql(['before one']);
+    afterEach(function(test){
+      calls.push('parent after ' + test.title);
     });
 
-    it('two', function(){
-      calls.should.eql([
-          'before one'
-        , 'after one'
-        , 'before two']);
-    });
-
-    it('three', function(){
+    it('foo', function(){
       calls.should.eql([
           'before one'
         , 'after one'
         , 'before two'
         , 'after two'
-        , 'before three']);
+        , 'before three'
+        , 'after three'
+        , 'parent before foo']);
     });
 
-    afterEach(function(test){
-      calls.push('after ' + test.title);
+    it('bar', function(){
+      calls.should.eql([
+          'before one'
+        , 'after one'
+        , 'before two'
+        , 'after two'
+        , 'before three'
+        , 'after three'
+        , 'parent before foo'
+        , 'parent after foo'
+        , 'parent before bar']);
+    });
+
+    describe('beforeEach()', function(){
+      beforeEach(function(test){
+        calls.push('before ' + test.title);
+      });
+
+      it('one', function(){
+        calls.should.eql(['before one']);
+      });
+
+      it('two', function(){
+        calls.should.eql([
+            'before one'
+          , 'after one'
+          , 'before two']);
+      });
+
+      it('three', function(){
+        calls.should.eql([
+            'before one'
+          , 'after one'
+          , 'before two'
+          , 'after two'
+          , 'before three']);
+      });
+
+      afterEach(function(test){
+        calls.push('after ' + test.title);
+      });
     });
   });
 });
