01dfc96301a5892d2b8f6c4ac220bea7f77bbc83
Tj Holowaychuk <tj@vision-media.ca>
2011-09-22 00:18:33 -0700

    emit end on uncaught

--- test/unit/uncaught.js
+++ test/unit/uncaught.js
@@ -1,6 +1,6 @@
 
 describe('uncaught', function(){
-  it('should report properly', function(test, done){
+  it('should report properly', function(done){
     // if you uncomment this :)
     process.nextTick(function(){
       throw new Error("I'm uncaught!");
