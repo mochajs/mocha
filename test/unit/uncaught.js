3b4bf098e1de12c4c1ba1e198cb40d63d78d2fa7
Tj Holowaychuk <tj@vision-media.ca>
2011-09-22 00:56:51 -0700

    Added quick hook error reporting

--- test/unit/uncaught.js
+++ test/unit/uncaught.js
@@ -1,9 +1,5 @@
 
 describe('uncaught', function(){
-  before(function(){
-    throw new Error('test')
-  })
-
   it('should report properly', function(done){
     process.nextTick(function(){
       // if you uncomment this :)
