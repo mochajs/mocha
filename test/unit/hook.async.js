0078383061861a82ba4f619eb356f2e5cf3961cd
Tj Holowaychuk <tj@vision-media.ca>
2011-09-22 00:40:04 -0700

    Fixed async hook arity check

--- test/unit/hook.async.js
+++ test/unit/hook.async.js
@@ -1,73 +1,82 @@
 
 describe('async', function(){
   var calls = [];
-
+  
   process.on('exit', function(){
     calls.should.eql([
         'before all'
-      , 'before one'
-      , 'after one'
-      , 'before two'
-      , 'after two'
-      , 'before three'
-      , 'after three'
+      , 'before'
+      , 'one'
+      , 'after'
+      , 'before'
+      , 'two'
+      , 'after'
+      , 'before'
+      , 'three'
+      , 'after'
       , 'after all']);
   })
-
+  
   beforeEach(function(){
     // not hit
     calls.push('parent before');
   })
-
+  
   afterEach(function(){
     // not hit
     calls.push('parent after' );
   })
-
+  
   describe('beforeEach()', function(){
     before(function(){
       calls.push('before all');
     });
-
+  
     after(function(){
       calls.push('after all');
     });
-
+  
     beforeEach(function(done){
       process.nextTick(function(){
-        calls.push('before ' + test.title);
+        calls.push('before');
         done();
       })
     })
-
+  
     it('one', function(done){
       calls.should.eql([
           'before all'
-        , 'before one']);
+        , 'before']);
+      calls.push('one');
       process.nextTick(done);
     })
     
     it('two', function(){
       calls.should.eql([
           'before all'
-        , 'before one'
-        , 'after one'
-        , 'before two']);
+        , 'before'
+        , 'one'
+        , 'after'
+        , 'before']);
+      calls.push('two');
     })
     
     it('three', function(){
       calls.should.eql([
           'before all'
-        , 'before one'
-        , 'after one'
-        , 'before two'
-        , 'after two'
-        , 'before three']);
+        , 'before'
+        , 'one'
+        , 'after'
+        , 'before'
+        , 'two'
+        , 'after'
+        , 'before']);
+      calls.push('three');
     })
-
+  
     afterEach(function(done){
       process.nextTick(function(){
-        calls.push('after ' + test.title);
+        calls.push('after');
         done();
       })
     })
