1b983e2b13bda1834db76b5deeab60bc88546aa4
Tj Holowaychuk <tj@vision-media.ca>
2011-10-03 21:41:31 -0700

    Added global var leak reporting. Closes #12
    mocha will tell you with test introduced the leak.
    we will want whitelisting support here as well

---
+++ test/unit/globals.js
@@ -0,0 +1,9 @@
+
+describe('global leaks', function(){
+  it('should cause tests to fail', function(){
+    // uncomment to test
+    // foo = 'bar';
+    // bar = 'baz';
+    // baz = 'raz';
+  });
+});
