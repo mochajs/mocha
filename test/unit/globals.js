c61f4d6ae42f7d3e4d764d9699444bba7de5d5e0
Tj Holowaychuk <tj@vision-media.ca>
2011-10-03 22:14:14 -0700

    misc

--- test/unit/globals.js
+++ test/unit/globals.js
@@ -8,7 +8,7 @@ describe('global leaks', function(){
   it('should cause tests to fail', function(){
     // uncomment to test
     // foo = 'bar';
-    bar = 'baz';
+    // bar = 'baz';
     // baz = 'raz';
   });
 
