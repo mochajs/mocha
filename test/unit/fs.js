b8a02a4f836cdb9e420f5252f6a7aca16e9f04bb
Tj Holowaychuk <tj@vision-media.ca>
2011-11-02 18:19:41 -0700

    Added quick fs example
    showing that you can use the done() callback as a regular
    callback with an error

---
+++ test/unit/fs.js
@@ -0,0 +1,20 @@
+
+var fs = require('fs');
+
+describe('fs', function(){
+  describe('.readFile()', function(){
+    describe('when the file exists', function(){
+      it('should succeed', function(done){
+        fs.writeFile('/tmp/mocha', 'wahoo', done)
+      })
+    })
+
+    describe('when the file does not exist', function(){
+      it('should fail', function(done){
+        // uncomment
+        // fs.readFile('/tmp/does-not-exist', done);
+        done();
+      })
+    })
+  })
+})
