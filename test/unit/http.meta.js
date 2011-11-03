e5e63e77d3ad35cc6a7ecfbfcbf51b6fccc2cc7f
Tj Holowaychuk <tj@vision-media.ca>
2011-11-02 19:54:38 -0700

    moar meta example

--- test/unit/http.meta.js
+++ test/unit/http.meta.js
@@ -2,21 +2,28 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res){
+  var accept = req.headers.accept || ''
+    , json = ~accept.indexOf('json');
+
   switch (req.url) {
     case '/':
       res.end('hello');
       break;
     case '/users':
-      res.end('tobi, loki, jane');
+      if (json) {
+        res.end('["tobi","loki","jane"]');
+      } else {
+        res.end('tobi, loki, jane');
+      }
       break;
   }
 })
 
 server.listen(8889);
 
-function get(url, body) {
+function get(url, body, header) {
   return function(done){
-    http.get({ path: url, port: 8889 }, function(res){
+    http.get({ path: url, port: 8889, headers: header }, function(res){
       var buf = '';
       res.statusCode.should.equal(200);
       res.setEncoding('utf8');
@@ -31,10 +38,15 @@ function get(url, body) {
 
 describe('http', function(){
   describe('GET /', function(){
-    it('should respond with hello', get('/', 'hello'))
+    it('should respond with hello',
+      get('/', 'hello'))
   })
 
   describe('GET /users', function(){
-    it('should respond with users', get('/users', 'tobi, loki, jane'))
+    it('should respond with users',
+      get('/users', 'tobi, loki, jane'))
+
+    it('should respond with users',
+      get('/users', '["tobi","loki","jane"]', { Accept: 'application/json' }))
   })
 })
