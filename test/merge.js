e47446112f2781bc6fbb1cd47e8bc6b21b5224f0
Tj Holowaychuk <tj@vision-media.ca>
2011-11-16 15:50:45 -0800

    Added suite merging. Closes #28
    the merge.js example there with --reporter spec
    will result in:
    
      merge
        stuff
          one
            ✓ should do something
          two
            ✓ should do stuff
          three
            ✓ should do something
          four
            ✓ should do something

---
+++ test/merge.js
@@ -0,0 +1,34 @@
+
+describe('merge', function(){
+  describe('stuff', function(){
+    describe('one', function(){
+      it('should do something', function(){
+        
+      })
+    })
+    
+    describe('two', function(){
+      it('should do stuff', function(){
+        
+      })
+    })
+  })
+})
+
+describe('merge', function(){
+  describe('stuff', function(){
+    describe('three', function(){
+      it('should do something', function(){
+        
+      })
+    })
+  })
+})
+
+describe('merge stuff', function(){
+  describe('four', function(){
+    it('should do something', function(){
+      
+    })
+  })
+})
