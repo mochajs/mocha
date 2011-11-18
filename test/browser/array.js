491c0c45c576e5979a294966577ef2b3729d9862
Tj Holowaychuk <tj@vision-media.ca>
2011-11-17 18:03:19 -0800

    array.js

---
+++ test/browser/array.js
@@ -0,0 +1,33 @@
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      var arr = [1,2,3];
+      assert(-1 == arr.indexOf(5));
+    })
+
+    it('should return the correct index when the value is present', function(){
+      var arr = [1,2,3];
+        assert(0 == arr.indexOf(1)); // just to test indentation
+      assert(1 == arr.indexOf(2));
+    })
+  })
+})
+
+describe('Array', function(){
+  describe('#pop()', function(){
+    it('should remove and return the last value', function(){
+      var arr = [1,2,3];
+      assert(arr.pop() == 3);
+    })
+  })
+})
+
+describe('Array', function(){
+  describe('#pop()', function(){
+    it('should adjust .length', function(){
+      var arr = [1,2,3];
+      arr.pop();
+      assert(arr.length == 2);
+    })
+  })
+})
