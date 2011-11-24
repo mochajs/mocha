667379c39f5c086f67aa4fec2fee45a4c308e0a9
TJ Holowaychuk <tj@vision-media.ca>
2011-11-24 05:51:19 -0800

    Fixed hook timeouts

--- test/timeout.js
+++ test/timeout.js
@@ -1,8 +1,14 @@
 
 describe('timeouts', function(){
+  beforeEach(function(done){
+    // uncomment
+    // setTimeout(done, 3000);
+    done();
+  })
+
   it('should error on timeout', function(done){
     // uncomment
     // setTimeout(done, 3000);
     done();
-  });
+  })
 });
