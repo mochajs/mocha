75ebe53718755f3ff3fd019dd0f0d91807d1c5b8
Tj Holowaychuk <tj@vision-media.ca>
2011-11-25 11:26:23 -0800

    Fixed global leak reporting in hooks

--- test/globals.js
+++ test/globals.js
@@ -1,5 +1,11 @@
 
 describe('global leaks', function(){
+  before(function(){
+    // uncomment to test
+    // foo = 'hey';
+    // bar = 'hey';
+  })
+  
   beforeEach(function(){
     // uncomment to test
     // foo = 'bar'
