b30eb8c1e080386ec574062ad0d06b04b27abce9
Tj Holowaychuk <tj@vision-media.ca>
2011-11-25 10:24:41 -0800

    Added Runnable

---
+++ test.js
@@ -0,0 +1,13 @@
+
+describe('something', function(){
+  beforeEach(function(done){
+    done()
+    done()
+    done()
+  })
+
+  it('should work', function(done){
+    // done(new Error('test'));
+    done();
+  })
+})
