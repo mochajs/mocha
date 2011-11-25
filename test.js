27dceada39b4271447199f588aa03edaa73a88c0
Tj Holowaychuk <tj@vision-media.ca>
2011-11-25 11:03:58 -0800

    Added Hook

--- test.js
+++ test.js
@@ -1,16 +1,25 @@
 
 describe('something', function(){
-  // beforeEach(function(done){
-  //     done()
-  //     done()
-  //     done()
-  //   })
-
-  it('should work', function(done){
-    done();
-    done();
-    done();
-    done();
-    done();
+  beforeEach(function(done){
+    done()
+    done()
+    done()
   })
+
+  it('should work', function(){
+    
+  })
+
+  // it('should work', function(done){
+  //   // done();
+  //   done()
+  //   done()
+  //   done()
+  //   done()
+  //   setTimeout(done, 4000);
+  // })
+  // 
+  // it('should work again', function(done){
+  //   setTimeout(done, 5000);
+  // })
 })
