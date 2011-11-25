fff4926a8ab0aca2cab3c51b671616ff450b7784
Tj Holowaychuk <tj@vision-media.ca>
2011-11-25 10:59:27 -0800

    utilize new "error" event

--- test.js
+++ test.js
@@ -1,13 +1,16 @@
 
 describe('something', function(){
-  beforeEach(function(done){
-    done()
-    done()
-    done()
-  })
+  // beforeEach(function(done){
+  //     done()
+  //     done()
+  //     done()
+  //   })
 
   it('should work', function(done){
-    // done(new Error('test'));
+    done();
+    done();
+    done();
+    done();
     done();
   })
 })
