14fc500b67b168c795451ad91d0b106a9e6d5510
Tj Holowaychuk <tj@vision-media.ca>
2011-11-25 11:30:47 -0800

    more tests

--- test/uncaught.js
+++ test/uncaught.js
@@ -1,5 +1,12 @@
 
 describe('uncaught', function(){
+  beforeEach(function(done){
+    process.nextTick(function(){
+      // throw new Error('oh noes');
+      done();
+    });
+  })
+
   it('should report properly', function(done){
     process.nextTick(function(){
       // if you uncomment this :)
