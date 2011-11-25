2c455d851255962aea58b1037640ebad5848d0f1
Tj Holowaychuk <tj@vision-media.ca>
2011-11-25 11:27:13 -0800

    more tests

--- test/multiple.done.js
+++ test/multiple.done.js
@@ -1,5 +1,11 @@
 
 describe('multiple calls to done()', function(){
+  beforeEach(function(done){
+    done()
+    // uncomment
+    // done()
+  })
+
   it('should fail in a test-case', function(done){
     process.nextTick(function(){
       done();
