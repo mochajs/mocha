ff011726ca045ef77b978b780600d767039352aa
David Henderson <david.henderson@triggeredmessaging.com>
2011-11-29 09:53:13 +0000

    Added suiteSetup and suiteTeardown to allow setup and teardown for the whole tdd suite#

--- test/interfaces/tdd.js
+++ test/interfaces/tdd.js
@@ -1,15 +1,29 @@
 
 suite('Array', function(){
   suite('#indexOf()', function(){
+    var initialValue = 32;
+
+    suiteSetup(function(done){
+      initialValue.should.eql(32);
+      initialValue = 42;
+      done();
+    });
+
     test('should return -1 when the value is not present', function(){
+      initialValue.should.eql(42);
       [1,2,3].indexOf(5).should.equal(-1);
       [1,2,3].indexOf(0).should.equal(-1);
     })
     
     test('should return the correct index when the value is present', function(){
+      initialValue.should.eql(42);      
       [1,2,3].indexOf(1).should.equal(0);
       [1,2,3].indexOf(2).should.equal(1);
       [1,2,3].indexOf(3).should.equal(2);
     })
+    suiteTeardown(function(done){
+      initialValue.should.eql(42);
+      done();
+    });
   })
 })
