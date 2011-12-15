7597bc8f615b5487c667b7ec9953213c427213e0
Tj Holowaychuk <tj@vision-media.ca>
2011-12-14 16:15:06 -0800

    test for this.timeout()

--- test/timeout.js
+++ test/timeout.js
@@ -11,4 +11,10 @@ describe('timeouts', function(){
     // setTimeout(done, 3000);
     done();
   })
-});
+
+  it('should allow overriding per-test', function(done){
+    this.timeout(1000);
+    setTimeout(done, 300);
+  })
+})
+
