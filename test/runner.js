ef7fcfd25170c426bd705d43938a1a354569d879
Attila Domokos <adomokos@gmail.com>
2012-01-22 23:08:38 -0500

    Switching to should.contain - I know it's depricated, but works

--- test/runner.js
+++ test/runner.js
@@ -18,10 +18,8 @@ describe('Runner', function(){
 
     it('should white-list globals', function(){
       runner.globals(['foo', 'bar']);
-      var globals = runner.globals();
-      var globalsLength = globals.length;
-      globals[globalsLength - 2].should.equal('foo');
-      globals[globalsLength - 1].should.equal('bar');
+      runner.globals().should.contain('foo');
+      runner.globals().should.contain('bar');
     })
   })
 
