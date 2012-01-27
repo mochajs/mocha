e71be501fd632c18907e657468d617dcd9c55997
Attila Domokos <adomokos@gmail.com>
2012-01-27 00:07:30 -0500

    Cleaning up white space, no logic changes

--- test/acceptance/interfaces/tdd.js
+++ test/acceptance/interfaces/tdd.js
@@ -14,9 +14,9 @@ suite('Array', function(){
       [1,2,3].indexOf(5).should.equal(-1);
       [1,2,3].indexOf(0).should.equal(-1);
     })
-    
+
     test('should return the correct index when the value is present', function(){
-      initialValue.should.eql(42);      
+      initialValue.should.eql(42);
       [1,2,3].indexOf(1).should.equal(0);
       [1,2,3].indexOf(2).should.equal(1);
       [1,2,3].indexOf(3).should.equal(2);
