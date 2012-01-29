1c292eb3f9e83f506b2993ec94670b06c1eb8782
Attila Domokos <adomokos@gmail.com>
2012-01-29 16:27:32 -0500

    A more robust dot reporter

--- test/reporters/dot.js
+++ test/reporters/dot.js
@@ -2,17 +2,18 @@ var Dot = require('../../lib/reporters/dot')
   , Runner = require('../../lib/runner')
   , Suite = require('../../lib/suite')
   , util = require('util')
+  , base = require('../../lib/reporters/base');
 
 describe('dot reporter', function() {
-  var suite, runner, output, unhook, dotReporter;
+  var suite, runner, output, dotReporter;
 
   // Using the idea from here: https://gist.github.com/729616
+  // This could be in a test util file
   function stdoutHook(callback) {
     var oldWrite = process.stdout.write
 
     process.stdout.write = (function(write) {
       return function(string, encoding, fd) {
-        //write.apply(process.stdout, arguments)
         callback(string, encoding, fd)
       }
     })(process.stdout.write);
@@ -22,39 +23,79 @@ describe('dot reporter', function() {
     };
   };
 
+  var captureConsoleOutput = function(fn) {
+    var unhook = stdoutHook(function(string, encoding, fd) {
+      output += util.inspect(string);
+    });
+
+    fn.call();
+
+    unhook();
+    return output;
+  };
+
+  var dotPattern = function(color) {
+    return '\'\\u001b[' + color + 'm.\\u001b[0m\''
+  };
+
   beforeEach(function() {
     suite = new Suite(null, 'root');
     runner = new Runner(suite);
     dotReporter = new Dot(runner);
     output = "";
-    unhook = stdoutHook(function(string, encoding, fd) {
-      output += util.inspect(string);
+  });
+
+  describe('- start -', function() { 
+    it('prints a line break', function() {
+      output = captureConsoleOutput(function() {
+        runner.emit('start');
+      });
+
+      output.should.equal('\'\\n  \'');
     });
   });
 
-  it('start prints a line break', function() {
-    runner.emit('start');
-    output.should.equal('\'\\n  \'');
+  describe('- pending -', function() {
+    it('pending prints a dot in pending color', function() {
+      output = captureConsoleOutput(function() {
+        runner.emit('pending');
+      });
 
-    unhook();
+      var pending = base.colors['pending'];
+      output.should.equal(dotPattern(pending));
+    });
   });
 
-  it('pending prints a dot in pending color', function() {
-    runner.emit('pending');
+  describe('- pass -', function() {
+    it('fast pass prints a dot in base\'s pass color', function() {
+      output = captureConsoleOutput(function() {
+        var test = { duration: 37 }
+        runner.emit('pass', test);
+      });
 
-    // 36 is the pending color
-    output.should.equal('\'\\u001b[36m.\\u001b[0m\'');
+      var pass = base.colors['pass'];
+      output.should.equal(dotPattern(pass));
+    });
 
-    unhook();
+    it('medium pass prints a dot in base\'s medium color', function() {
+      output = captureConsoleOutput(function() {
+        var test = { duration: 38 }
+        runner.emit('pass', test);
+      });
+
+      var medium = base.colors['medium'];
+      output.should.equal(dotPattern(medium));
+    });
+
+    it('slow pass prints a dot in bright yellow color', function() {
+      output = captureConsoleOutput(function() {
+        var test = { duration: 76 }
+        runner.emit('pass', test);
+      });
+
+      var brightYellow = base.colors['bright yellow'];
+      output.should.equal(dotPattern(brightYellow));
+    });
   });
 
-  it('fast pass prints a dot', function() {
-    var test = {}
-    runner.emit('pass', test);
-
-    // 90 is the fast pass color
-    output.should.equal('\'\\u001b[90m.\\u001b[0m\'');
-
-    unhook();
-  });
 });
