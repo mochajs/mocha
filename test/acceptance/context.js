67fb9d101a125708d90b9256c59f8c1b5aba1f80
TJ Holowaychuk <tj@vision-media.ca>
2012-02-24 14:16:13 -0800

    added test for cascading Context

--- test/acceptance/context.js
+++ test/acceptance/context.js
@@ -1,59 +1,26 @@
 
 describe('Context', function(){
-  var prev;
+  beforeEach(function(){
+    this.calls = ['before'];
+  })
 
-  before(function(){
-    this.age = 2;
+  describe('nested', function(){
+    beforeEach(function(){
+      this.calls.push('before two');
+    })
+
+    it('should work', function(){
+      this.calls.should.eql(['before', 'before two']);
+      this.calls.push('test');
+    })
+
+    after(function(){
+      this.calls.should.eql(['before', 'before two', 'test']);
+      this.calls.push('after two');
+    })
   })
 
   after(function(){
-    this.age.should.equal(2);
-    this.name.should.equal('Tobi');
-    this.inspect().should.equal('{\n  "age": 2,\n  "name": "Tobi"\n}');
-  })
-
-  beforeEach(function(){
-    this.name = 'Tobi';
-    this.age.should.equal(2);
-  })
-
-  afterEach(function(){
-    this.name.should.equal('Tobi');
-    prev = this;
-  })
-
-  it('should be maintained between hooks', function(){
-    this.name.should.equal('Tobi');
-    this.age.should.equal(2);
-  })
-
-  it('should provide the same object', function(){
-    this.should.equal(prev);
-  })
-
-  describe('when async', function(){
-    var prev;
-
-    beforeEach(function(done){
-      this.name = 'Tobi';
-      this.age.should.equal(2);
-      done();
-    })
-
-    afterEach(function(done){
-      this.name.should.equal('Tobi');
-      prev = this;
-      done();
-    })
-
-    it('should be maintained between hooks', function(done){
-      this.name.should.equal('Tobi');
-      done();
-    })
-
-    it('should provide the same object', function(done){
-      this.should.equal(prev);
-      done();
-    })
+    this.calls.should.eql(['before', 'before two', 'test', 'after two']);
   })
 })
