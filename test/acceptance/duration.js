12b7b48947bf7ac4ed5af8cde8c43b8978cb82b4
Raynos <raynos2@gmail.com>
2012-03-04 14:30:33 -0800

    Removed jQuery dependency

--- test/acceptance/duration.js
+++ test/acceptance/duration.js
@@ -2,19 +2,25 @@
 describe('durations', function(){
   describe('when slow', function(){
     it('should highlight in red', function(done){
-      setTimeout(done, 100);
+      setTimeout(function(){
+        done();
+      }, 100);
     })
   })
   
   describe('when reasonable', function(){
     it('should highlight in yellow', function(done){
-      setTimeout(done, 50);
+      setTimeout(function(){
+        done();
+      }, 50);
     })
   })
   
   describe('when fast', function(){
     it('should highlight in green', function(done){
-      setTimeout(done, 10);
+      setTimeout(function(){
+        done();
+      }, 10);
     })
   })
 })
--- test/browser/array.js
+++ test/browser/array.js
@@ -1,28 +1,31 @@
 describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      var arr = [1,2,3];
-      assert(-1 == arr.indexOf(5));
+  describe('#push()', function(){
+    it('should append a value', function(){
+      var arr = [];
+      arr.push('foo');
+      arr.push('bar');
+      arr.push('baz');
+        assert('foo' == arr[0]); // to test indentation
+        assert('bar' == arr[1]);
+      assert('baz' == arr[2]);
     })
 
-    it('should be pending')
-
-    it('should return the correct index when the value is present', function(){
-      var arr = [1,2,3];
-        assert(0 == arr.indexOf(1)); // just to test indentation
-      assert(1 == arr.indexOf(2));
+    it('should return the length', function(){
+      var arr = [];
+      assert(1 == arr.push('foo'));
+      assert(2 == arr.push('bar'));
+      assert(3 == arr.push('baz'));
     })
   })
 })
 
 describe('Array', function(){
   describe('#pop()', function(){
-    it('should remove and return the last value', function(done){
+    it('should remove and return the last value', function(){
       var arr = [1,2,3];
       assert(arr.pop() == 3);
-      setTimeout(function(){
-        doesNotExist();
-      }, 0);
+      assert(arr.pop() == 2);
+      assert(arr.pop() == 1);
     })
 
     it('should adjust .length', function(){
