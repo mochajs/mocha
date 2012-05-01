acd161c5d83e581c4054515e7cb140ed4ab80ad0
TJ Holowaychuk <tj@vision-media.ca>
2012-05-01 08:50:02 -0700

    refactored grep tests

---
+++ test/grep.js
@@ -0,0 +1,35 @@
+
+var Mocha = require('../');
+
+describe('Mocha', function(){
+  describe('"grep" option', function(){
+    it('should add a RegExp to the mocha.options object', function(){
+      var mocha = new Mocha({ grep: /foo/ });
+      mocha.options.grep.toString().should.equal('/foo/');
+    })
+
+    it('should convert grep string to a RegExp', function(){
+      var mocha = new Mocha({ grep: 'foo' });
+      mocha.options.grep.toString().should.equal('/foo/');
+    })
+  })
+
+  describe('.grep()', function(){
+    it('should add a RegExp to the mocha.options object', function(){
+      var mocha = new Mocha;
+      mocha.grep(/foo/);
+      mocha.options.grep.toString().should.equal('/foo/');
+    })
+
+    it('should convert grep string to a RegExp', function(){
+      var mocha = new Mocha;
+      mocha.grep('foo');
+      mocha.options.grep.toString().should.equal('/foo/');
+    })
+
+    it('should return it\'s parent Mocha object for chainability', function(){
+      var mocha = new Mocha;
+      mocha.grep().should.equal(mocha);
+    })
+  })
+})
