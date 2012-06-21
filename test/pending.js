e30ae4d84235d1fc88e4e62e2ed7e89c29c73da7
Benjamin Horsleben <benjamin@fizkerinc.dk>
2012-06-21 13:22:41 +0200

    Added some whitespace.

--- test/pending.js
+++ test/pending.js
@@ -5,6 +5,7 @@ describe('pending tests', function() {
   var oldAddTest
     , latestTestIsPending
     , allTests
+
   beforeEach(function() {
     allTests = [];
     latestTestIsPending = false;
@@ -14,21 +15,25 @@ describe('pending tests', function() {
       latestTestIsPending = !!test.pending;
     };
   });
+
   afterEach(function() {
     mocha.Suite.prototype.addTest = oldAddTest;
   });
+
   describe('When calling xit', function() {
     it('should add a new pending test', function() {
       xit('blabla', function() {});
       latestTestIsPending.should.equal(true);
     });
   });
+
   describe('When calling it with no function', function() {
     it('should add a new pending test', function() {
       xit('blabla');
       latestTestIsPending.should.equal(true);
     });
   });
+
   describe('When calling xdescribe with only it calls', function() {
     beforeEach(function() {
       xdescribe('bla', function() {
@@ -37,15 +42,18 @@ describe('pending tests', function() {
         xit('ghi', function() {});
       });
     });
+
     it('should add tests', function() {
       allTests.length.should.equal(3);
     });
+
     it('should only add pending tests', function() {
       allTests.every(function(test) {
         return !!test.pending;
       }).should.equal(true);
     });
   });
+
   describe('When calling xdescribe with nested describes', function() {
     beforeEach(function() {
       xdescribe('bla', function() {
@@ -54,9 +62,11 @@ describe('pending tests', function() {
         });
       });
     });
+
     it('should add tests', function() {
       allTests.length.should.equal(1);
     });
+
     it('should only add pending tests', function() {
       allTests[0].pending.should.equal(true);
     });
