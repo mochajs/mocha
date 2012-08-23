826a49b06245561a30c25374a2ec53b3e933cdc8
Sebastien Lambla <seb@serialseb.com>
2012-08-23 11:25:53 +0100

    Makes the ui pluggable alongside reporters

---
+++ test/acceptance/interfaces/custom/example.js
@@ -0,0 +1,8 @@
+var yoda = 'amazing';
+
+hmmm('When nine hundred years old you reach', function() {
+    padawan('look as good you wont', function() {
+      yoda.should.equal('amazing');
+    });
+  }
+);
