e45b52f899a449353a2708ae74fc21993aa16d53
Alexander Early <alexander.early@gmail.com>
2013-05-05 15:32:34 -0700

    added complicated bail test

---
+++ test/acceptance/misc/bail2.js
@@ -0,0 +1,76 @@
+describe("Granular bail tests: ", function () {
+
+  var runs = 0;
+
+  this.bail(false);
+
+  describe("bailing suite: ", function () {
+    this.bail(true);
+    it("should fail", function () {
+      throw new Error("this is ok");
+    });
+
+    it("should not get here", function () {
+      throw new Error("we should have bailed");
+    });
+  });
+
+  describe("second bailing suite: ", function () {
+    this.bail(true);
+
+    it("should have run no successful tests by now", function () {
+      runs += 1;
+      runs.should.eql(1);
+    });
+
+    it("should fail", function () {
+      throw new Error("this is ok");
+    });
+
+    it("should not get here", function () {
+      throw new Error("we should have bailed");
+    });
+    
+  });
+
+  describe("non-bailing suite: ", function () {
+    var bail = this.bail();
+    it("should run this test, the second successful test", function () {
+      runs += 1;
+      runs.should.eql(2);
+    });
+
+    it("this suite should not bail", function () {
+      bail.should.equal(false);
+    });
+
+    it("should run this and fail", function () {
+      throw new Error("this is ok");
+    });
+
+    it("should run this test, the third test", function () {
+      runs += 1;
+      runs.should.eql(3);
+    });
+
+  });
+
+  describe("third bailing suite: ", function () {
+    this.bail(true);
+
+    it("should have run 3 successful tests by now", function () {
+      runs += 1;
+      runs.should.eql(4);
+    });
+
+    it("should fail", function () {
+      throw new Error("this is ok");
+    });
+
+    it("should not get here", function () {
+      throw new Error("we should have bailed");
+    });
+    
+  });
+
+});
