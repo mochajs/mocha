bc97cc3705735942dff3db5735ddd66d6e7f451a
Alexander Early <alexander.early@gmail.com>
2013-05-13 14:21:23 -0700

    working test for parallel suites

---
+++ test/acceptance/parallel.js
@@ -0,0 +1,48 @@
+describe('parallel suite', function () {
+  var start;
+
+  before(function (done) {
+    start = new Date();
+    done();
+  });
+
+  after(function (done) {
+    var end = new Date();
+    (end.getTime() - start.getTime()).should.be.below(200);
+    done();
+  })
+
+  this._parallel = true;
+
+  describe('parallel suite 1', function () {
+    it('test 1-1', function (done) {
+      setTimeout(done, 0);
+    });
+
+    it('async test 1-2', function (done) {
+      setTimeout(done, 100);
+    });
+
+    after(function (done) {
+      var end = new Date();
+      (end.getTime() - start.getTime()).should.be.below(110);
+      done();
+    })
+  })
+
+  describe('parallel suite 2', function () {
+    it('test 2-1', function (done) {
+      setTimeout(done, 60);
+    })
+
+    it('test 2-2', function (done) {
+      setTimeout(done, 60);
+    })
+
+    after(function (done) {
+      var end = new Date();
+      (end.getTime() - start.getTime()).should.be.below(130);
+      done();
+    })
+  });
+})
