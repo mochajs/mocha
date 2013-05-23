729f6a050fb8e92f6c3aadf348703d52dd87934c
Romain Prieto <romain.prieto@gmail.com>
2013-05-23 12:04:52 +1000

    Diff unit tests: using should.eql to "showDiff" on objects

--- test/acceptance/diffs.js
+++ test/acceptance/diffs.js
@@ -11,19 +11,19 @@ describe('diffs', function(){
   it('should display a diff for small strings', function(){
     var a = 'foo bar baz'
       , b = 'foo rar baz';
-    a.should.equal(b);
+    a.should.eql(b);
   });
 
   it('should display a diff for medium strings', function(){
     var a = 'foo bar baz\nfoo bar baz\nfoo bar baz'
       , b = 'foo bar baz\nfoo rar baz\nfoo bar raz';
-    a.should.equal(b);
+    a.should.eql(b);
   });
 
   it('should display a diff for entire object dumps', function(){
     var a = { name: 'joe',  age: 30, address: {city: 'new york', country: 'us'}}
       , b = { name: 'joel', age: 30, address: {city: 'new york', country: 'usa'}};
-    a.should.equal(b);
+    a.should.eql(b);
   });
 
   it('should display a word diff for large strings', function(){
