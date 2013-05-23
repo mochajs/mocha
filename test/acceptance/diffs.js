1a90f3d97baf6a4965cb8614395b74f858609573
Romain Prieto <romain.prieto@gmail.com>
2013-05-23 11:58:15 +1000

    Basic tests to check the output

--- test/acceptance/diffs.js
+++ test/acceptance/diffs.js
@@ -4,21 +4,30 @@ var fs = require('fs')
   , cssout = fs.readFileSync('test/acceptance/fixtures/css.out', 'ascii');
 
 describe('diffs', function(){
-  it('should display a word diff for small strings', function(){
+  
+  // uncomment the assertions, and run with different params to check the output
+  // ex: --color, --no-color, --unified-diff
+  
+  it('should display a diff for small strings', function(){
     var a = 'foo bar baz'
       , b = 'foo rar baz';
+    a.should.equal(b);
+  });
 
-    // a.should.equal(b);
-  })
-
-  it('should display a word diff for medium strings', function(){
+  it('should display a diff for medium strings', function(){
     var a = 'foo bar baz\nfoo bar baz\nfoo bar baz'
       , b = 'foo bar baz\nfoo rar baz\nfoo bar raz';
+    a.should.equal(b);
+  });
 
-    // a.should.equal(b);
-  })
+  it('should display a diff for entire object dumps', function(){
+    var a = { name: 'joe',  age: 30, address: {city: 'new york', country: 'us'}}
+      , b = { name: 'joel', age: 30, address: {city: 'new york', country: 'usa'}};
+    a.should.equal(b);
+  });
 
   it('should display a word diff for large strings', function(){
     // cssin.should.equal(cssout);
-  })
-})
+  });
+  
+});
