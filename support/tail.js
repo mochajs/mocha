2efb5ac3ed8b1ca0ba3777c95ab7e32f482a4068
Seth McLaughlin <s@sethmcl.com>
2013-08-07 10:34:48 -0700

    Added shim for Array.prototype.indexOf() so mocha.js will run in IE7/8

--- support/tail.js
+++ support/tail.js
@@ -126,3 +126,40 @@ mocha.run = function(fn){
  */
 
 Mocha.process = process;
+
+/**
+ * Array.prototype.indexOf shim for IE 7/8 compatibility
+ * Pulled from MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+ */
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError();
+    }
+    var n, k, t = Object(this),
+        len = t.length >>> 0;
+
+    if (len === 0) {
+      return -1;
+    }
+    n = 0;
+    if (arguments.length > 1) {
+      n = Number(arguments[1]);
+      if (n != n) { // shortcut for verifying if it's NaN
+        n = 0;
+      } else if (n != 0 && n != Infinity && n != -Infinity) {
+        n = (n > 0 || -1) * Math.floor(Math.abs(n));
+      }
+    }
+    if (n >= len) {
+      return -1;
+    }
+    for (k = n >= 0 ? n : Math.max(len - Math.abs(n), 0); k < len; k++) {
+      if (k in t && t[k] === searchElement) {
+        return k;
+      }
+    }
+    return -1;
+  };
+}
