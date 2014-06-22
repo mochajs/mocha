9db4987928795c2f325f3689215a4fdb0d12c027
Travis Jeffery <tj@travisjeffery.com>
2014-06-21 22:35:49 -0400

    Fix tests to be faster.

--- test/acceptance/failing/timeout.js
+++ test/acceptance/failing/timeout.js
@@ -1,13 +1,12 @@
-
 describe('timeout', function(){
-  this.timeout(20);
+  this.timeout(1);
 
   it('should be honored with sync suites', function(){
-    sleep(30);
+    sleep(2);
   });
 
   it('should be honored with async suites', function(done){
-    sleep(30);
+    sleep(2);
     done();
   });
 
