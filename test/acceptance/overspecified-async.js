e8db3da036a26ec989f1656af925f0d323fe6d0c
Mike Pennisi <mike@mikepennisi.com>
2014-08-22 19:08:40 -0400

    Fail when test resolution method is overspecified
    Users may register `Runnable`s as asynchronous in one of two ways:
    
    - Via callback (by defining the body function to have an arity of one)
    - Via promise (by returning a Promise object from the body function)
    
    When both a callback function is specified *and* a Promise object is
    returned, the `Runnable`'s resolution condition is ambiguous.
    Practically speaking, users are most likely to make this mistake as they
    transition between asynchronous styles.
    
    Currently, Mocha silently prefers the callback amd ignores the Promise
    object. Update the implementation of the `Runnable` class to fail
    immediately when the test resolution method is over-specified in this
    way.

---
+++ test/acceptance/overspecified-async.js
@@ -0,0 +1,8 @@
+describe('overspecified asynchronous resolution method', function() {
+  it('should fail when multiple methods are used', function(done) {
+    setTimeout(done, 0);
+
+    // uncomment
+    // return { then: function() {} };
+  });
+});
