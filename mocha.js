acbca8f92d7a64460770be063934dcf7d8a86d5d
Christopher Hiller <chiller@badwing.com>
2014-08-30 15:44:03 -0700

    #1329 again: fixed boolean blunder

--- mocha.js
+++ mocha.js
@@ -5990,7 +5990,7 @@ mocha.run = function(fn){
 
   return Mocha.prototype.run.call(mocha, function(err){
     // The DOM Document is not available in Web Workers.
-    if (global.document && options.noHighlighting !== false) {
+    if (global.document && options.noHighlighting !== true) {
       Mocha.utils.highlightTags('code');
     }
     if (fn) fn(err);
