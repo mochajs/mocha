53ef07519ce4359aee9d11b5b572c3ee5ddc8c70
dasilvacontin <daviddasilvacontin@gmail.com>
2014-11-07 22:32:10 +0100

    may fix intermittent http build failure, closes #1422

--- test/acceptance/http.js
+++ test/acceptance/http.js
@@ -7,6 +7,7 @@ var server = http.createServer(function(req, res){
 server.listen(8888);
 
 describe('http', function(){
+  this.timeout(500);
   it('should provide an example', function(done){
     http.get({ path: '/', port: 8888 }, function(res){
       res.should.have.property('statusCode', 200);
