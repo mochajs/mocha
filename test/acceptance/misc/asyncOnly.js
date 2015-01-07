7657cb11d960cf2cd8407b256019b2e34dc93328
Jason Lai <jason@getpebble.com>
2015-01-06 18:26:14 -0800

    Allow --async-only to be satisfied by returning a promise
    This results in a slight change to the behavior of --async-only:
    instead of failing immediately, check to see if the test returned
    a promise (or otherwise failed) before complaining about not
    having a done callback.

--- test/acceptance/misc/asyncOnly.js
+++ test/acceptance/misc/asyncOnly.js
@@ -6,4 +6,23 @@ describe('asyncOnly', function(){
   it('should pass', function(done){
     done();
   })
+
+  it('should ignore pending tests')
+
+  it('should fail when test throws an error', function(){
+    // the async warning only applies if the test would have otherwise passed
+    throw Error('you should see this error');
+  })
+
+  describe('with a function that returns a promise', function() {
+    it('should pass', function(){
+      var fulfilledPromise = {
+        then: function (fulfilled, rejected) {
+          process.nextTick(fulfilled);
+        }
+      };
+
+      return fulfilledPromise;
+    })
+  })
 })
