ebc6aee50dd1fdca9ab940b43ab79db72f0a4f93
Ajay Kodali <ajay.kodali@citrix.com>
2015-07-15 16:09:16 -0700

    * Fixed async hook error handling issue where an error in the hook was bailing sibling suites too * Added unit test cases for async hook error handling * Fixed minor issue in existing sync hook error test case * Moving code to fixtures dir to make the hook.err.js tests more readable * Trimming the hook error test cases by removing hooks not under test * Fix indentation * Rename var to afterAllHookCalled

---
+++ test/integration/fixtures/hooks/before.hook.async.error.js
@@ -0,0 +1,19 @@
+describe('spec 1', function () {
+  before(function (done) {
+    console.log('before');
+    process.nextTick(function () {
+      throw new Error('before hook error');
+    });
+  });
+  it('should not be called because of error in before hook', function () {
+    console.log('test 1');
+  });
+  it('should not be called because of error in before hook', function () {
+    console.log('test 2');
+  });
+});
+describe('spec 2', function () {
+  it('should be called, because hook error was in a sibling suite', function () {
+    console.log('test 3');
+  });
+});
---
+++ test/integration/fixtures/hooks/multiple.hook.async.error.js
@@ -0,0 +1,139 @@
+before(function () {
+  console.log('root before');
+});
+beforeEach(function () {
+  console.log('root before each');
+});
+describe('1', function () {
+  beforeEach(function () {
+    console.log('1 before each');
+  });
+
+  describe('1.1', function () {
+    before(function () {
+      console.log('1.1 before');
+    });
+    beforeEach(function (done) {
+      console.log('1.1 before each');
+      process.nextTick(function () {
+        throw new Error('1.1 before each hook failed');
+      });
+    });
+    it('1.1 test 1', function () {
+      console.log('1.1 test 1');
+    });
+    it('1.1 test 2', function () {
+      console.log('1.1 test 2');
+    });
+    afterEach(function () {
+      console.log('1.1 after each');
+    });
+    after(function (done) {
+      console.log('1.1 after');
+      process.nextTick(function () {
+        throw new Error('1.1 after hook failed');
+      });
+    });
+  });
+
+  describe('1.2', function () {
+    before(function () {
+      console.log('1.2 before');
+    });
+    beforeEach(function () {
+      console.log('1.2 before each');
+    });
+    it('1.2 test 1', function () {
+      console.log('1.2 test 1');
+    });
+    it('1.2 test 2', function () {
+      console.log('1.2 test 2');
+    });
+    afterEach(function (done) {
+      console.log('1.2 after each');
+      process.nextTick(function () {
+        throw new Error('1.2 after each hook failed');
+      });
+    });
+    after(function () {
+      console.log('1.2 after');
+    });
+  });
+
+  afterEach(function () {
+    console.log('1 after each');
+  });
+
+  after(function () {
+    console.log('1 after');
+  });
+});
+
+describe('2', function () {
+  beforeEach(function (done) {
+    console.log('2 before each');
+    process.nextTick(function () {
+      throw new Error('2 before each hook failed');
+    });
+  });
+
+  describe('2.1', function () {
+    before(function () {
+      console.log('2.1 before');
+    });
+    beforeEach(function () {
+      console.log('2.1 before each');
+    });
+    it('2.1 test 1', function () {
+      console.log('2.1 test 1');
+    });
+    it('2.1 test 2', function () {
+      console.log('2.1 test 2');
+    });
+    afterEach(function () {
+      console.log('2.1 after each');
+    });
+    after(function () {
+      console.log('2.1 after');
+    });
+  });
+
+  describe('2.2', function () {
+    before(function () {
+      console.log('2.2 before');
+    });
+    beforeEach(function () {
+      console.log('2.2 before each');
+    });
+    it('2.2 test 1', function () {
+      console.log('2.2 test 1');
+    });
+    it('2.2 test 2', function () {
+      console.log('2.2 test 2');
+    });
+    afterEach(function () {
+      console.log('2.2 after each');
+    });
+    after(function () {
+      console.log('2.2 after');
+    });
+  });
+
+  afterEach(function (done) {
+    console.log('2 after each');
+    process.nextTick(function () {
+      throw new Error('2 after each hook failed');
+    });
+  });
+
+  after(function () {
+    console.log('2 after');
+  });
+});
+
+after(function () {
+  console.log('root after');
+});
+afterEach(function () {
+  console.log('root after each');
+});
