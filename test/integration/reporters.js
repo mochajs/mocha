e950ea6b3601717f3272eb9f7bc205a972400ca8
Daniel St. Jules <danielst.jules@gmail.com>
2015-09-09 12:29:29 -0700

    Fix #1864: xunit missing output with --reporter-options output

--- test/integration/reporters.js
+++ test/integration/reporters.js
@@ -1,8 +1,12 @@
 var assert = require('assert');
+var os     = require('os');
+var fs     = require('fs');
+var crypto = require('crypto');
+var path   = require('path');
 var run    = require('./helpers').runMocha;
 
 describe('reporters', function() {
-  this.timeout(1000);
+  this.timeout(3000);
 
   describe('markdown', function() {
     var res;
@@ -26,6 +30,31 @@ describe('reporters', function() {
       ].join('\n');
 
       assert(res.output.indexOf(src) !== -1, 'No assert found');
+    });
+  });
+
+  describe('xunit', function() {
+    it('prints test cases with --reporter-options output (issue: 1864)', function(done) {
+      var randomStr = crypto.randomBytes(8).toString('hex');
+      var tmpDir = os.tmpDir().replace(new RegExp(path.sep + '$'), '');
+      var tmpFile = tmpDir + path.sep + 'test-issue-1864-' + randomStr + '.xml';
+
+      var args = ['--reporter=xunit', '--reporter-options', 'output=' + tmpFile];
+      var expectedOutput = [
+        '<testcase classname="suite" name="test1" time="0"/>',
+        '<testcase classname="suite" name="test2" time="0"/>',
+        '</testsuite>'
+      ].join('\n');
+
+      run('passing.js', args, function(err, result) {
+        if (err) return done(err);
+
+        var xml = fs.readFileSync(tmpFile, 'utf8');
+        fs.unlinkSync(tmpFile);
+
+        assert(xml.indexOf(expectedOutput) !== -1, 'Did not output all xml');
+        done(err);
+      });
     });
   });
 });
