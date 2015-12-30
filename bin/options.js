4a9fc550bbce06d727a4b963073041d57c9c64f7
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2015-12-30 21:29:44 +0100

    fixes #451, fixes #1721, fixes #1504

--- bin/options.js
+++ bin/options.js
@@ -2,7 +2,8 @@
  * Dependencies.
  */
 
-var fs = require('fs');
+var fs = require('fs')
+  , path = require('path');
 
 /**
  * Export `getOptions`.
@@ -15,25 +16,55 @@ module.exports = getOptions;
  */
 
 function getOptions() {
-  var optsPath = process.argv.indexOf('--opts') !== -1
-        ? process.argv[process.argv.indexOf('--opts') + 1]
-        : 'test/mocha.opts';
+  var lookupOptsPaths = ['mocha.opts', 'test/mocha.opts']
+
+  process.argv.slice(2)
+    .forEach(function (arg, i, argv){
+      var regex = /^\-\-?/
+        , isOptionArg = argv[i - 1] && regex.test(argv[i - 1]) || regex.test(arg)
+      if (isOptionArg) return;
+      Array.prototype.unshift.apply(lookupOptsPaths, getOptionsLookupTree(arg));
+    })
+
+  if (~process.argv.indexOf('--opts')) {
+    lookupOptsPaths = process.argv[process.argv.indexOf('--opts') + 1]; // should have max priority
+  }
 
   try {
-    var opts = fs.readFileSync(optsPath, 'utf8')
-      .replace(/\\\s/g, '%20')
-      .split(/\s/)
-      .filter(Boolean)
-      .map(function(value) {
-        return value.replace(/%20/g, ' ');
-      });
-
-    process.argv = process.argv
-      .slice(0, 2)
-      .concat(opts.concat(process.argv.slice(2)));
+    var optsPath;
+    while(optsPath = lookupOptsPaths.shift()){
+      if (!fs.existsSync(optsPath))
+        continue;
+      var opts = fs.readFileSync(optsPath, 'utf8')
+        .replace(/\\\s/g, '%20')
+        .split(/\s/)
+        .filter(Boolean)
+        .map(function(value) {
+          return value.replace(/%20/g, ' ');
+        });
+      process.argv = process.argv
+        .slice(0, 2)
+        .concat(opts.concat(process.argv.slice(2)));
+      break;
+    }
   } catch (err) {
     // ignore
   }
 
   process.env.LOADED_MOCHA_OPTS = true;
+}
+
+/**
+ * Get option's lookup tree.
+ */
+
+function getOptionsLookupTree(filepath){
+  try {
+    var dirname = fs.lstatSync(filepath).isDirectory() ? filepath : path.dirname(filepath)
+    return dirname.split(path.sep).map(function(_, index, array){
+      return path.join.apply(null, array.slice(0, array.length - index).concat('mocha.opts'))
+    })
+  } catch (err) {
+    return [] // path does not exist
+  }
 }
