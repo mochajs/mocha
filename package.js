69837c682b891a7609b33d1990bf6745c7740f70
Tom Coleman <tom@thesnail.org>
2016-02-25 16:24:53 +1100

    Version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 // Using the "wrapper package" version format
 Package.describe({
-  name: "tmeasday:mocha",
+  name: "avital:mocha",
   summary: "Write package tests with mocha and run them in the browser or from the command line with spacejam.",
   git: "https://github.com/practicalmeteor/meteor-mocha.git",
   version: '2.1.0_7',
