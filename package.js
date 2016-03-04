55e8a44197229e690a15960c0f979a6156c03530
Tom Coleman <tom@thesnail.org>
2016-03-04 13:19:06 +1100

    Preparing for PR

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 // Using the "wrapper package" version format
 Package.describe({
-  name: "avital:mocha",
+  name: "practicalmeteor:mocha",
   summary: "Write package tests with mocha and run them in the browser or from the command line with spacejam.",
   git: "https://github.com/practicalmeteor/meteor-mocha.git",
   version: '2.1.0_7',
@@ -14,7 +14,7 @@ Npm.depends({
 
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom("1.3-beta.12");
 
   api.use('tmeasday:test-reporter-helpers@0.2.0');
   api.use('coffeescript');
