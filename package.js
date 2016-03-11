tmeasday/meteor-mocha

Whoops, neglected this!

package.js
@@ -3,7 +3,8 @@ Package.describe({
   name: "tmeasday:mocha",
   summary: "Write package tests with mocha and run them in the browser or from the command line with spacejam.",
   git: "https://github.com/practicalmeteor/meteor-mocha.git",
-  version: '2.1.0_5'
+  version: '2.1.0_5',
+  testOnly: true
 });
 
 

mochajs/mocha

package.js
@@ -3,7 +3,13 @@ Package.describe({
   name: "tmeasday:mocha",
   summary: "Write package tests with mocha and run them in the browser or from the command line with spacejam.",
   git: "https://github.com/practicalmeteor/meteor-mocha.git",
-  version: '2.1.0_7'
+  version: '2.1.0_7',
+  testOnly: true
+});
+
+
+Npm.depends({
+  mocha: "2.1.0"
 });
 
 
bbd326ca36b922afcf4f64eaf40659b832700ba0
Tom Coleman <tom@thesnail.org>
2016-02-25 16:23:39 +1100

    Whoops, neglected this!

--- package.js
+++ package.js
@@ -3,7 +3,13 @@ Package.describe({
   name: "tmeasday:mocha",
   summary: "Write package tests with mocha and run them in the browser or from the command line with spacejam.",
   git: "https://github.com/practicalmeteor/meteor-mocha.git",
-  version: '2.1.0_7'
+  version: '2.1.0_7',
+  testOnly: true
+});
+
+
+Npm.depends({
+  mocha: "2.1.0"
 });
 
 
