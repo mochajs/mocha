100b68a2846c18d600a5917a957927ae0a3acf10
Juan Sepulveda <juan@lavaina.com>
2016-03-29 20:15:30 -0400

    Bump version and update refecences

--- meteor/test-package/package.js
+++ meteor/test-package/package.js
@@ -6,7 +6,7 @@ Package.describe({
 
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.2.1');
+  api.versionsFrom('1.3');
 
   api.use([
     'meteor',
@@ -24,7 +24,7 @@ Package.onTest(function (api) {
     'coffeescript',
     'practicalmeteor:loglevel',
     'practicalmeteor:chai',
-    'practicalmeteor:mocha@2.1.1-rc.1',
+    'practicalmeteor:mocha@2.1.1-rc.2',
     'test-package'
   ]);
 
