b641f9e2a23e487489a51bd92f02f30ec50097e2
Juan Sepulveda <juan@lavaina.com>
2016-03-29 20:03:37 -0400

    Update versions from 1.3

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 
 
 Package.onUse(function (api) {
-  api.versionsFrom("1.3-rc.1");
+  api.versionsFrom("1.3");
 
   api.use('tmeasday:test-reporter-helpers@0.2.1');
   api.use('coffeescript');
