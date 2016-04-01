34b3e7c7db77bbd8b62847138e5d16e0bf3e3714
Juan Sepulveda <juan@lavaina.com>
2016-04-01 15:27:49 -0400

    Export mocha symbols

--- meteor/src/index.js
+++ meteor/src/index.js
@@ -1,9 +1,21 @@
-import "./setup-mocha"
-import { hideOtherCSS, hideApp } from 'meteor/tmeasday:test-reporter-helpers';
-import MochaRunner from "./meteor/src/lib/MochaRunner"
+import {runnables}  from "./setup"
+import MochaRunner  from "./lib/MochaRunner"
+import BaseReporter from "./reporters/BaseReporter"
+import HtmlReporter from "./reporters/HtmlReporter"
+import {hideOtherCSS, hideApp} from 'meteor/tmeasday:test-reporter-helpers';
 
 export const runTests = () => {
   hideApp('.mocha-wrapper');
   hideOtherCSS();
-  MochaRunner.setReporter(practical.mocha.HtmlReporter)
+  MochaRunner.setReporter(HtmlReporter)
 };
+
+let { before, after, beforeEach, afterEach,
+      describe, xdescribe, it, xit, specify,
+      xspecify, xcontext, context } = runnables;
+
+export {BaseReporter}
+export {MochaRunner}
+export { before, after, beforeEach, afterEach,
+describe, xdescribe, it, xit, specify,
+xspecify, xcontext, context };
