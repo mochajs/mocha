d2ba7a9676c88d06709f383a6243d46977758bd1
ScottFreeCode <ScottFreeCode@users.noreply.github.com>
2016-06-10 19:10:11 -0400

    Clarify nature of meta test
    This should make it possible to understand when just looking at the
    fixture.

--- test/integration/fixtures/regression/issue-2286.js
+++ test/integration/fixtures/regression/issue-2286.js
@@ -1,5 +1,5 @@
-describe('suite', function () {
-  beforeEach(function () { this.skip() })
-  after(function () { console.log('after in suite') })
-  it('test', function () {})
+describe('testception for issue 2286 meta test', function () {
+  beforeEach('skips tests', function () { this.skip() })
+  after('should run', function () { console.log('after in suite') })
+  it('skipped by beforeEach', function () {})
 })
