dd74788cc357e3709b9a219bfd54b618354159a3
Christopher Hiller <boneskull@boneskull.com>
2016-06-26 17:37:13 -0700

    allow skip() in async test context; closes #2334

--- test/integration/pending.js
+++ test/integration/pending.js
@@ -60,4 +60,47 @@ describe('pending', function() {
       });
     });
   });
+
+  describe('asynchronous skip()', function() {
+    this.timeout(1000);
+
+    describe('in spec', function() {
+      it('should immediately skip the spec and run all others', function(done) {
+        run('pending/skip.async.spec.js', args, function(err, res) {
+          assert(!err);
+          assert.equal(res.stats.pending, 1);
+          assert.equal(res.stats.passes, 1);
+          assert.equal(res.stats.failures, 0);
+          assert.equal(res.code, 0);
+          done();
+        });
+      });
+    });
+
+    describe('in before', function() {
+      it('should skip all suite specs', function(done) {
+        run('pending/skip.async.before.js', args, function(err, res) {
+          assert(!err);
+          assert.equal(res.stats.pending, 2);
+          assert.equal(res.stats.passes, 0);
+          assert.equal(res.stats.failures, 0);
+          assert.equal(res.code, 0);
+          done();
+        });
+      });
+    });
+
+    describe('in beforeEach', function() {
+      it('should skip all suite specs', function(done) {
+        run('pending/skip.sync.beforeEach.js', args, function(err, res) {
+          assert(!err);
+          assert.equal(res.stats.pending, 2);
+          assert.equal(res.stats.passes, 0);
+          assert.equal(res.stats.failures, 0);
+          assert.equal(res.code, 0);
+          done();
+        });
+      });
+    });
+  });
 });
