aa8ff21a2f23549679faa55a654a684fca0c8d68
Christopher Hiller <boneskull@boneskull.com>
2016-06-28 08:20:56 -0700

    Allow skip() in async test context
    Closes #2334. PR #2335.

---
+++ test/integration/fixtures/pending/skip.async.beforeEach.js
@@ -0,0 +1,16 @@
+describe('skip in beforeEach', function() {
+  beforeEach(function(done) {
+    var self = this;
+    setTimeout(function() {
+      self.skip();
+    }, 50);
+  });
+
+  it('should never run this test', function() {
+    throw new Error('never thrown');
+  });
+
+  it('should never run this test', function() {
+    throw new Error('never thrown');
+  });
+});
