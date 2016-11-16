5572b50671a02b60315b4987eba8b428f60c998b
Rijnhard Hessel <r.hessel.git@gmail.com>
2016-11-16 14:46:07 +0200

    stripped out hacky Command.parseOptions code using a custom parser instead of Commanders. updated test cases expanded args in `mocha` because it needs to happen before the call to `_mocha`

--- test/cli/cli.spec.js
+++ test/cli/cli.spec.js
@@ -7,6 +7,8 @@ var cli = require('../../lib/cli/cli');
 var path = require('path');
 var Mocha = require('../../');
 
+delete process.env.LOADED_MOCHA_OPTS;
+
 var testArgs1 = [
   '/usr/bin/nodejs',
   '/random/path/mocha/bin/_mocha',
@@ -39,77 +41,19 @@ describe('Cli', function () {
     it('should have these properties before parsing', function () {
       program.should.be.an.instanceOf(Command);
       program.should.have.property('_makeCommand', true);
-      program.parseOptions.should.be.a.Function;
       // ensure its correctly set before parsing
-      program.opts.should.be.a.Function;
-    });
-
-    it('should not mutate arguments', function () {
-      // this is to make sure using multiple instances doesn't break
-      var program1 = cli.makeCommand();
-      var program2 = cli.makeCommand();
-      var program3 = cli.makeCommand();
-
-      program1.parseOptions(testArgs1);
-      program2.parseOptions(testArgs1);
-      program3.parseOptions(testArgs1);
-
-      program1.globals.should.be.an.Array;
-      program1.globals.should.deepEqual([
-        'okGlobalA',
-        'okGlobalB',
-        'okGlobalC',
-        'callback*'
-      ]);
-      program1.require.should.be.an.Array;
-      program1.require.should.deepEqual([
-        'should',
-        path.resolve('./test/setup')
-      ]);
-
-      program2.globals.should.be.an.Array;
-      program2.globals.should.deepEqual([
-        'okGlobalA',
-        'okGlobalB',
-        'okGlobalC',
-        'callback*'
-      ]);
-      program2.require.should.be.an.Array;
-      program2.require.should.deepEqual([
-        'should',
-        path.resolve('./test/setup')
-      ]);
-
-      program3.globals.should.be.an.Array;
-      program3.globals.should.deepEqual([
-        'okGlobalA',
-        'okGlobalB',
-        'okGlobalC',
-        'callback*'
-      ]);
-      program3.require.should.be.an.Array;
-      program3.require.should.deepEqual([
-        'should',
-        path.resolve('./test/setup')
-      ]);
+      program.should.have.property('_opts');
+      program._opts.should.be.a.Function;
     });
 
     it('should have these properties after only option parsing', function () {
       var program1 = cli.makeCommand();
 
-      var parsed = program1.parseOptions(testArgs1);
-      parsed.should.be.an.Object;
-      parsed.should.deepEqual({
-        args: [
-          '/usr/bin/nodejs',
-          '/random/path/mocha/bin/_mocha',
-          'test/cli/cli.spec.js'
-        ],
-        unknown: []
-      });
+      cli.parseArgv(program1, testArgs1);
 
       // ensure its correctly set after parsing
-      program1.opts.should.be.a.Function;
+      program1._opts.should.be.a.Function;
+      program1.opts.should.be.a.String;
       program1.globals.should.be.an.Array;
       program1.globals.should.deepEqual([
         'okGlobalA',
@@ -126,13 +70,10 @@ describe('Cli', function () {
       program1.timeout.should.eql('200');
       program1.ui.should.eql('bdd');
       program1.reporter.should.eql('dot');
-
-      // rawArgs should not be set, since parseOptions doesn't set it by default
-      expect(program1.rawArgs).to.not.be.ok;
     });
 
     it('should have these properties after parsing', function () {
-      program.parse(testArgs1);
+      cli.parseArgv(program, testArgs1);
 
       // ensure its correctly set after parsing
       program.opts.should.be.a.Function;
@@ -178,32 +119,107 @@ describe('Cli', function () {
     });
   });
 
+  describe('.parseArgv()', function () {
+    it('should not mutate arguments', function () {
+      // this is to make sure using multiple instances doesn't break
+      var program1 = cli.makeCommand();
+      var program2 = cli.makeCommand();
+      var program3 = cli.makeCommand();
+
+      cli.parseArgv(program1, testArgs1);
+      cli.parseArgv(program2, testArgs1);
+      cli.parseArgv(program3, testArgs1);
+
+      program1.globals.should.be.an.Array;
+      program1.globals.should.deepEqual([
+        'okGlobalA',
+        'okGlobalB',
+        'okGlobalC',
+        'callback*'
+      ]);
+      program1.require.should.be.an.Array;
+      program1.require.should.deepEqual([
+        'should',
+        path.resolve('./test/setup')
+      ]);
+
+      program2.globals.should.be.an.Array;
+      program2.globals.should.deepEqual([
+        'okGlobalA',
+        'okGlobalB',
+        'okGlobalC',
+        'callback*'
+      ]);
+      program2.require.should.be.an.Array;
+      program2.require.should.deepEqual([
+        'should',
+        path.resolve('./test/setup')
+      ]);
+
+      program3.globals.should.be.an.Array;
+      program3.globals.should.deepEqual([
+        'okGlobalA',
+        'okGlobalB',
+        'okGlobalC',
+        'callback*'
+      ]);
+      program3.require.should.be.an.Array;
+      program3.require.should.deepEqual([
+        'should',
+        path.resolve('./test/setup')
+      ]);
+    });
+
+    it('should expand opts', function () {
+      var program1 = cli.makeCommand();
+      cli.parseArgv(program1, testArgs1);
+
+      program1.rawArgs.should.deepEqual(
+        testArgs1.slice(0, 2)
+          .concat(testOpts)
+          .concat(testArgs1.slice(2))
+      );
+    });
+
+    it('should not expand opts if process.env.LOADED_MOCHA_OPTS is true', function () {
+      process.env.LOADED_MOCHA_OPTS = true;
+
+      var program1 = cli.makeCommand();
+      cli.parseArgv(program1, testArgs1);
+
+      program1.rawArgs.should.deepEqual(testArgs1);
+
+      // cleanup
+      delete process.env.LOADED_MOCHA_OPTS;
+    });
+  });
+
   xdescribe('.setMochaOptions()', function () {
     xit('should set options on mocha', function () {
       var program = cli.makeCommand();
-      program.parse(testArgs1);
+      cli.parseArgv(program, testArgs1);
       var mocha = new Mocha();
-      cli.setMochaOptions(mocha, program.opts());
+      cli.setMochaOptions(mocha, program._opts());
 
       // how to test this? we should test as many options being set as possible
     });
 
     xit('should separate global scope per instance', function () {
       var program = cli.makeCommand();
-      program.parse(testArgs1);
+      cli.parseArgv(program, testArgs1);
     });
   });
 
   xdescribe('.run()', function () {
     var program1 = cli.makeCommand();
-    program1.parse(testArgs2);
+    cli.parseArgv(program1, testArgs2);
 
     it('should run mocha ', function () {
       var mocha = new Mocha();
-      cli.setMochaOptions(mocha, program1.opts());
+      cli.setMochaOptions(mocha, program1._opts());
       // how to test this?
 
-      cli.run(mocha, program1.opts(), cli.resolveFiles(program1.args, program1.opts()));
+      cli.run(mocha, program1._opts(), cli.resolveFiles(program1.args, program1._opts()));
     });
   });
 
@@ -215,15 +231,28 @@ describe('Cli', function () {
       files[0].should.be.eql(path.resolve('test/cli/cli.spec.js'));
     });
 
-    it('should recursively resolve and sort files', function () {
+    it('should resolve and sort files', function () {
       var files = cli.resolveFiles(['test/cli'], {
-        recursive: true,
+        recursive: false,
         sort: true
       });
 
       files.should.have.length(2);
       files[0].should.be.eql(path.resolve('test/cli/cli.spec.js'));
       files[1].should.be.eql(path.resolve('test/cli/parser.spec.js'));
+    });
+
+    it('should recursively resolve and sort files', function () {
+      var files = cli.resolveFiles(['test/cli'], {
+        recursive: true,
+        sort: true
+      });
+
+      files.should.have.length(4);
+      files[0].should.be.eql(path.resolve('test/cli/cli.spec.js'));
+      files[1].should.be.eql(path.resolve('test/cli/parser.spec.js'));
+      files[2].should.be.eql(path.resolve('test/cli/requires/a.js'));
+      files[3].should.be.eql(path.resolve('test/cli/requires/b.js'));
     });
 
     it('should resolve test.opts', function () {
---
+++ test/cli/requires/a.js
@@ -0,0 +1,3 @@
+'use strict';
+
+global.a = 'A';
