614190daad082569b8e453aca6f859638056745b
Craig Taub <craigtaub@gmail.com>
2017-02-05 23:01:00 +0000

    fix lint error

--- test/reporters/tap.spec.js
+++ test/reporters/tap.spec.js
@@ -35,7 +35,7 @@ describe('TAP reporter', function () {
       TAP.call({}, runner);
 
       var expectedArray = [
-        '1..' + expectedTotal+ '\n'
+        '1..' + expectedTotal + '\n'
       ];
       process.stdout.write = stdoutWrite;
 
