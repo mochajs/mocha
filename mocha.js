0f7f560538d7ee3b0eb91f899eacc03fd7203798
ZeroBinbin <727832784@qq.com>
2017-03-29 11:11:18 +0800

    fix the erorr that it can not unhide all suites
    by convert from HTMLCollection to Array

--- mocha.js
+++ mocha.js
@@ -2883,7 +2883,7 @@ function hideSuitesWithout (classname) {
  * Unhide .hidden suites.
  */
 function unhide () {
-  var els = document.getElementsByClassName('suite hidden');
+  var els = [].slice.call(document.getElementsByClassName('suite hidden'));
   for (var i = 0; i < els.length; ++i) {
     els[i].className = els[i].className.replace('suite hidden', 'suite');
   }
