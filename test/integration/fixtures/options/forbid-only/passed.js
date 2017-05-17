164934ae56e4b389edc76ec664f0f6b1d0cb4280
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-05-17 09:05:55 -0700

    update test description

--- test/integration/fixtures/options/forbid-only/passed.js
+++ test/integration/fixtures/options/forbid-only/passed.js
@@ -1,4 +1,4 @@
-describe('forbid only - all test pass', function () {
+describe('forbid only - `.only` is not used', function () {
   it('test1', function () {});
   it('test2', function () {});
   it('test3', function () {});
