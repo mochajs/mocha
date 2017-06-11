e5eaab041ddbbab4a7c091376c5088820b4609de
Esref Durna <edurna@gmail.com>
2017-06-11 00:26:27 +0000

    eslint fixes: karma.conf.js     1:13  error  Extra semicolon  semi     3:23  error  Extra semicolon  semi     4:27  error  Extra semicolon  semi     5:31  error  Extra semicolon  semi     6:55  error  Extra semicolon  semi     7:32  error  Extra semicolon  semi    10:20  error  Extra semicolon  semi    37:58  error  Extra semicolon  semi    39:13  error  Extra semicolon  semi    51:4   error  Extra semicolon  semi    65:24  error  Extra semicolon  semi    66:18  error  Extra semicolon  semi    69:37  error  Extra semicolon  semi    71:42  error  Extra semicolon  semi    72:80  error  Extra semicolon  semi    80:12  error  Extra semicolon  semi    81:48  error  Extra semicolon  semi    83:60  error  Extra semicolon  semi    87:41  error  Extra semicolon  semi    88:82  error  Extra semicolon  semi    90:58  error  Extra semicolon  semi    91:60  error  Extra semicolon  semi    97:10  error  Extra semicolon  semi    98:46  error  Extra semicolon  semi   100:58  error  Extra semicolon  semi   103:31  error  Extra semicolon  semi   105:38  error  Extra semicolon  semi   109:32  error  Extra semicolon  semi   110:23  error  Extra semicolon  semi   117:24  error  Extra semicolon  semi   120:74  error  Extra semicolon  semi   125:6   error  Extra semicolon  semi   127:42  error  Extra semicolon  semi   130:18  error  Extra semicolon  semi   131:2   error  Extra semicolon  semi   134:24  error  Extra semicolon  semi   135:34  error  Extra semicolon  semi   136:27  error  Extra semicolon  semi   142:4   error  Extra semicolon  semi   143:31  error  Extra semicolon  semi   154:4   error  Extra semicolon  semi   156:22  error  Extra semicolon  semi   158:21  error  Extra semicolon  semi   161:30  error  Extra semicolon  semi   162:39  error  Extra semicolon  semi
    ✖ 45 problems (45 errors, 0 warnings)

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var path = require('path');
-var mkdirp = require('mkdirp');
-var baseBundleDirpath = path.join(__dirname, '.karma');
-var osName = require('os-name');
+var fs = require('fs')
+var path = require('path')
+var mkdirp = require('mkdirp')
+var baseBundleDirpath = path.join(__dirname, '.karma')
+var osName = require('os-name')
 
 module.exports = function (config) {
-  var bundleDirpath;
+  var bundleDirpath
   var cfg = {
     frameworks: [
       'browserify',
@@ -34,9 +34,9 @@ module.exports = function (config) {
             if (!err && bundleDirpath) {
               // write bundle to directory for debugging
               fs.writeFileSync(path.join(bundleDirpath,
-                'bundle.' + Date.now() + '.js'), content);
+                'bundle.' + Date.now() + '.js'), content)
             }
-          });
+          })
       }
     },
     reporters: ['spec'],
@@ -48,7 +48,7 @@ module.exports = function (config) {
         reporter: 'html'
       }
     }
-  };
+  }
 
   // see https://github.com/saucelabs/karma-sauce-example
 
@@ -62,14 +62,14 @@ module.exports = function (config) {
 
   // TO RUN LOCALLY, execute:
   // `CI=1 SAUCE_USERNAME=<user> SAUCE_ACCESS_KEY=<key> BROWSER=<browser> PLATFORM=<platform> make test-browser`
-  var env = process.env;
-  var sauceConfig;
+  var env = process.env
+  var sauceConfig
 
   if (env.CI) {
-    console.error('CI mode enabled');
+    console.error('CI mode enabled')
     if (env.TRAVIS) {
-      console.error('Travis-CI detected');
-      bundleDirpath = path.join(baseBundleDirpath, process.env.TRAVIS_BUILD_ID);
+      console.error('Travis-CI detected')
+      bundleDirpath = path.join(baseBundleDirpath, process.env.TRAVIS_BUILD_ID)
       if (env.BROWSER && env.PLATFORM) {
         if (env.SAUCE_USERNAME && env.SAUCE_ACCESS_KEY) {
           // correlate build/tunnel with Travis
@@ -77,70 +77,70 @@ module.exports = function (config) {
             build: 'TRAVIS #' + env.TRAVIS_BUILD_NUMBER +
             ' (' + env.TRAVIS_BUILD_ID + ')',
             tunnelIdentifier: env.TRAVIS_JOB_NUMBER
-          };
-          console.error('Configured SauceLabs');
+          }
+          console.error('Configured SauceLabs')
         } else {
-          console.error('No SauceLabs credentials present');
+          console.error('No SauceLabs credentials present')
         }
       }
     } else if (env.APPVEYOR) {
-      console.error('AppVeyor detected');
-      bundleDirpath = path.join(baseBundleDirpath, process.env.APPVEYOR_BUILD_ID);
+      console.error('AppVeyor detected')
+      bundleDirpath = path.join(baseBundleDirpath, process.env.APPVEYOR_BUILD_ID)
     } else {
-      console.error('Local/unknown environment detected');
-      bundleDirpath = path.join(baseBundleDirpath, 'local');
+      console.error('Local/unknown environment detected')
+      bundleDirpath = path.join(baseBundleDirpath, 'local')
       // don't need to run sauce from appveyor b/c travis does it.
       if (env.SAUCE_USERNAME || env.SAUCE_ACCESS_KEY) {
         sauceConfig = {
           build: require('os')
             .hostname() + ' (' + Date.now() + ')'
-        };
-        console.error('Configured SauceLabs');
+        }
+        console.error('Configured SauceLabs')
       } else {
-        console.error('No SauceLabs credentials present');
+        console.error('No SauceLabs credentials present')
       }
     }
-    mkdirp.sync(bundleDirpath);
+    mkdirp.sync(bundleDirpath)
   } else {
-    console.error('CI mode disabled');
+    console.error('CI mode disabled')
   }
 
   if (sauceConfig) {
-    cfg.sauceLabs = sauceConfig;
-    addSauceTests(cfg);
+    cfg.sauceLabs = sauceConfig
+    addSauceTests(cfg)
   }
 
   // the MOCHA_UI env var will determine if we're running interface-specific
   // tests.  since you can only load one at a time, each must be run separately.
   // each has its own set of acceptance tests and a fixture.
   // the "bdd" fixture is used by default.
-  var ui = env.MOCHA_UI;
+  var ui = env.MOCHA_UI
   if (ui) {
     if (cfg.sauceLabs) {
-      cfg.sauceLabs.testName = 'Interface "' + ui + '" integration tests';
+      cfg.sauceLabs.testName = 'Interface "' + ui + '" integration tests'
     }
     cfg.files = [
       'test/browser-fixtures/' + ui + '.fixture.js',
       'test/interfaces/' + ui + '.spec.js'
-    ];
+    ]
   } else if (cfg.sauceLabs) {
-    cfg.sauceLabs.testName = 'Unit Tests';
+    cfg.sauceLabs.testName = 'Unit Tests'
   }
 
-  config.set(cfg);
-};
+  config.set(cfg)
+}
 
 function addSauceTests (cfg) {
-  var env = process.env;
-  cfg.reporters.push('saucelabs');
-  cfg.customLaunchers = {};
+  var env = process.env
+  cfg.reporters.push('saucelabs')
+  cfg.customLaunchers = {}
   cfg.customLaunchers[env.BROWSER] = {
     base: 'SauceLabs',
     browserName: env.BROWSER.split('@')[0],
     version: env.BROWSER.split('@')[1],
     platform: env.PLATFORM
-  };
-  cfg.browsers = [env.BROWSER];
+  }
+  cfg.browsers = [env.BROWSER]
 
   // See https://github.com/karma-runner/karma-sauce-launcher
   // See https://github.com/bermi/sauce-connect-launcher#advanced-usage
@@ -151,13 +151,13 @@ function addSauceTests (cfg) {
       connectRetries: 10,
       connectRetryTimeout: 60000
     }
-  };
+  }
 
-  cfg.concurrency = 5;
+  cfg.concurrency = 5
 
-  cfg.retryLimit = 5;
+  cfg.retryLimit = 5
 
   // for slow browser booting, ostensibly
-  cfg.captureTimeout = 120000;
-  cfg.browserNoActivityTimeout = 20000;
+  cfg.captureTimeout = 120000
+  cfg.browserNoActivityTimeout = 20000
 }
