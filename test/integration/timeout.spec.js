28ce7c93755df87560ea436b6e43d480d467697b
ScottFreeCode <ScottFreeCode@users.noreply.github.com>
2017-08-11 03:51:55 -0400

    Correct issue with timeout 0 closing prematurely, without causing any new issues allowing --no-exit to close

--- test/integration/timeout.spec.js
+++ test/integration/timeout.spec.js
@@ -2,6 +2,8 @@
 
 var assert = require('assert');
 var run = require('./helpers').runMochaJSON;
+var fork = require('child_process').fork;
+var path = require('path');
 var args = [];
 
 describe('this.timeout()', function () {
@@ -16,6 +18,58 @@ describe('this.timeout()', function () {
       assert.equal(res.stats.failures, 2);
       assert.equal(res.code, 2);
       done();
+    });
+  });
+
+  describe('0 (no timeout)', function () {
+    this.timeout(10 * 1000);
+
+    it('does not spuriously end async tests that miss calling `done`', function (done) {
+      var child = fork(path.join(__dirname, '..', '..', 'bin', '_mocha'), [path.join(__dirname, 'fixtures', 'timeout-0-done.fixture.js')], { silent: true });
+      child.on('error', function (error) {
+        if (timeout) { clearTimeout(timeout); }
+        if (done) { done(error); }
+        done = null;
+      });
+      var timeout;
+      child.on('message', function () {
+        timeout = setTimeout(function () {
+          child.kill('SIGINT');
+          if (done) { done(); }
+          done = null;
+        }, 5000);
+      });
+      child.on('exit', fail);
+      child.on('close', fail);
+      function fail () {
+        clearTimeout(timeout);
+        if (done) { done(new Error('Test ended despite disabled timeout!')); }
+        done = null;
+      }
+    });
+
+    it('does not spuriously end promise tests that never resolve', function (done) {
+      var child = fork(path.join(__dirname, '..', '..', 'bin', '_mocha'), [path.join(__dirname, 'fixtures', 'timeout-0-promise.fixture.js')], { silent: true });
+      child.on('error', function (error) {
+        if (timeout) { clearTimeout(timeout); }
+        if (done) { done(error); }
+        done = null;
+      });
+      var timeout;
+      child.on('message', function () {
+        timeout = setTimeout(function () {
+          child.kill('SIGINT');
+          if (done) { done(); }
+          done = null;
+        }, 5000);
+      });
+      child.on('exit', fail);
+      child.on('close', fail);
+      function fail () {
+        clearTimeout(timeout);
+        if (done) { done(new Error('Test ended despite disabled timeout!')); }
+        done = null;
+      }
     });
   });
 });
