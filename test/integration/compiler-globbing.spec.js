69dc39c6552662d4b3c9cacc8b0cd3ebbaaef1e1
Scott Santucci <ScottFreeCode@users.noreply.github.com>
2017-09-14 00:25:04 -0400

    mocha.opts + tests can pass or fail

--- test/integration/compiler-globbing.spec.js
+++ test/integration/compiler-globbing.spec.js
@@ -6,14 +6,14 @@ var path = require('path');
 
 describe('globbing like --compilers', function () {
   it('should find a file of each type', function (done) {
-    exec('"' + process.execPath + '" "' + path.join('..', 'bin', 'mocha') + '" --require coffee-script/register --require compiler-fixtures/foo.js -R json "compiler/*.@(coffee|foo)"', { cwd: path.join(__dirname, '..') }, function (error, stdout) {
-      if (error) { return done(error); }
+    exec('"' + process.execPath + '" "' + path.join('bin', 'mocha') + '" --require coffee-script/register --require test/compiler-fixtures/foo -R json "test/compiler/*.@(coffee|foo)"', { cwd: path.join(__dirname, '..', '..') }, function (error, stdout) {
+      if (error && !stdout) { return done(error); }
       var results = JSON.parse(stdout);
-      expect(results).to.have.property('passes');
+      expect(results).to.have.property('tests');
       var titles = [];
-      for (var index = 0; index < results.passes.length; index += 1) {
-        expect(results.passes[index]).to.have.property('fullTitle');
-        titles.push(results.passes[index].fullTitle);
+      for (var index = 0; index < results.tests.length; index += 1) {
+        expect(results.tests[index]).to.have.property('fullTitle');
+        titles.push(results.tests[index].fullTitle);
       }
       expect(titles).to.contain('coffeescript should work');
       expect(titles).to.contain('custom compiler should work');
