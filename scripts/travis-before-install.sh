d111ac81530bb003362b5126496b2c03ee0d5080
Christopher Hiller <boneskull@boneskull.com>
2017-09-29 11:44:41 -0700

    remove special treatment of unsupported node version in travis before-install script

--- scripts/travis-before-install.sh
+++ scripts/travis-before-install.sh
@@ -5,15 +5,5 @@ runSanityTest () {
   ./bin/mocha --opts /dev/null --reporter spec test/sanity/sanity.spec.js
 }
 
-# install npm@1.4 locally, and try to install production dependencies w/ it.
-if [[ ${TRAVIS_NODE_VERSION} =~ 'v0.10' ]]
-then
-  echo "Downgrading to npm v1.4.x..."
-  npm install npm@~1.4
-  ./node_modules/.bin/npm install --production
-  runSanityTest
-  rm -rf node_modules/
-else
-  npm install --production
-  runSanityTest
-fi
+npm install --production
+runSanityTest
