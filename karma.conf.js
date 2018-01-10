95decf68ac61a54c7e572122cd8b4cd1965d3299
38elements <mh19820223@gmail.com>
2018-01-10 16:59:26 +0900

    Browser test

--- karma.conf.js
+++ karma.conf.js
@@ -143,6 +143,7 @@ module.exports = config => {
       if (cfg.sauceLabs) {
         cfg.sauceLabs.testName = 'Unit Tests';
       }
+      cfg.files.push('test/browser-specific/html-reporter.spec.js');
   }
 
   config.set(cfg);
