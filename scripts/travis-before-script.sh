51a9116d0b3501122ea769019f4199e7852e5359
Outsider <outsideris@gmail.com>
2018-01-19 23:22:29 +0900

    extract checking AMD bundle as own test

--- scripts/travis-before-script.sh
+++ scripts/travis-before-script.sh
@@ -2,6 +2,3 @@
 
 # bundle artifacts to AWS go here
 mkdir -p .karma
-
-# ensure we are building a non-broken bundle for AMD
-npm start build.mochajs && [[ -z "$(grep 'define.amd' mocha.js)" ]] || exit 1
