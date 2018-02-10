f8b5472fa75eed1c8891ebec3168818f4aefda04
dfberry <dinaberry@outlook.com>
2018-02-10 11:19:29 -0800

    works so far

--- package-scripts.js
+++ package-scripts.js
@@ -221,6 +221,10 @@ module.exports = {
     watchDocs: {
       script: 'nps prewatchDocs && bundle exec jekyll serve --source ./docs --destination ./docs/_site --config ./docs/_config.yml --safe --drafts --watch',
       description: 'Watch documentation for changes'
+    },
+    buildApiDocs:{
+      script:"node node_modules/documentation/bin/documentation.js build ./lib/mocha.js -f md  --sort-order 'alpha' -a=public > docs/mocha-api.md",
+      description:"build api docs"
     }
   }
 };
