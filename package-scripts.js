1aa1331293adb1d06f11fa194fe3f6c1096e8752
dfberry <dinaberry@outlook.com>
2018-02-11 09:39:06 -0800

    first solid pass

--- package-scripts.js
+++ package-scripts.js
@@ -223,8 +223,12 @@ module.exports = {
       description: 'Watch documentation for changes'
     },
     buildApiDocs:{
-      script:"node node_modules/documentation/bin/documentation.js build ./lib/mocha.js -f json  --sort-order 'alpha' -a=public > docs/mocha-api.json && node node_modules/documentation/bin/documentation.js build ./lib/mocha.js -f md  --sort-order 'alpha' -a=public > docs/mocha-api.md",
+      script:"node node_modules/documentation/bin/documentation.js build ./lib/mocha.js -f md  --sort-order 'alpha' -a=public > docs/mocha-api.md",
       description:"build api docs, cli: 'npm start buildApiDocs' "
+    },
+    buildApiDocsDebug:{
+      script:"node node_modules/documentation/bin/documentation.js build ./lib/mocha.js -f json  --sort-order 'alpha' -a=public > docs/mocha-api.json",
+      description:"build debuggable api docs, cli: 'npm start buildApiDocsDebug' "
     }
   }
 };
