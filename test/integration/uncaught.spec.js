4935e785ed1810f04a60835b365792dc1d3f4358
johanblumenberg <johan.blumenberg@gmail.com>
2018-03-12 13:03:06 +0100

    Timeout and uncaught exception handled the same way

--- test/integration/uncaught.spec.js
+++ test/integration/uncaught.spec.js
@@ -16,7 +16,7 @@ describe('uncaught exceptions', function () {
       assert.equal(res.stats.failures, 1);
 
       assert.equal(res.failures[0].fullTitle,
-        'uncaught "before each" hook');
+        'uncaught "before each" hook for "test"');
       assert.equal(res.code, 1);
       done();
     });
