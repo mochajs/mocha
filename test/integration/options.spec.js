747dd5be5adf8164a4eb40634559e965bf8c7105
Craig Taub <craigtaub@gmail.com>
2018-05-09 11:20:12 +0100

    increase time
    debug appveyor
    
    debug - check colors
    
    debug  - sigints event
    
    debug - proc listeners
    
    remove logs

--- test/integration/options.spec.js
+++ test/integration/options.spec.js
@@ -523,12 +523,14 @@ describe('options', function() {
           }
 
           var expectedCloseCursor = '\u001b[?25h';
+          console.log('data', data);
           expect(data.output, 'to contain', expectedCloseCursor);
           expect(data.code, 'to be', 130);
           done();
         });
         var t = setTimeout(function() {
           // kill the child process
+          console.log('TIMER SIGINT')
           mocha.kill('SIGINT');
         }, 500);
       });
