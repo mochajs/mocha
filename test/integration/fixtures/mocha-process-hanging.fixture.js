498b13fee9042d2b8b7e52133d970f3b0feb98c9
Craig Taub <craigtaub@gmail.com>
2018-05-09 17:19:16 +0100

    remove unused spec + tidy

--- test/integration/fixtures/mocha-process-hanging.fixture.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-require('../../../bin/_mocha');
-process.stdout.on('data', function(data) {});
-
-describe('hanging process', function() {
-  it('test should pass', function () {
-  });
-});
