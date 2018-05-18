0a5604f13b1759b3395cab2c97c05896505c6830
Christopher Hiller <boneskull@boneskull.com>
2018-05-18 16:03:37 -0700

    reformat missed files

--- scripts/dedefine.js
+++ scripts/dedefine.js
@@ -7,10 +7,9 @@
 
 const through = require('through2');
 const defineRx = /typeof\s+define\s*===?\s*['"]function['"]\s*&&\s*(?:define\.amd|typeof\s+define\.amd\s*===?\s*['"]object['"]\s*&&\s*define\.amd)/g;
-function createStream () {
-  return through.obj(function (chunk, enc, next) {
-    this.push(String(chunk)
-      .replace(defineRx, 'false'));
+function createStream() {
+  return through.obj(function(chunk, enc, next) {
+    this.push(String(chunk).replace(defineRx, 'false'));
     next();
   });
 }
