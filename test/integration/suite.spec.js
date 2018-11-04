be17ea5beecfb6fb4dfebcd584a19e0493c77aa5
P. Roebuck <plroebuck@users.noreply.github.com>
2018-11-03 20:52:59 -0500

    Warn that suites cannot return a value (#3550)
    * Give a `DeprecationWarning` on suite callback returning any value.
    * Deprecation warning: Show a message only once; use `process.nextTick` when falling back to `console.warn`
    * Add a test for `utils.deprecate`
    * style: Make prettier happy
    * test(deprecate.spec.js): Make PR requested changes for approval (per @boneskull)

--- test/integration/suite.spec.js
+++ test/integration/suite.spec.js
@@ -9,8 +9,7 @@ describe('suite w/no callback', function() {
   it('should throw a helpful error message when a callback for suite is not supplied', function(done) {
     run('suite/suite-no-callback.fixture.js', args, function(err, res) {
       if (err) {
-        done(err);
-        return;
+        return done(err);
       }
       var result = res.output.match(/no callback was supplied/) || [];
       assert.equal(result.length, 1);
@@ -24,8 +23,7 @@ describe('skipped suite w/no callback', function() {
   it('should not throw an error when a callback for skipped suite is not supplied', function(done) {
     run('suite/suite-skipped-no-callback.fixture.js', args, function(err, res) {
       if (err) {
-        done(err);
-        return;
+        return done(err);
       }
       var pattern = new RegExp('Error', 'g');
       var result = res.output.match(pattern) || [];
@@ -40,12 +38,26 @@ describe('skipped suite w/ callback', function() {
   it('should not throw an error when a callback for skipped suite is supplied', function(done) {
     run('suite/suite-skipped-callback.fixture.js', args, function(err, res) {
       if (err) {
-        done(err);
-        return;
+        return done(err);
       }
       var pattern = new RegExp('Error', 'g');
       var result = res.output.match(pattern) || [];
       assert.equal(result.length, 0);
+      done();
+    });
+  });
+});
+
+describe('suite returning a value', function() {
+  this.timeout(2000);
+  it('should give a deprecation warning for suite callback returning a value', function(done) {
+    run('suite/suite-returning-value.fixture.js', args, function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      var pattern = new RegExp('Deprecation Warning', 'g');
+      var result = res.output.match(pattern) || [];
+      assert.equal(result.length, 1);
       done();
     });
   });
