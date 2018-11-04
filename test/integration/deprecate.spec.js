46971692f7a728ddeba87def4bbe4e4f86199776
Paul Roebuck <plroebuck@users.noreply.github.com>
2018-11-03 20:11:08 -0500

    test(deprecate.spec.js): Make PR requested changes for approval
    Removed `.only` from `describe`. Removed `console.trace` debug output.

--- test/integration/deprecate.spec.js
+++ test/integration/deprecate.spec.js
@@ -4,13 +4,12 @@ var assert = require('assert');
 var run = require('./helpers').runMocha;
 var args = [];
 
-describe.only('utils.deprecate test', function() {
+describe('utils.deprecate test', function() {
   it('should print unique deprecation only once', function(done) {
     run('deprecate.fixture.js', args, function(err, res) {
       if (err) {
         return done(err);
       }
-      console.trace(res);
       var result = res.output.match(/deprecated thing/g) || [];
       assert.equal(result.length, 2);
       done();
