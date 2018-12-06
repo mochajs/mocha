85fcfcffbdac79e45edc4acd5d4ae7e532ae4706
juergba <filodron@gmail.com>
2018-12-06 16:40:24 +0100

    tests

---
+++ test/integration/fixtures/package/subdir1/subdir1-1/package-with-testdir.fixture.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var getTestDirectory = require('../../../../../../bin/options').getTestDirectory;
+
+describe("directories.test of package.json", function() {
+  it('test.directories set to "./subdir1-1"', function() {
+    console.log(`test file found: directory is ${getTestDirectory()}`);
+  });
+});
---
+++ test/integration/package.spec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var spawn = require('child_process').spawnSync;
+var path = require('path');
+var assert = require('assert');
+
+var cmd = 'node ' + path.join(__dirname, '../../bin/mocha');
+
+describe('package.json with directories.test set', function() {
+  var lines;
+  before(function() {
+    lines = spawn(cmd, [], {
+      cwd: path.join(__dirname, './fixtures/package/subdir1'),
+      shell: true
+    });
+  });
+  it('should find test file in subdir1-1', function() {
+    var line = /test file found: .+/.exec(lines.stdout.toString());
+    assert.equal(line, 'test file found: directory is ./subdir1-1');
+  });
+});
+
+describe('package.json without directories.test', function() {
+  var lines;
+  before(function() {
+    lines = spawn(cmd, [], {
+      cwd: path.join(__dirname, './fixtures/package/subdir2'),
+      shell: true
+    });
+  });
+  it('should not find any file', function() {
+    var line = /Warning: Could not find .+/.exec(lines.stderr.toString());
+    assert.equal(
+      line,
+      'Warning: Could not find any test files matching pattern: test'
+    );
+  });
+});
