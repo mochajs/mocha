ee88f5e0682a8e770e333fd400eaf0f3f66dcf89
Juerg B <44573692+juergba@users.noreply.github.com>
2018-12-19 18:51:30 +0100

    Behavior of after/afterEach hooks with --bail flag (#3617)
    * runner.js: delete second end emit
    
    * tests
    
    * documentation
    
    * runner.js
    
    * tests: corrections
    
    closes #3398, closes #3598, closes #3457, closes #3617

--- test/integration/fixtures/options/bail-async.fixture.js
+++ test/integration/fixtures/options/bail-async.fixture.js
@@ -3,21 +3,21 @@
 describe('suite1', function () {
   it('should display this spec', function () {});
 
-  it('should only display this error', function () {
+  it('should only display this error', function (done) {
     throw new Error('this should be displayed');
   });
 
-  it('should not display this error', function () {
+  it('should not display this error', function (done) {
     throw new Error('this should not be displayed');
   });
 });
 
 describe('suite2', function () {
-  before(function () {
+  before(function (done) {
     throw new Error('this hook should not be displayed');
   });
 
-  it('should not display this error', function () {
+  it('should not display this error', function (done) {
     throw new Error('this should not be displayed');
   });
 });
