5a4e23ca489bff946627b9851eb1947421e5b87f
Peter Müller <munter@fumle.dk>
2019-01-06 11:49:58 +0100

    Try/catch the whole test skipping logic to avoid false positive builds, since a throw would result in tests being skipped

--- scripts/test-runner-filter.js
+++ scripts/test-runner-filter.js
@@ -1,40 +1,48 @@
 'use strict';
 
-const minimatch = require('minimatch');
-const childProcess = require('child_process');
-const gitDiffRange = (
-  process.env.TRAVIS_COMMIT_RANGE || 'master..HEAD'
-).replace('...', '..');
-const changedFiles = childProcess
-  .execSync(`git diff --name-only ${gitDiffRange}`, {encoding: 'utf8'})
-  .split('\n')
-  .slice(0, -1);
+try {
+  const minimatch = require('minimatch');
+  const childProcess = require('child_process');
+  const gitDiffRange = (
+    process.env.TRAVIS_COMMIT_RANGE || 'master..HEAD'
+  ).replace('...', '..');
+  const changedFiles = childProcess
+    .execSync(`git diff --name-only ${gitDiffRange}`, {encoding: 'utf8'})
+    .split('\n')
+    .slice(0, -1);
 
-const testRelatedFilePatterns = [
-  'scripts/test-runner-filter.js',
-  'bin/**/*',
-  'lib/**/*',
-  'test/**/*',
-  '.mocharc.yml',
-  '.wallaby.js',
-  'index.js',
-  'package.json',
-  'package-lock.json',
-  'package-scripts.json',
-  '.nycrc',
-  '.travis.yml',
-  'browser-entry.js',
-  'karma.conf.js',
-  'mocha.*'
-];
+  const testRelatedFilePatterns = [
+    'scripts/test-runner-filter.js',
+    'bin/**/*',
+    'lib/**/*',
+    'test/**/*',
+    '.mocharc.yml',
+    '.wallaby.js',
+    'index.js',
+    'package.json',
+    'package-lock.json',
+    'package-scripts.json',
+    '.nycrc',
+    '.travis.yml',
+    'browser-entry.js',
+    'karma.conf.js',
+    'mocha.*'
+  ];
 
-const shouldRunTest = changedFiles.some(filePath =>
-  testRelatedFilePatterns.some(pattern => minimatch(filePath, pattern))
-);
+  const shouldRunTest = changedFiles.some(filePath =>
+    testRelatedFilePatterns.some(pattern => minimatch(filePath, pattern))
+  );
 
-if (!shouldRunTest) {
-  console.log('No library related files have changed. Skipping test');
-  process.exit(1);
+  if (!shouldRunTest) {
+    console.log('No library related files have changed. Skipping test');
+    process.exit(1);
+  }
+
+  console.log('Library related files have changed. Continuing full tests');
+} catch (err) {
+  console.log(
+    'Caught an error while checking git diff against test file patterns:'
+  );
+  console.log(err);
+  console.log('Running the full test suite as a consequence');
 }
-
-console.log('Library related files have changed. Continuing full tests');
