3f4ccac1da91a6cae69f384c72c4c57b7296a793
Christopher Hiller <boneskull@boneskull.com>
2019-02-04 11:51:58 -0800

    Revert 00ca06b0e957ec4f067268c98053782ac5dcb69f; closes #3414
    - Updates events test to match correct event order
    - Add test for retries-and-bail case

--- test/integration/fixtures/runner/events-bail-retries.fixture.js
+++ test/integration/fixtures/runner/events-bail-retries.fixture.js
@@ -2,8 +2,6 @@
 var Runner = require('../../../../lib/runner.js');
 var assert = require('assert');
 var constants = Runner.constants;
-var EVENT_DELAY_BEGIN = constants.EVENT_DELAY_BEGIN;
-var EVENT_DELAY_END = constants.EVENT_DELAY_END;
 var EVENT_HOOK_BEGIN = constants.EVENT_HOOK_BEGIN;
 var EVENT_HOOK_END = constants.EVENT_HOOK_END;
 var EVENT_RUN_BEGIN = constants.EVENT_RUN_BEGIN;
@@ -13,12 +11,10 @@ var EVENT_SUITE_END = constants.EVENT_SUITE_END;
 var EVENT_TEST_BEGIN = constants.EVENT_TEST_BEGIN;
 var EVENT_TEST_END = constants.EVENT_TEST_END;
 var EVENT_TEST_FAIL = constants.EVENT_TEST_FAIL;
-var EVENT_TEST_PASS = constants.EVENT_TEST_PASS;
-var EVENT_TEST_PENDING = constants.EVENT_TEST_PENDING;
 var EVENT_TEST_RETRY = constants.EVENT_TEST_RETRY;
 
 var emitOrder = [
-  EVENT_RUN_BEGIN,
+ EVENT_RUN_BEGIN,
   EVENT_SUITE_BEGIN,
   EVENT_SUITE_BEGIN,
   EVENT_HOOK_BEGIN,
@@ -26,19 +22,16 @@ var emitOrder = [
   EVENT_TEST_BEGIN,
   EVENT_HOOK_BEGIN,
   EVENT_HOOK_END,
-  EVENT_TEST_PASS,
-  EVENT_TEST_END,
+  EVENT_TEST_RETRY,
   EVENT_HOOK_BEGIN,
   EVENT_HOOK_END,
-  EVENT_SUITE_BEGIN,
   EVENT_TEST_BEGIN,
   EVENT_HOOK_BEGIN,
   EVENT_HOOK_END,
-  EVENT_TEST_PASS,
+  EVENT_TEST_FAIL,
   EVENT_TEST_END,
   EVENT_HOOK_BEGIN,
   EVENT_HOOK_END,
-  EVENT_SUITE_END,
   EVENT_HOOK_BEGIN,
   EVENT_HOOK_END,
   EVENT_SUITE_END,
@@ -48,6 +41,7 @@ var emitOrder = [
 
 var realEmit = Runner.prototype.emit;
 Runner.prototype.emit = function(event, ...args) {
+  // console.log(`emit: ${event}`);
   assert.strictEqual(event, emitOrder.shift());
   return realEmit.call(this, event, ...args);
 };
@@ -55,9 +49,8 @@ Runner.prototype.emit = function(event, ...args) {
 describe('suite A', function() {
   before('before', function() {});
   beforeEach('beforeEach', function() {});
-  it('test A', function() {});
-  describe('suite B', function() {
-    it('test B', function() {});
+  it('test A', function() {
+    throw new Error('error test A');
   });
   afterEach('afterEach', function() {});
   after('after', function() {});
