7af00e21c39446a13cc38a5188bf482e322ec768
James Lim <email@wanseob.com>
2019-03-02 02:20:53 +0900

    Remove temp doc in the post-processing step

--- package-scripts.js
+++ package-scripts.js
@@ -277,7 +277,7 @@ module.exports = {
       },
       postbuild: {
         script:
-          'buildProduction docs/_site/index.html --outroot docs/_dist --canonicalroot https://mochajs.org/ --optimizeimages --svgo --inlinehtmlimage 9400 --inlinehtmlscript 0 --asyncscripts && cp docs/_headers docs/_dist/_headers && node scripts/netlify-headers.js >> docs/_dist/_headers',
+          'buildProduction docs/_site/index.html --outroot docs/_dist --canonicalroot https://mochajs.org/ --optimizeimages --svgo --inlinehtmlimage 9400 --inlinehtmlscript 0 --asyncscripts && cp docs/_headers docs/_dist/_headers && node scripts/netlify-headers.js >> docs/_dist/_headers && shx rm docs/index.md.tmp',
         description: 'Post-process docs after build',
         hiddenFromHelp: true
       },
