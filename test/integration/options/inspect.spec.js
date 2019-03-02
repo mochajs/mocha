5e3057333db581d7f06d7d0baac241b57aecb9cc
Craig Taub <craigtaub@gmail.com>
2019-03-02 16:34:12 +0000

    limit to v8+

--- test/integration/options/inspect.spec.js
+++ test/integration/options/inspect.spec.js
@@ -4,17 +4,16 @@ var helpers = require('../helpers');
 var invokeMocha = helpers.invokeMocha;
 var DEFAULT_FIXTURE = helpers.DEFAULT_FIXTURE;
 
-describe('--debug', function() {
+describe('--inspect', function() {
   describe('Node.js v8+', function() {
     before(function() {
       if (process.version.substring(0, 2) === 'v6') {
         this.skip();
       }
     });
-
     it('should invoke --inspect', function(done) {
       invokeMocha(
-        ['--debug', DEFAULT_FIXTURE],
+        ['--inspect', DEFAULT_FIXTURE],
         function(err, res) {
           if (err) {
             return done(err);
@@ -28,7 +27,7 @@ describe('--debug', function() {
 
     it('should invoke --inspect-brk', function(done) {
       var proc = invokeMocha(
-        ['--debug-brk', DEFAULT_FIXTURE],
+        ['--inspect-brk', DEFAULT_FIXTURE],
         function(err, res) {
           if (err) {
             return done(err);
@@ -45,9 +44,9 @@ describe('--debug', function() {
       }, 2000);
     });
 
-    it('should invoke --debug and --debug-brk', function(done) {
+    it('should invoke --inspect and --inspect-brk', function(done) {
       var proc = invokeMocha(
-        ['--debug', '--debug-brk', DEFAULT_FIXTURE],
+        ['--inspect', '--inspect-brk', DEFAULT_FIXTURE],
         function(err, res) {
           if (err) {
             return done(err);
@@ -66,7 +65,7 @@ describe('--debug', function() {
 
     it('should respect custom host/port', function(done) {
       invokeMocha(
-        ['--debug=127.0.0.1:9229', DEFAULT_FIXTURE],
+        ['--inspect=127.0.0.1:9229', DEFAULT_FIXTURE],
         function(err, res) {
           if (err) {
             return done(err);
@@ -83,50 +82,8 @@ describe('--debug', function() {
     });
 
     it('should warn about incorrect usage for version', function(done) {
-      invokeMocha(
-        ['--debug=127.0.0.1:9229', DEFAULT_FIXTURE],
-        function(err, res) {
-          if (err) {
-            return done(err);
-          }
-          expect(res, 'to contain output', /"--debug" is not available/i);
-          done();
-        },
-        'pipe'
-      );
-    });
-  });
-
-  describe('Node.js v6', function() {
-    // note that v6.3.0 and newer supports --inspect but still supports --debug.
-    before(function() {
-      if (process.version.substring(0, 2) !== 'v6') {
-        this.skip();
-      }
-    });
-
-    it('should start debugger', function(done) {
       var proc = invokeMocha(
-        ['--debug', DEFAULT_FIXTURE],
-        function(err, res) {
-          if (err) {
-            return done(err);
-          }
-          expect(res, 'to contain output', /Debugger listening/i);
-          done();
-        },
-        'pipe'
-      );
-
-      // debugger must be manually killed
-      setTimeout(function() {
-        process.kill(proc.pid, 'SIGINT');
-      }, 2000);
-    });
-
-    it('should respect custom host/port', function(done) {
-      var proc = invokeMocha(
-        ['--debug=127.0.0.1:9229', DEFAULT_FIXTURE],
+        ['--inspect', '--inspect-brk', DEFAULT_FIXTURE],
         function(err, res) {
           if (err) {
             return done(err);
@@ -134,13 +91,14 @@ describe('--debug', function() {
           expect(
             res,
             'to contain output',
-            /Debugger listening on .*127.0.0.1:9229/i
+            /"--inspect" and "--inspect-brk" together is redundant/i
           );
           done();
         },
         'pipe'
       );
 
+      // debugger must be manually killed
       setTimeout(function() {
         process.kill(proc.pid, 'SIGINT');
       }, 2000);
