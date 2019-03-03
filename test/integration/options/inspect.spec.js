d83459798816638cdf2e36a284ea2af80060a0c5
Craig Taub <craigtaub@gmail.com>
2019-03-03 19:20:04 +0000

    fix for all node versions

--- test/integration/options/inspect.spec.js
+++ test/integration/options/inspect.spec.js
@@ -5,103 +5,96 @@ var invokeMocha = helpers.invokeMocha;
 var DEFAULT_FIXTURE = helpers.DEFAULT_FIXTURE;
 
 describe('--inspect', function() {
-  describe('Node.js v8+', function() {
-    before(function() {
-      if (process.version.substring(0, 2) === 'v6') {
-        this.skip();
-      }
-    });
-    it('should start debugger from --inspect', function(done) {
-      invokeMocha(
-        ['--inspect', DEFAULT_FIXTURE],
-        function(err, res) {
-          if (err) {
-            return done(err);
-          }
-          expect(res, 'to contain output', /Debugger listening/i);
-          done();
-        },
-        'pipe'
-      );
-    });
+  it('should start debugger from --inspect', function(done) {
+    invokeMocha(
+      ['--inspect', DEFAULT_FIXTURE],
+      function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        expect(res, 'to contain output', /Debugger listening/i);
+        done();
+      },
+      'pipe'
+    );
+  });
 
-    it('should start deubgger from --inspect-brk', function(done) {
-      var proc = invokeMocha(
-        ['--inspect-brk', DEFAULT_FIXTURE],
-        function(err, res) {
-          if (err) {
-            return done(err);
-          }
-          expect(res, 'to contain output', /Debugger listening/i);
-          done();
-        },
-        'pipe'
-      );
+  it('should start deubgger from --inspect-brk', function(done) {
+    var proc = invokeMocha(
+      ['--inspect-brk', DEFAULT_FIXTURE],
+      function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        expect(res, 'to contain output', /Debugger listening/i);
+        done();
+      },
+      'pipe'
+    );
 
-      // debugger must be manually killed
-      setTimeout(function() {
-        process.kill(proc.pid, 'SIGINT');
-      }, 2000);
-    });
+    // debugger must be manually killed
+    setTimeout(function() {
+      process.kill(proc.pid, 'SIGINT');
+    }, 2000);
+  });
 
-    it('should start debugger if supply both --inspect and --inspect-brk', function(done) {
-      var proc = invokeMocha(
-        ['--inspect', '--inspect-brk', DEFAULT_FIXTURE],
-        function(err, res) {
-          if (err) {
-            return done(err);
-          }
-          expect(res, 'to contain output', /Debugger listening/i);
-          done();
-        },
-        'pipe'
-      );
+  it('should start debugger if supply both --inspect and --inspect-brk', function(done) {
+    var proc = invokeMocha(
+      ['--inspect', '--inspect-brk', DEFAULT_FIXTURE],
+      function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        expect(res, 'to contain output', /Debugger listening/i);
+        done();
+      },
+      'pipe'
+    );
 
-      // debugger must be manually killed
-      setTimeout(function() {
-        process.kill(proc.pid, 'SIGINT');
-      }, 2000);
-    });
+    // debugger must be manually killed
+    setTimeout(function() {
+      process.kill(proc.pid, 'SIGINT');
+    }, 2000);
+  });
 
-    it('should respect custom host/port', function(done) {
-      invokeMocha(
-        ['--inspect=127.0.0.1:9229', DEFAULT_FIXTURE],
-        function(err, res) {
-          if (err) {
-            return done(err);
-          }
-          expect(
-            res,
-            'to contain output',
-            /Debugger listening on .*127.0.0.1:9229/i
-          );
-          done();
-        },
-        'pipe'
-      );
-    });
+  it('should respect custom host/port', function(done) {
+    invokeMocha(
+      ['--inspect=127.0.0.1:9229', DEFAULT_FIXTURE],
+      function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        expect(
+          res,
+          'to contain output',
+          /Debugger listening on .*127.0.0.1:9229/i
+        );
+        done();
+      },
+      'pipe'
+    );
+  });
 
-    it('should warn about incorrect usage', function(done) {
-      var proc = invokeMocha(
-        ['--inspect', '--inspect-brk', DEFAULT_FIXTURE],
-        function(err, res) {
-          if (err) {
-            return done(err);
-          }
-          expect(
-            res,
-            'to contain output',
-            /"--inspect" and "--inspect-brk" together is redundant/i
-          );
-          done();
-        },
-        'pipe'
-      );
+  it('should warn about incorrect usage', function(done) {
+    var proc = invokeMocha(
+      ['--inspect', '--inspect-brk', DEFAULT_FIXTURE],
+      function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        expect(
+          res,
+          'to contain output',
+          /"--inspect" and "--inspect-brk" together is redundant/i
+        );
+        done();
+      },
+      'pipe'
+    );
 
-      // debugger must be manually killed
-      setTimeout(function() {
-        process.kill(proc.pid, 'SIGINT');
-      }, 2000);
-    });
+    // debugger must be manually killed
+    setTimeout(function() {
+      process.kill(proc.pid, 'SIGINT');
+    }, 2000);
   });
 });
