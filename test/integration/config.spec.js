e34484653c34e5e5b2fb7959a4e98788c41024a1
Paul Roebuck <plroebuck@users.noreply.github.com>
2019-03-17 09:49:33 -0500

    test(integration/config.spec.js): Add tests for configuring with a package
    As I used symlinks, some tests will likely fail on Windows...

--- test/integration/config.spec.js
+++ test/integration/config.spec.js
@@ -18,7 +18,7 @@ describe('config', function() {
   });
 
   /**
-   * @returns {String} pathname to project root directory
+   * @returns {String} pathname to Mocha project root directory
    */
   function getProjectRootDir() {
     var searchPaths = module.parent.paths;
@@ -30,18 +30,72 @@ describe('config', function() {
     }
   }
 
-  it('should load a ".js" config file given relative path', function() {
-    var projRootDir = getProjectRootDir();
+  describe('when configuring Mocha via a ".js" file', function() {
     var configDir = path.join(__dirname, 'fixtures', 'config');
-    var relConfigDir = configDir.substring(projRootDir.length + 1);
-    var js;
-
-    function _loadConfig() {
-      js = loadConfig(path.join(relConfigDir, 'mocharc.js'));
-    }
-
-    expect(_loadConfig, 'not to throw');
     var json = loadConfig(path.join(configDir, 'mocharc.json'));
-    expect(js, 'to equal', json);
+
+    it('should load configuration given absolute path', function() {
+      var js;
+
+      function _loadConfig() {
+        js = loadConfig(path.join(configDir, 'mocharc.js'));
+      }
+
+      expect(_loadConfig, 'not to throw');
+      expect(js, 'to equal', json);
+    });
+
+    it('should load configuration given relative path', function() {
+      var projRootDir = getProjectRootDir();
+      var relConfigDir = configDir.substring(projRootDir.length + 1);
+      var js;
+
+      function _loadConfig() {
+        js = loadConfig(path.join('.', relConfigDir, 'mocharc.js'));
+      }
+
+      expect(_loadConfig, 'not to throw');
+      expect(js, 'to equal', json);
+    });
+  });
+
+  describe('when configuring Mocha via package', function() {
+    var projRootDir = getProjectRootDir();
+    var configDir = path.join('fixtures', 'config');
+    var pkgName = 'mocha-config';
+    var pkgDir = path.join(__dirname, configDir, pkgName);
+
+    before(function() {
+      fs.symlinkSync(
+        pkgDir,
+        path.join(projRootDir, 'node_modules', pkgName),
+        'dir'
+      );
+    });
+
+    it('should load configuration given valid package name', function() {
+      var configDir = path.join(__dirname, 'fixtures', 'config');
+      var js;
+
+      function _loadConfig() {
+        js = loadConfig(pkgName);
+      }
+
+      expect(_loadConfig, 'not to throw');
+      var json = loadConfig(path.join(configDir, 'mocharc.json'));
+      expect(js, 'to equal', json);
+    });
+
+    it('should throw given invalid package name', function() {
+      function _loadConfig() {
+        loadConfig(pkgName.toUpperCase());
+      }
+
+      expect(_loadConfig, 'to throw');
+    });
+
+    after(function() {
+      fs.unlinkSync(path.join(projRootDir, 'node_modules', pkgName));
+    });
   });
 });
