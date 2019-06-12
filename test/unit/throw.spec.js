56e2232bc43aaa9a9feaea5921fae860db0d3b3a
Peter Rust <peter@cornerstonenw.com>
2019-06-12 12:06:47 -0700

    implemented test in throw.spec.js & got it passing

--- test/unit/throw.spec.js
+++ test/unit/throw.spec.js
@@ -2,6 +2,7 @@
 
 /* eslint no-throw-literal: off */
 
+var sinon = require('sinon');
 var Mocha = require('../../lib/mocha');
 var Suite = Mocha.Suite;
 var Test = Mocha.Test;
@@ -13,11 +14,13 @@ var STATE_FAILED = Runnable.constants.STATE_FAILED;
 describe('a test that throws', function() {
   var suite;
   var runner;
+  var sandbox;
   var uncaughtHandlers;
 
   beforeEach(function() {
     suite = new Suite('Suite', 'root');
     runner = new Runner(suite);
+    sandbox = sinon.createSandbox();
 
     // see https://github.com/mochajs/mocha/pull/2983#issuecomment-350428522
     uncaughtHandlers = process.listeners('uncaughtException') || [];
@@ -164,6 +167,37 @@ describe('a test that throws', function() {
       runner.on(EVENT_RUN_END, function() {
         expect(runner.failures, 'to be', 1);
         expect(test.state, 'to be', STATE_FAILED);
+        done();
+      });
+      runner.run();
+    });
+  });
+
+  describe('stack', function() {
+    it('should include the stack when throwing async', function(done) {
+      var test = new Test('im async and throw null async', function(done2) {
+        process.nextTick(function throwError() {
+          throw new Error('test error');
+        });
+      });
+      suite.addTest(test);
+      runner = new Runner(suite);
+      sandbox.stub(runner, 'fail');
+
+      runner.on(EVENT_RUN_END, function() {
+        try {
+          expect(runner.fail, 'to have all calls satisfying', [
+            expect.it('to be a', Runnable),
+            expect.it('to be an', Error).and('to satisfy', {
+              message: /test error/i,
+              stack: /throwError/i,
+              uncaught: true
+            })
+          ]).and('was called once');
+        } catch (err) {
+          return done(err);
+        }
+
         done();
       });
       runner.run();
