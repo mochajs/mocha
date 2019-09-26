9caa3183755f49b89d4380026eab8fc2d585e22c
Juerg B <44573692+juergba@users.noreply.github.com>
2019-09-26 07:32:14 +0200

    uncaughtException: fix double EVENT_RUN_END events (#4025)
    abort runner instead of end event

--- test/integration/fixtures/uncaught-fatal.fixture.js
+++ test/integration/fixtures/uncaught-fatal.fixture.js
@@ -1,11 +1,22 @@
 'use strict';
 
-it('should bail if a successful test asynchronously fails', function(done) {
-  done();
-  process.nextTick(function () {
-    throw new Error('global error');
-  });
-});
+describe('fatal uncaught exception', function () {
+  describe('first suite', function () {
+    it('should bail if a successful test asynchronously fails', function (done) {
+      done();
+      process.nextTick(function () {
+        throw new Error('global error');
+      });
+    });
 
-it('should not actually get run', function () {
+    it('should not actually get run', function () {
+      throw new Error('should never throw - first suite');
+    });
+  });
+  
+  describe('second suite', function () {
+    it('should not actually get run', function () {
+      throw new Error('should never throw - second suite');
+    });
+  });
 });
