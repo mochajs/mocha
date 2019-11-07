f0110e495b0bcc0031520d4e60334087fd82b3b6
juhongchul <jhc9639@naver.com>
2019-11-07 14:16:27 +0900

    Clean up files in the after test whether test is success or not, the files in the migrateOpts testing is will be removed.`

--- scripts/migrate-opts.js
+++ scripts/migrate-opts.js
@@ -14,37 +14,37 @@ const jsonfile = require('jsonfile');
 const beautify = require('js-beautify').js;
 const fs = require('fs');
 const loadMochaOpts = require('../lib/cli/options.js').loadMochaOpts;
-exports.setJsType = content =>
+const setJsType = content =>
   beautify(`(module.exports = ${JSON.stringify(content)})`);
 const setYamlType = content => YAML.stringify(content);
-exports.writeFile = {
-  yaml: (content, _path = process.cwd()) =>
+const writeFile = {
+  yaml: (content, _path) =>
     fs.writeFileSync(path.join(_path, `.mocharc.yaml`), setYamlType(content)),
-  yml: (content, _path = process.cwd()) =>
+  yml: (content, _path) =>
     fs.writeFileSync(path.join(_path, `.mocharc.yml`), setYamlType(content)),
-  js: (content, _path = process.cwd()) =>
-    fs.writeFileSync(path.join(_path, `.mocharc.js`), this.setJsType(content)),
-  json: (content, _path = process.cwd()) =>
+  js: (content, _path) =>
+    fs.writeFileSync(path.join(_path, `.mocharc.js`), setJsType(content)),
+  json: (content, _path) =>
     jsonfile.writeFileSync(path.join(_path, `.mocharc.json`), content, {
       spaces: 1
     })
 };
-exports.writeConfig = (type, content) => {
+const writeConfig = (type, content, _path) => {
   if (type === 'yaml') {
-    this.writeFile.yaml(content);
+    writeFile.yaml(content, _path);
   } else if (type === 'yml') {
-    this.writeFile.yml(content);
+    writeFile.yml(content, _path);
   } else if (type === 'js') {
-    this.writeFile.js(content);
+    writeFile.js(content, _path);
   } else {
-    this.writeFile.json(content);
+    writeFile.json(content, _path);
   }
   return content;
 };
 
-const init = (filepath, type) => {
+exports.init = (filepath, type, _path = process.cwd()) => {
   const content = loadMochaOpts({opts: filepath});
-  this.writeConfig(type, content);
+  writeConfig(type, content, _path);
 };
 exports.command = 'migrate-opts';
 
@@ -55,5 +55,5 @@ exports.builder = yargs => yargs.option('file').option('type');
 exports.handler = argv => {
   const filepath = path.join(process.cwd(), argv.file[0]);
   const type = argv.type;
-  init(filepath, type);
+  this.init(filepath, type);
 };
--- test/migrate-opts/migrate-opts.spec.js
+++ test/migrate-opts/migrate-opts.spec.js
@@ -1,85 +1,35 @@
 'use strict';
-const fs = require('fs');
-const path = require('path');
-const {exec} = require('child_process');
-const {expect} = require('chai');
-const migrateOpts = require('../../scripts/migrate-opts');
-const command =
-  'node ./bin/mocha migrate-opts -file ./test/migrate-opts/mocha.opts -type json';
-const _path = path.join(process.cwd(), '.mocharc.json');
-const isFileCreate = path => {
-  return new Promise((resolve, reject) => {
-    exec(command, err => {
-      if (err) reject(err);
-      resolve(fs.existsSync(path));
+var fs = require('fs');
+var path = require('path');
+var expect = require('chai').expect;
+var migrateOpts = require('../../scripts/migrate-opts');
+var types = ['json', 'js', 'yml', 'yaml'];
+
+describe('Test migrate-opts.js script', function() {
+  types.forEach(function(type) {
+    it('should be worked writeFile ' + type, function() {
+      var filepath = path.join(process.cwd(), './test/migrate-opts/mocha.opts');
+      var _path = path.join(process.cwd(), './test/migrate-opts');
+      migrateOpts.init(filepath, type, _path);
+      var __path = path.join(
+        process.cwd(),
+        './test/migrate-opts',
+        '.mocharc.' + type
+      );
+      var isFile = fs.existsSync(__path);
+      expect(isFile).to.equal(true);
+      if (isFile) fs.unlinkSync(__path);
     });
   });
-};
-
-const parsedOpts = {
-  reporter: 'dot'
-};
-describe('Test migrate-opts.js script', () => {
-  it('command should be create file', async () => {
-    const isFile = await isFileCreate(_path);
-    expect(isFile).to.equal(true);
-    fs.unlinkSync(_path);
-  });
-
-  it('should be worked writeFile.yaml', async () => {
-    const _path = path.join(process.cwd(), './test/migrate-opts');
-    migrateOpts.writeFile.yaml(parsedOpts, _path);
-    const __path = path.join(
-      process.cwd(),
-      './test/migrate-opts',
-      '.mocharc.yaml'
-    );
-    const isFile = fs.existsSync(__path);
-    expect(isFile).to.equal(true);
-    fs.unlinkSync(__path);
-  });
-
-  it('should be worked writeFile.yml', async () => {
-    const _path = path.join(process.cwd(), './test/migrate-opts');
-    migrateOpts.writeFile.yml(parsedOpts, _path);
-    const __path = path.join(
-      process.cwd(),
-      './test/migrate-opts',
-      '.mocharc.yml'
-    );
-    const isFile = fs.existsSync(__path);
-    expect(isFile).to.equal(true);
-    fs.unlinkSync(__path);
-  });
-
-  it('should be worked writeFile.json', async () => {
-    const _path = path.join(process.cwd(), './test/migrate-opts');
-    migrateOpts.writeFile.json(parsedOpts, _path);
-    const __path = path.join(
-      process.cwd(),
-      './test/migrate-opts',
-      '.mocharc.json'
-    );
-    const isFile = fs.existsSync(__path);
-    expect(isFile).to.equal(true);
-    fs.unlinkSync(__path);
-  });
-
-  it('should be worked writeFile.js', async () => {
-    const _path = path.join(process.cwd(), './test/migrate-opts');
-    migrateOpts.writeFile.js(parsedOpts, _path);
-    const __path = path.join(
-      process.cwd(),
-      './test/migrate-opts',
-      '.mocharc.js'
-    );
-    const isFile = fs.existsSync(__path);
-    expect(isFile).to.equal(true);
-    fs.unlinkSync(__path);
-  });
-
-  it('should be worked setJsType', async () => {
-    const ret = migrateOpts.setJsType(parsedOpts).toString();
-    expect(ret).to.include('reporter');
+  after(function() {
+    types.forEach(function(type) {
+      var __path = path.join(
+        process.cwd(),
+        './test/migrate-opts',
+        '.mocharc.' + type
+      );
+      var isFile = fs.existsSync(__path);
+      if (isFile) fs.unlinkSync(__path);
+    });
   });
 });
