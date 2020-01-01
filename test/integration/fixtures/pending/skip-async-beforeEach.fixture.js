24c22bef53e4539dd17b0d3b2123953bb8a3a883
Juerg B <44573692+juergba@users.noreply.github.com>
2020-01-01 09:36:31 +0100

    Fix hook pattern of this.skip() in beforeEach hooks (#3741)

--- test/integration/fixtures/pending/skip-async-beforeEach.fixture.js
+++ test/integration/fixtures/pending/skip-async-beforeEach.fixture.js
@@ -1,20 +1,48 @@
 'use strict';
+var assert = require('assert');
 
-describe('skip in beforeEach', function () {
-  beforeEach(function (done) {
+describe('skip in beforeEach', function() {
+  var runOrder = [];
+  beforeEach(function(done) {
+    runOrder.push('beforeEach');
     var self = this;
-    setTimeout(function () {
+    setTimeout(function() {
       self.skip();   // done() is not required
-    }, 0);
+    }, 10);
   });
 
-  it('should skip this test-1', function () {
+  it('should skip this test-1', function() {
     throw new Error('never run this test');
   });
-  it('should skip this test-2', function () {
-    throw new Error('never run this test');
+
+  describe('inner', function() {
+    beforeEach(function() {
+      runOrder.push('should not run');
+    });
+
+    it('should skip this test-2', function() {
+      throw new Error('never run this test');
+    });
+    it('should skip this test-3', function() {
+      throw new Error('never run this test');
+    });
+
+    afterEach(function() {
+      runOrder.push('should not run');
+    });
   });
-  it('should skip this test-3', function () {
-    throw new Error('never run this test');
+
+  afterEach(function() {
+    runOrder.push('afterEach');
+  });
+  after(function() {
+    runOrder.push('after');
+    assert.deepStrictEqual(runOrder, [
+      'beforeEach', 'afterEach',
+      'beforeEach', 'afterEach',
+      'beforeEach', 'afterEach',
+      'after'
+    ]);
+    throw new Error('should throw this error');
   });
 });
