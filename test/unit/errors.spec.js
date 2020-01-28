731e3c6c17dba778ec72623ca1cb9788ae72ce6d
naort <naort@voyagerlabs.co>
2020-01-28 14:31:16 +0200

    add unit tests to errors.js

--- test/unit/errors.spec.js
+++ test/unit/errors.spec.js
@@ -15,6 +15,32 @@ describe('Errors', function() {
     });
   });
 
+  var missingFile = ['not-exist'];
+  var expectedErrorMessage = 'files not exists' + missingFile;
+  it('should include expected code in thrown not existing errors', function() {
+    var throwError = function() {
+      throw errors.createFileNotExistsError(expectedErrorMessage);
+    };
+    expect(throwError, 'to throw', {
+      message: expectedErrorMessage,
+      code: 'ERR_MOCHA_FILE_NOT_FOUND'
+    });
+  });
+
+  var invalidExceptionErrorMessage = 'invalid message';
+  it('should include expected code in thrown not existing errors', function() {
+    var throwError = function() {
+      throw errors.createInvalidExceptionError(
+        invalidExceptionErrorMessage,
+        []
+      );
+    };
+    expect(throwError, 'to throw', {
+      message: invalidExceptionErrorMessage,
+      code: 'ERR_MOCHA_INVALID_EXCEPTION'
+    });
+  });
+
   it('should include expected code in thrown interface errors', function() {
     var throwError = function() {
       throw errors.createInvalidInterfaceError(expectedMessage, 'badUi');
