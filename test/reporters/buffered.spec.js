5092f08521a36f16993eacb8d1a7a216654412cc
Christopher Hiller <boneskull@boneskull.com>
2020-03-25 17:38:20 -0700

    add test for Buffered reporter

---
+++ test/reporters/buffered.spec.js
@@ -0,0 +1,232 @@
+'use strict';
+
+// this reporter does not actually output anything to the terminal, so we
+// need to test it differently.
+
+const {
+  EVENT_SUITE_BEGIN,
+  EVENT_SUITE_END,
+  EVENT_TEST_FAIL,
+  EVENT_TEST_PASS,
+  EVENT_TEST_PENDING,
+  EVENT_TEST_BEGIN,
+  EVENT_TEST_END,
+  EVENT_TEST_RETRY,
+  EVENT_DELAY_BEGIN,
+  EVENT_DELAY_END,
+  EVENT_HOOK_BEGIN,
+  EVENT_HOOK_END,
+  EVENT_RUN_END
+} = require('../../lib/runner').constants;
+const {EventEmitter} = require('events');
+const {createSandbox} = require('sinon');
+const rewiremock = require('rewiremock/node');
+
+describe('Buffered', function() {
+  let sandbox;
+  let runner;
+  let Buffered;
+
+  beforeEach(function() {
+    sandbox = createSandbox();
+    runner = new EventEmitter();
+    Buffered = rewiremock.proxy(
+      require.resolve('../../lib/reporters/buffered.js'),
+      {
+        '../../lib/serializer': {
+          SerializableEvent: {
+            create: (eventName, runnable, err) => ({
+              eventName,
+              data: runnable,
+              error: err,
+              __type: 'MockSerializableEvent'
+            })
+          },
+          SerializableWorkerResult: {
+            create: (events, failures) => ({
+              events,
+              failures,
+              __type: 'MockSerializableWorkerResult'
+            })
+          }
+        },
+        '../../lib/reporters/base': class MockBase {}
+      }
+    );
+    rewiremock.enable();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+    rewiremock.disable();
+  });
+
+  describe('constructor', function() {
+    it('should listen for Runner events', function() {
+      // EventEmitter#once calls thru to EventEmitter#on, which
+      // befouls our assertion below.
+      sandbox.stub(runner, 'once');
+      sandbox.stub(runner, 'on');
+      // eslint-disable-next-line no-new
+      new Buffered(runner);
+      expect(runner.on, 'to have calls satisfying', [
+        // via Buffered
+        [EVENT_SUITE_BEGIN, expect.it('to be a function')],
+        [EVENT_SUITE_END, expect.it('to be a function')],
+        [EVENT_TEST_BEGIN, expect.it('to be a function')],
+        [EVENT_TEST_PENDING, expect.it('to be a function')],
+        [EVENT_TEST_FAIL, expect.it('to be a function')],
+        [EVENT_TEST_PASS, expect.it('to be a function')],
+        [EVENT_TEST_RETRY, expect.it('to be a function')],
+        [EVENT_TEST_END, expect.it('to be a function')],
+        [EVENT_HOOK_BEGIN, expect.it('to be a function')],
+        [EVENT_HOOK_END, expect.it('to be a function')]
+      ]);
+    });
+
+    it('should listen for Runner events expecting to occur once', function() {
+      sandbox.stub(runner, 'once');
+      // eslint-disable-next-line no-new
+      new Buffered(runner);
+      expect(runner.once, 'to have calls satisfying', [
+        [EVENT_DELAY_BEGIN, expect.it('to be a function')],
+        [EVENT_DELAY_END, expect.it('to be a function')],
+        [EVENT_RUN_END, expect.it('to be a function')]
+      ]);
+    });
+  });
+
+  describe('event', function() {
+    let reporter;
+
+    beforeEach(function() {
+      reporter = new Buffered(runner);
+    });
+
+    describe('on EVENT_RUN_END', function() {
+      it('should remove all listeners', function() {
+        runner.emit(EVENT_RUN_END);
+        expect(runner.listeners(), 'to be empty');
+      });
+    });
+
+    describe('on any other event listened for', function() {
+      it('should populate its `events` array with SerializableEvents', function() {
+        const suite = {
+          title: 'some suite'
+        };
+        const test = {
+          title: 'some test'
+        };
+        runner.emit(EVENT_SUITE_BEGIN, suite);
+        runner.emit(EVENT_TEST_BEGIN, test);
+        runner.emit(EVENT_TEST_PASS, test);
+        runner.emit(EVENT_TEST_END, test);
+        runner.emit(EVENT_SUITE_END, suite);
+        expect(reporter.events, 'to equal', [
+          {
+            eventName: EVENT_SUITE_BEGIN,
+            data: suite,
+            __type: 'MockSerializableEvent'
+          },
+          {
+            eventName: EVENT_TEST_BEGIN,
+            data: test,
+            __type: 'MockSerializableEvent'
+          },
+          {
+            eventName: EVENT_TEST_PASS,
+            data: test,
+            __type: 'MockSerializableEvent'
+          },
+          {
+            eventName: EVENT_TEST_END,
+            data: test,
+            __type: 'MockSerializableEvent'
+          },
+          {
+            eventName: EVENT_SUITE_END,
+            data: suite,
+            __type: 'MockSerializableEvent'
+          }
+        ]);
+      });
+    });
+  });
+
+  describe('instance method', function() {
+    let reporter;
+
+    beforeEach(function() {
+      reporter = new Buffered(runner);
+    });
+
+    describe('done', function() {
+      it('should execute its callback with a SerializableWorkerResult', function() {
+        const suite = {
+          title: 'some suite'
+        };
+        const test = {
+          title: 'some test'
+        };
+        runner.emit(EVENT_SUITE_BEGIN, suite);
+        runner.emit(EVENT_TEST_BEGIN, test);
+        runner.emit(EVENT_TEST_PASS, test);
+        runner.emit(EVENT_TEST_END, test);
+        runner.emit(EVENT_SUITE_END, suite);
+        const cb = sandbox.stub();
+        reporter.done(0, cb);
+        expect(cb, 'to have a call satisfying', [
+          {
+            events: [
+              {
+                eventName: EVENT_SUITE_BEGIN,
+                data: suite,
+                __type: 'MockSerializableEvent'
+              },
+              {
+                eventName: EVENT_TEST_BEGIN,
+                data: test,
+                __type: 'MockSerializableEvent'
+              },
+              {
+                eventName: EVENT_TEST_PASS,
+                data: test,
+                __type: 'MockSerializableEvent'
+              },
+              {
+                eventName: EVENT_TEST_END,
+                data: test,
+                __type: 'MockSerializableEvent'
+              },
+              {
+                eventName: EVENT_SUITE_END,
+                data: suite,
+                __type: 'MockSerializableEvent'
+              }
+            ],
+            failures: 0,
+            __type: 'MockSerializableWorkerResult'
+          }
+        ]);
+      });
+
+      it('should reset its `events` prop', function() {
+        const suite = {
+          title: 'some suite'
+        };
+        const test = {
+          title: 'some test'
+        };
+        runner.emit(EVENT_SUITE_BEGIN, suite);
+        runner.emit(EVENT_TEST_BEGIN, test);
+        runner.emit(EVENT_TEST_PASS, test);
+        runner.emit(EVENT_TEST_END, test);
+        runner.emit(EVENT_SUITE_END, suite);
+        const cb = sandbox.stub();
+        reporter.done(0, cb);
+        expect(reporter.events, 'to be empty');
+      });
+    });
+  });
+});
