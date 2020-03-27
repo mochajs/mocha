9a2a81650d02c6f5e12d0e65eaa92f0017031f99
Christopher Hiller <boneskull@boneskull.com>
2020-03-26 17:30:44 -0700

    add tests for --parallel with --retries and --allow-uncaught

--- test/integration/options/parallel.spec.js
+++ test/integration/options/parallel.spec.js
@@ -2,23 +2,55 @@
 
 var path = require('path');
 var helpers = require('../helpers');
-var runMochaJSON = helpers.runMochaJSON;
+var runMochaAsync = helpers.runMochaAsync;
+var invokeMochaAsync = helpers.invokeMochaAsync;
 
 describe('--parallel', function() {
-  it('should not appear fundamentally different than without', function(done) {
-    runMochaJSON(
-      path.join('options', 'parallel', '*.fixture.js'),
-      ['--parallel'],
-      function(err, res) {
-        if (err) {
-          return done(err);
-        }
-        expect(res, 'to have failed')
-          .and('to have passed test count', 2)
-          .and('to have pending test count', 1)
-          .and('to have failed test count', 2);
-        done();
-      }
-    );
+  it('should not appear fundamentally different than without', function() {
+    return expect(
+      runMochaAsync(path.join('options', 'parallel', 'test-*.fixture.js'), [
+        '--parallel'
+      ]),
+      'when fulfilled',
+      'to have failed'
+    )
+      .and('when fulfilled', 'to have passed test count', 2)
+      .and('when fulfilled', 'to have pending test count', 1)
+      .and('when fulfilled', 'to have failed test count', 2);
+  });
+
+  describe('when used with --retries', function() {
+    it('should retry tests appropriately', function() {
+      return expect(
+        runMochaAsync(
+          path.join('options', 'parallel', 'retries-*.fixture.js'),
+          ['--parallel']
+        ),
+        'when fulfilled',
+        'to have failed'
+      )
+        .and('when fulfilled', 'to have passed test count', 1)
+        .and('when fulfilled', 'to have pending test count', 0)
+        .and('when fulfilled', 'to have failed test count', 1)
+        .and('when fulfilled', 'to contain output', /count: 3/);
+    });
+  });
+
+  describe('when used with --allow-uncaught', function() {
+    it('should bubble up an exception', function() {
+      return expect(
+        invokeMochaAsync(
+          [
+            require.resolve('../fixtures/options/parallel/uncaught.fixture.js'),
+            '--parallel',
+            '--allow-uncaught'
+          ],
+          'pipe'
+        )[1],
+        'when fulfilled',
+        'to contain output',
+        /Error: existential isolation/i
+      ).and('when fulfilled', 'to have exit code', 1);
+    });
   });
 });
