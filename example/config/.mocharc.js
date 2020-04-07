491c6bc747161a35d8949f5be22d6a49370d5deb
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2020-04-07 19:56:43 +0200

    Add reporter options example
    I tried to add reporter options as `reporterOptions` or `reporterOption` and failed miserably for quite a while until I accidentally found out that it needs to be specified like in the example below. I think it's worth adding an example to the documentation to save other people's time.

--- example/config/.mocharc.js
+++ example/config/.mocharc.js
@@ -1,17 +1,47 @@
 'use strict';
 
-// Here's a JavaScript-based config file.
+// This is a JavaScript-based config file containing every Mocha option plus others.
 // If you need conditional logic, you might want to use this type of config.
 // Otherwise, JSON or YAML is recommended.
 
 module.exports = {
+  'allow-uncaught': false,
+  'async-only': false,
+  bail: false,
+  'check-leaks': false,
+  color: true,
+  delay: false,
   diff: true,
+  exit: false, // could be expressed as "'no-exit': true"
   extension: ['js'],
+  // fgrep: something, // fgrep and grep are mutually exclusive
+  file: ['/path/to/some/file', '/path/to/some/other/file'],
+  'forbid-only': false,
+  'forbid-pending': false,
+  'full-trace': false,
+  global: ['jQuery', '$'],
+  // grep: something, // fgrep and grep are mutually exclusive
+  growl: false,
+  ignore: ['/path/to/some/ignored/file'],
+  'inline-diffs': false,
+  // invert: false, // needs to be used with grep or fgrep
+  jobs: 1,
   package: './package.json',
+  parallel: false,
+  recursive: false,
   reporter: 'spec',
-  slow: 75,
-  timeout: 2000,
+  'reporter-option': ['foo=bar', 'baz=quux'],
+  require: '@babel/register',
+  retries: 1,
+  slow: '75',
+  sort: false,
+  spec: ['test/**/*.spec.js'], // the positional arguments!
+  timeout: '2000', // same as "timeout: '2s'"
+  // timeout: false, // same as "'no-timeout': true" or "timeout: 0"
+  'trace-warnings': true, // node flags ok
   ui: 'bdd',
+  'v8-stack-trace-limit': 100, // V8 flags are prepended with "v8-"
+  watch: false,
   'watch-files': ['lib/**/*.js', 'test/**/*.js'],
   'watch-ignore': ['lib/vendor']
 };
