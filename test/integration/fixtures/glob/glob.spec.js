b747c356de2fde7746050113ac9b456dc0105d5b
juergba <filodron@gmail.com>
2020-04-09 15:51:04 +0200

    adapt existing tests

--- test/integration/fixtures/glob/glob.spec.js
+++ test/integration/fixtures/glob/glob.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('globbing test', function() {
-  it('should find this test', function() {
+  it('should find this glob/test', function() {
     // see test/integration/glob.spec.js for details
   });
 });
