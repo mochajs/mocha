400bf9b9319bdf8f0d9aa05ff8c7a54f6fe65d2a
Christopher Hiller <boneskull@boneskull.com>
2020-04-21 11:58:56 -0700

    refactor validatePlugins to throw coded errors
    - add `createInvalidPluginError` for reporters, UIs, and future plugins
    - ensures the original error is output if the module exists, but it throws (see `test/node-unit/cli/fixtures/bad-module.fixture.js`)
    - remove unneeded `process.cwd()` from call to `path.resolve()`
    
    Ref: #4198

--- test/node-unit/cli/run-helpers.spec.js
+++ test/node-unit/cli/run-helpers.spec.js
@@ -1,49 +1,96 @@
 'use strict';
 
 const {validatePlugin, list} = require('../../../lib/cli/run-helpers');
-const {createSandbox} = require('sinon');
 
-describe('cli "run" command', function() {
-  let sandbox;
-
-  beforeEach(function() {
-    sandbox = createSandbox();
-  });
-
-  afterEach(function() {
-    sandbox.restore();
-  });
-
-  describe('helpers', function() {
-    describe('validatePlugin()', function() {
-      it('should disallow an array of module names', function() {
+describe('run helper functions', function() {
+  describe('validatePlugin()', function() {
+    describe('when used with "reporter" key', function() {
+      it('should disallow an array of names', function() {
         expect(
-          () => validatePlugin({foo: ['bar']}, 'foo'),
-          'to throw a',
-          TypeError
+          () => validatePlugin({reporter: ['bar']}, 'reporter'),
+          'to throw',
+          {
+            code: 'ERR_MOCHA_INVALID_REPORTER',
+            message: /can only be specified once/i
+          }
+        );
+      });
+
+      it('should fail to recognize an unknown reporter', function() {
+        expect(
+          () => validatePlugin({reporter: 'bar'}, 'reporter'),
+          'to throw',
+          {code: 'ERR_MOCHA_INVALID_REPORTER', message: /cannot find module/i}
         );
       });
     });
 
-    describe('list()', function() {
-      describe('when provided a flat array', function() {
-        it('should return a flat array', function() {
-          expect(list(['foo', 'bar']), 'to equal', ['foo', 'bar']);
-        });
+    describe('when used with an "interfaces" key', function() {
+      it('should disallow an array of names', function() {
+        expect(
+          () => validatePlugin({interface: ['bar']}, 'interface'),
+          'to throw',
+          {
+            code: 'ERR_MOCHA_INVALID_INTERFACE',
+            message: /can only be specified once/i
+          }
+        );
       });
-      describe('when provided a nested array', function() {
-        it('should return a flat array', function() {
-          expect(list([['foo', 'bar'], 'baz']), 'to equal', [
-            'foo',
-            'bar',
-            'baz'
-          ]);
-        });
+
+      it('should fail to recognize an unknown interface', function() {
+        expect(
+          () => validatePlugin({interface: 'bar'}, 'interface'),
+          'to throw',
+          {code: 'ERR_MOCHA_INVALID_INTERFACE', message: /cannot find module/i}
+        );
       });
-      describe('when given a comma-delimited string', function() {
-        it('should return a flat array', function() {
-          expect(list('foo,bar'), 'to equal', ['foo', 'bar']);
-        });
+    });
+
+    describe('when used with an unknown plugin type', function() {
+      it('should fail', function() {
+        expect(
+          () => validatePlugin({frog: 'bar'}, 'frog'),
+          'to throw',
+          /unknown plugin/i
+        );
+      });
+    });
+
+    describe('when a plugin throws an exception upon load', function() {
+      it('should fail and report the original error', function() {
+        expect(
+          () =>
+            validatePlugin(
+              {
+                reporter: require.resolve('./fixtures/bad-module.fixture.js')
+              },
+              'reporter'
+            ),
+          'to throw',
+          {message: /wonky/, code: 'ERR_MOCHA_INVALID_REPORTER'}
+        );
+      });
+    });
+  });
+
+  describe('list()', function() {
+    describe('when provided a flat array', function() {
+      it('should return a flat array', function() {
+        expect(list(['foo', 'bar']), 'to equal', ['foo', 'bar']);
+      });
+    });
+    describe('when provided a nested array', function() {
+      it('should return a flat array', function() {
+        expect(list([['foo', 'bar'], 'baz']), 'to equal', [
+          'foo',
+          'bar',
+          'baz'
+        ]);
+      });
+    });
+    describe('when given a comma-delimited string', function() {
+      it('should return a flat array', function() {
+        expect(list('foo,bar'), 'to equal', ['foo', 'bar']);
       });
     });
   });
