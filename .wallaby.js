aec295db3afa41b3007ccfc69e4e796299658665
Christopher Hiller <boneskull@boneskull.com>
2020-04-21 10:00:46 -0700

    remove cruft from wallaby config
    also increase default timeout
    
    Ref: #4198

--- .wallaby.js
+++ .wallaby.js
@@ -36,7 +36,7 @@ module.exports = () => {
       // running mocha instance is not the same as mocha under test,
       // running mocha is the project's source code mocha, mocha under test is instrumented version of the source code
       const runningMocha = wallaby.testFramework;
-      runningMocha.timeout(200);
+      runningMocha.timeout(1000);
       // to expose it/describe etc. on the mocha under test
       const MochaUnderTest = require('./');
       const mochaUnderTest = new MochaUnderTest();
@@ -46,18 +46,6 @@ module.exports = () => {
         '',
         mochaUnderTest
       );
-      // to make test/node-unit/color.spec.js pass, we need to run mocha in the project's folder context
-      const childProcess = require('child_process');
-      const execFile = childProcess.execFile;
-      childProcess.execFile = function() {
-        let opts = arguments[2];
-        if (typeof opts === 'function') {
-          opts = {};
-          Array.prototype.splice.call(arguments, 2, 0, opts);
-        }
-        opts.cwd = wallaby.localProjectDir;
-        return execFile.apply(this, arguments);
-      };
       require('./test/setup');
     },
     debug: true
