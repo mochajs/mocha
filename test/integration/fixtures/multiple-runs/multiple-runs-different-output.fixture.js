b16def030e208f3f39906589ae040b2850bd5efd
Nico Jansen <jansennico@gmail.com>
2020-04-22 10:35:52 +0200

    Add integration test for running a suite multiple times

---
+++ test/integration/fixtures/multiple-runs/multiple-runs-different-output.fixture.js
@@ -0,0 +1,22 @@
+'use strict';
+const Mocha = require('../../../../lib/mocha');
+
+const mocha = new Mocha( { reporter: 'json' } );
+if(process.argv.indexOf('--no-clean-references') >= 0) {
+  mocha.cleanReferencesAfterRun(false);
+}
+if(process.argv.indexOf('--directly-dispose') >= 0){
+  mocha.dispose();
+}
+mocha.addFile(require.resolve('./multiple-runs-different-output-suite.fixture.js'));
+console.log('[');
+mocha.run(() => {
+  console.log(',');
+  mocha.run(() => {
+    console.log(',');
+    mocha.run(() => {
+      console.log(']');
+    });
+  });
+});
+
