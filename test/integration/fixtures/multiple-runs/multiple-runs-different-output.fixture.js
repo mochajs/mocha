e21359e6eea5a2cb37ddab8c525d245e7361a4c1
Nico Jansen <jansennico@gmail.com>
2020-04-22 11:17:53 +0200

    Make sure tests run on older node versions

--- test/integration/fixtures/multiple-runs/multiple-runs-different-output.fixture.js
+++ test/integration/fixtures/multiple-runs/multiple-runs-different-output.fixture.js
@@ -1,22 +1,31 @@
 'use strict';
 const Mocha = require('../../../../lib/mocha');
 
-const mocha = new Mocha( { reporter: 'json' } );
-if(process.argv.indexOf('--no-clean-references') >= 0) {
+const mocha = new Mocha({ reporter: 'json' });
+if (process.argv.indexOf('--no-clean-references') >= 0) {
   mocha.cleanReferencesAfterRun(false);
 }
-if(process.argv.indexOf('--directly-dispose') >= 0){
+if (process.argv.indexOf('--directly-dispose') >= 0) {
   mocha.dispose();
 }
 mocha.addFile(require.resolve('./multiple-runs-different-output-suite.fixture.js'));
 console.log('[');
-mocha.run(() => {
-  console.log(',');
+try {
   mocha.run(() => {
     console.log(',');
-    mocha.run(() => {
-      console.log(']');
-    });
+    try {
+      mocha.run(() => {
+        console.log(',');
+        mocha.run(() => {
+          console.log(']');
+        });
+      });
+    } catch (err) {
+      console.error(err.code);
+      throw err;
+    }
   });
-});
-
+} catch (err) {
+  console.error(err.code);
+  throw err;
+}
--- test/integration/fixtures/multiple-runs/start-second-run-if-previous-is-still-running.fixture.js
+++ test/integration/fixtures/multiple-runs/start-second-run-if-previous-is-still-running.fixture.js
@@ -1,8 +1,12 @@
 'use strict';
 const Mocha = require('../../../../lib/mocha');
 
-const mocha = new Mocha( { reporter: 'json' } );
+const mocha = new Mocha({ reporter: 'json' });
 mocha.addFile(require.resolve('./start-second-run-if-previous-is-still-running-suite.fixture.js'));
 mocha.run();
-mocha.run();
+try {
+  mocha.run();
+} catch (err) {
+  console.error(err.code);
+}
 
