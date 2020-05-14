555463949c368d755a256d79c4f0158e09e308f3
Christopher Hiller <boneskull@boneskull.com>
2020-05-14 14:49:44 -0700

    fix improper warnings for invalid reporters (#4275)
    * fix improper warnings for invalid reporters
    
        also: reorganize, add, and refactor a bunch of problematic unit tests for `lib/mocha.js`.  better isolation except where we can't really do that (calling `require`)
    
        there's still missing tests in here, but this is an improvement.
    
    * restrict use of Object.assign in ESLint config
    * add wrapper around process.cwd()
        - better isolation
        - a use of `process.cwd()` in `lib/runner.js` was left in place to avoid conflicts, because another PR will remove it

--- test/node-unit/mocha.spec.js
+++ test/node-unit/mocha.spec.js
@@ -1,31 +1,63 @@
 'use strict';
 
-const Mocha = require('../../lib/mocha');
-const utils = require('../../lib/utils');
+const path = require('path');
+const rewiremock = require('rewiremock/node');
 const {createSandbox} = require('sinon');
+const {EventEmitter} = require('events');
+
+const MODULE_PATH = require.resolve('../../lib/mocha.js');
+const DUMB_FIXTURE_PATH = require.resolve('./fixtures/dumb-module.fixture.js');
+const DUMBER_FIXTURE_PATH = require.resolve(
+  './fixtures/dumber-module.fixture.js'
+);
 
 describe('Mocha', function() {
-  const opts = {reporter: utils.noop}; // no output
-  const dumbFilepath = require.resolve('./fixtures/dumb-module.fixture.js');
-  const dumberFilepath = require.resolve('./fixtures/dumber-module.fixture.js');
-
-  let mocha;
+  let stubs;
+  let opts;
+  let Mocha;
   let sandbox;
 
   beforeEach(function() {
     sandbox = createSandbox();
-    mocha = new Mocha(opts);
-    delete require.cache[dumbFilepath];
-    delete require.cache[dumberFilepath];
+    opts = {reporter: sandbox.stub()};
+
+    stubs = {};
+    stubs.utils = {
+      supportsEsModules: sandbox.stub().returns(false),
+      warn: sandbox.stub(),
+      isString: sandbox.stub(),
+      noop: sandbox.stub(),
+      cwd: sandbox.stub().returns(process.cwd())
+    };
+    stubs.suite = Object.assign(sandbox.createStubInstance(EventEmitter), {
+      slow: sandbox.stub(),
+      timeout: sandbox.stub(),
+      bail: sandbox.stub()
+    });
+    stubs.Suite = sandbox.stub().returns(stubs.suite);
+    stubs.Suite.constants = {};
+
+    Mocha = rewiremock.proxy(MODULE_PATH, r => ({
+      '../../lib/utils': r.with(stubs.utils).callThrough(),
+      '../../lib/suite': stubs.Suite
+    }));
+    delete require.cache[DUMB_FIXTURE_PATH];
+    delete require.cache[DUMBER_FIXTURE_PATH];
   });
 
   afterEach(function() {
-    delete require.cache[dumbFilepath];
-    delete require.cache[dumberFilepath];
+    delete require.cache[DUMB_FIXTURE_PATH];
+    delete require.cache[DUMBER_FIXTURE_PATH];
     sandbox.restore();
   });
 
   describe('instance method', function() {
+    let mocha;
+
+    beforeEach(function() {
+      mocha = new Mocha(opts);
+    });
+
     describe('addFile()', function() {
       it('should add the given file to the files array', function() {
         mocha.addFile('some-file.js');
@@ -40,9 +72,12 @@ describe('Mocha', function() {
     describe('loadFiles()', function() {
       it('should load all files from the files array', function() {
         this.timeout(1000);
-        mocha.files = [dumbFilepath, dumberFilepath];
+        mocha.files = [DUMB_FIXTURE_PATH, DUMBER_FIXTURE_PATH];
         mocha.loadFiles();
-        expect(require.cache, 'to have keys', [dumbFilepath, dumberFilepath]);
+        expect(require.cache, 'to have keys', [
+          DUMB_FIXTURE_PATH,
+          DUMBER_FIXTURE_PATH
+        ]);
       });
 
       it('should execute the optional callback if given', function() {
@@ -54,13 +89,13 @@ describe('Mocha', function() {
 
     describe('unloadFiles()', function() {
       it('should delegate Mocha.unloadFile() for each item in its list of files', function() {
-        mocha.files = [dumbFilepath, dumberFilepath];
+        mocha.files = [DUMB_FIXTURE_PATH, DUMBER_FIXTURE_PATH];
         sandbox.stub(Mocha, 'unloadFile');
         mocha.unloadFiles();
         expect(Mocha.unloadFile, 'to have a call exhaustively satisfying', [
-          dumbFilepath
+          DUMB_FIXTURE_PATH
         ])
-          .and('to have a call exhaustively satisfying', [dumberFilepath])
+          .and('to have a call exhaustively satisfying', [DUMBER_FIXTURE_PATH])
           .and('was called twice');
       });
 
@@ -68,14 +103,96 @@ describe('Mocha', function() {
         expect(mocha.unloadFiles(), 'to be', mocha);
       });
     });
+
+    describe('reporter()', function() {
+      describe('when a reporter exists relative to the cwd', function() {
+        beforeEach(function() {
+          stubs.utils.cwd.returns(
+            path.resolve(__dirname, '..', '..', 'lib', 'reporters')
+          );
+        });
+
+        it('should load from current working directory', function() {
+          expect(function() {
+            mocha.reporter('./spec.js');
+          }, 'not to throw');
+        });
+
+        describe('when the reporter throws upon load', function() {
+          it('should throw "invalid reporter" exception', function() {
+            expect(
+              function() {
+                mocha.reporter(
+                  '../../test/node-unit/fixtures/wonky-reporter.fixture.js'
+                );
+              },
+              'to throw',
+              {
+                code: 'ERR_MOCHA_INVALID_REPORTER'
+              }
+            );
+          });
+
+          it('should warn about the error before throwing', function() {
+            try {
+              mocha.reporter(
+                '../../test/node-unit/fixtures/wonky-reporter.fixture.js'
+              );
+            } catch (ignored) {
+            } finally {
+              expect(stubs.utils.warn, 'to have a call satisfying', [
+                expect.it('to match', /reporter blew up/)
+              ]);
+            }
+          });
+        });
+      });
+
+      describe('when a reporter exists relative to the "mocha" module path', function() {
+        it('should load from module path', function() {
+          expect(function() {
+            mocha.reporter('./reporters/spec');
+          }, 'not to throw');
+        });
+
+        describe('when the reporter throws upon load', function() {
+          it('should throw "invalid reporter" exception', function() {
+            expect(
+              function() {
+                mocha.reporter(
+                  '../../test/node-nit/fixtures/wonky-reporter.fixture.js'
+                );
+              },
+              'to throw',
+              {
+                code: 'ERR_MOCHA_INVALID_REPORTER'
+              }
+            );
+          });
+
+          it('should warn about the error before throwing', function() {
+            try {
+              mocha.reporter(
+                require.resolve('./fixtures/wonky-reporter.fixture.js')
+              );
+            } catch (ignored) {
+            } finally {
+              expect(stubs.utils.warn, 'to have a call satisfying', [
+                expect.it('to match', /reporter blew up/)
+              ]);
+            }
+          });
+        });
+      });
+    });
   });
 
   describe('static method', function() {
     describe('unloadFile()', function() {
       it('should unload a specific file from cache', function() {
-        require(dumbFilepath);
-        Mocha.unloadFile(dumbFilepath);
-        expect(require.cache, 'not to have key', dumbFilepath);
+        require(DUMB_FIXTURE_PATH);
+        Mocha.unloadFile(DUMB_FIXTURE_PATH);
+        expect(require.cache, 'not to have key', DUMB_FIXTURE_PATH);
       });
     });
   });
