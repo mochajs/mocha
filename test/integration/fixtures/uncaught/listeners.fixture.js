cb5eb8ed42abfd0d63c5013353843f1208ff6582
Christopher Hiller <boneskull@boneskull.com>
2020-05-20 12:08:37 -0700

    multiple async done() calls result in failure; closes #4151 (#4152)
    - added a method in `errors` module to create a "multiple done" err
    - modernize `multiple-done.spec.js`
    - refactor errors into constants in `errors` module
    - remove `Runner#started` prop; replace with `Runner#state` prop + constants
    - add a catchall `createFatalError()` function to `errors` module; this is called when a test fails twice by other means (unsure what those means are yet)
    - force color in Travis CI b/c my eyes
    - remove `Runner#uncaughtEnd`; move logic to `Runner#uncaught`, since we can now rely on the value of `Runner#state`.
    - upgrade `unexpected-eventemitter`
    - fix potential listener leak in `Runner#run`

--- test/integration/fixtures/uncaught/listeners.fixture.js
+++ test/integration/fixtures/uncaught/listeners.fixture.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const assert = require('assert');
-const mocha = require("../../../../lib/mocha");
+const mocha = require('../../../../lib/mocha');
 
 // keep this low to avoid warning
 for (let i = 0; i < 5; i++) {
-  const r = new mocha.Runner(new mocha.Suite("" + i, undefined));
+  const r = new mocha.Runner(new mocha.Suite('' + i, undefined));
   r.run();
 }
 
 assert.equal(process.listenerCount('uncaughtException'), 1);
-assert.equal(process.listeners('uncaughtException')[0].name, 'uncaughtEnd');
+assert.equal(process.listeners('uncaughtException')[0].name, 'uncaught');
