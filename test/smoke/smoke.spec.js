bdbf3ed43d8be1ac317f271f614cb98eb881d78e
Christopher Hiller <boneskull@boneskull.com>
2020-05-22 10:44:55 -0700

    renamed smoke test
    - add notes about what it is intended to do, and what it cannot do
    
    Signed-off-by: Christopher Hiller <boneskull@boneskull.com>

---
+++ test/smoke/smoke.spec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// This test ensures Mocha's dependencies are properly in place,
+// and is intended to be run after an `npm install --production` in a clean
+// working copy. It helps avoid publishing Mocha with `dependencies`
+// in `devDependencies` or otherwise in the wrong place.
+// It does not ensure that all files are present in the published package!
+
+var assert = require('assert');
+
+describe('a production installation of Mocha', function() {
+  it('should be able to execute a test', function() {
+    assert.ok(true);
+  });
+});
