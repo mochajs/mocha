76f5e66b0e75eb281166235ad2a60bb811358aaa
Gil Tayar <gil@tayar.org>
2020-05-29 09:43:44 +0300

    add "static" named exports

--- index.mjs
+++ index.mjs
@@ -14,3 +14,14 @@ export const it = Mocha.it;
 export const xit = Mocha.xit;
 export const test = Mocha.test;
 export const run = Mocha.run;
+
+export const utils = Mocha.utils;
+export const interfaces = Mocha.interfaces;
+export const reporters = Mocha.reporters;
+export const Runnable = Mocha.Runnable;
+export const Context = Mocha.Context;
+export const Runner = Mocha.Runner;
+export const Suite = Mocha.Suite;
+export const Hook = Mocha.Hook;
+export const Test = Mocha.Test;
+export const unloadFile = Mocha.unloadFile;
