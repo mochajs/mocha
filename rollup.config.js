21371633e704c0735c54909f92cc3afed7eefdb6
Chuf <42591821+GChuf@users.noreply.github.com>
2020-08-18 23:49:53 +0200

    Update dependencies & dev dependencies (#4396)
    * Update dependencies & dev dependencies
    
    * fix build, remove spec:true from babel config
    
    update lint-staged config
    
    Signed-off-by: Christopher Hiller <boneskull@boneskull.com>
    
    Co-authored-by: Christopher Hiller <boneskull@boneskull.com>

--- rollup.config.js
+++ rollup.config.js
@@ -38,7 +38,6 @@ const config = {
           '@babel/preset-env',
           {
             modules: false,
-            spec: true,
             useBuiltIns: 'usage',
             forceAllTransforms: true,
             corejs: {
