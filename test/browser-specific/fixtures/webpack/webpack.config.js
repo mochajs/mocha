043da052690a93acb40d105009d2777e57e2b80a
Christopher Hiller <boneskull@boneskull.com>
2020-09-02 13:25:30 -0700

    redirect webpack test output to a temp dir
    Signed-off-by: Christopher Hiller <boneskull@boneskull.com>

--- test/browser-specific/fixtures/webpack/webpack.config.js
+++ test/browser-specific/fixtures/webpack/webpack.config.js
@@ -1,9 +1,18 @@
 'use strict';
 
 const FailOnErrorsPlugin = require('fail-on-errors-webpack-plugin');
+const {tmpdir} = require('os');
+const {join} = require('path');
+
+const outputPath = join(tmpdir(), 'mocha-test-webpack');
+
+console.error('output dir: %s', outputPath);
 
 module.exports = {
   entry: require.resolve('./webpack.fixture.mjs'),
+  output: {
+    path: outputPath
+  },
   plugins: [
     new FailOnErrorsPlugin({
       failOnErrors: true,
