1b9aa8226856c9d3bd48b408a4a3308c62a404b7
Nico Jansen <jansennico@gmail.com>
2021-02-16 19:17:03 +0100

    skip parallel test for node 10

--- test/integration/parallel.spec.js
+++ test/integration/parallel.spec.js
@@ -2,17 +2,27 @@
 
 const assert = require('assert');
 const {runMochaJSONAsync} = require('./helpers');
+const semver = require('semver');
 
 describe('parallel run', () => {
-  it('it should allow "const {it} = require("mocha") syntax', async () => {
-    const result = await runMochaJSONAsync('parallel/test3.mjs', [
-      '--parallel',
-      '--jobs',
-      '2',
-      require.resolve('./fixtures/parallel/test1.mjs'),
-      require.resolve('./fixtures/parallel/test2.mjs')
-    ]);
-    assert.strictEqual(result.stats.failures, 1);
-    assert.strictEqual(result.stats.passes, 2);
+  /**
+   * @see https://github.com/mochajs/mocha/issues/4559
+   */
+  it('should allow `import {it} from "mocha"` module syntax', async () => {
+    if (semver.major(process.version) <= 10) {
+      console.log(
+        `[SKIPPED] for node ${process.version} (es module syntax isn't supported on node <= 10)`
+      );
+    } else {
+      const result = await runMochaJSONAsync('parallel/test3.mjs', [
+        '--parallel',
+        '--jobs',
+        '2',
+        require.resolve('./fixtures/parallel/test1.mjs'),
+        require.resolve('./fixtures/parallel/test2.mjs')
+      ]);
+      assert.strictEqual(result.stats.failures, 1);
+      assert.strictEqual(result.stats.passes, 2);
+    }
   });
 });
