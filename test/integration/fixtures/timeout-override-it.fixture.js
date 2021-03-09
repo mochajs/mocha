a94583936cc785bc6d194ace0d54ade9ec29a7a7
Alexander Fenster <fenster@google.com>
2021-03-09 10:14:35 -0800

    test: require mocha in fixture

--- test/integration/fixtures/timeout-override-it.fixture.js
+++ test/integration/fixtures/timeout-override-it.fixture.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const {describe, it} = require('../../../index.js');
+
 describe('timeout override', function() {
   it('should allow using it().timeout()', function(done) {
     setTimeout(done, 1);
