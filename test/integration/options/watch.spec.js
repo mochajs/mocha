8285910c4d4efc0f1e4a22d14939d103815b7fa5
JeongHoon Byun (a.k.a Outsider) <outsideris@gmail.com>
2021-04-26 00:31:30 +0900

    Watch for test files are crashed (#4614)
    Signed-off-by: Outsider <outsideris@gmail.com>

--- test/integration/options/watch.spec.js
+++ test/integration/options/watch.spec.js
@@ -47,6 +47,19 @@ describe('--watch', function() {
       });
     });
 
+    it('reruns test when watched test file crashes', function() {
+      const testFile = path.join(tempDir, 'test.js');
+      copyFixture(DEFAULT_FIXTURE, testFile);
+
+      replaceFileContents(testFile, 'done();', 'done((;');
+
+      return runMochaWatchJSONAsync([testFile], tempDir, () => {
+        replaceFileContents(testFile, 'done((;', 'done();');
+      }).then(results => {
+        expect(results, 'to have length', 1);
+      });
+    });
+
     describe('when in parallel mode', function() {
       it('reruns test when watched test file is touched', function() {
         const testFile = path.join(tempDir, 'test.js');
@@ -56,6 +69,19 @@ describe('--watch', function() {
           touchFile(testFile);
         }).then(results => {
           expect(results, 'to have length', 2);
+        });
+      });
+
+      it('reruns test when watched test file is crashed', function() {
+        const testFile = path.join(tempDir, 'test.js');
+        copyFixture(DEFAULT_FIXTURE, testFile);
+
+        replaceFileContents(testFile, 'done();', 'done((;');
+
+        return runMochaWatchJSONAsync([testFile], tempDir, () => {
+          replaceFileContents(testFile, 'done((;', 'done();');
+        }).then(results => {
+          expect(results, 'to have length', 1);
         });
       });
     });
