78de2e79f24e6498a5d4555d8fd044dc7e4ec527
Juerg B <44573692+juergba@users.noreply.github.com>
2021-05-21 11:11:06 +0200

    Drop support of Node v10 (#4633)

--- test/integration/parallel.spec.js
+++ test/integration/parallel.spec.js
@@ -2,27 +2,20 @@
 
 const assert = require('assert');
 const {runMochaJSONAsync} = require('./helpers');
-const semver = require('semver');
 
 describe('parallel run', () => {
   /**
    * @see https://github.com/mochajs/mocha/issues/4559
    */
   it('should allow `import {it} from "mocha"` module syntax', async () => {
-    if (semver.major(process.version) <= 10) {
-      console.log(
-        `[SKIPPED] for node ${process.version} (es module syntax isn't supported on node <= 10)`
-      );
-    } else {
-      const result = await runMochaJSONAsync('parallel/test3.mjs', [
-        '--parallel',
-        '--jobs',
-        '2',
-        require.resolve('./fixtures/parallel/test1.mjs'),
-        require.resolve('./fixtures/parallel/test2.mjs')
-      ]);
-      assert.strictEqual(result.stats.failures, 1);
-      assert.strictEqual(result.stats.passes, 2);
-    }
+    const result = await runMochaJSONAsync('parallel/test3.mjs', [
+      '--parallel',
+      '--jobs',
+      '2',
+      require.resolve('./fixtures/parallel/test1.mjs'),
+      require.resolve('./fixtures/parallel/test2.mjs')
+    ]);
+    assert.strictEqual(result.stats.failures, 1);
+    assert.strictEqual(result.stats.passes, 2);
   });
 });
