62d870875faa0593ec8bd931f218e5f9b0747188
Ray Foss <git@vblip.com>
2021-06-03 16:47:45 -0500

    docs

--- bin/_mocha.js
+++ bin/_mocha.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
-/**
- * This file remains for backwards compatibility only.
- * Don't put stuff in this file.
- * @see module:lib/cli
- */
+// Node has a hard time telling what loader to use without an extension
+// If you use the node --loader option, use this
+// https://github.com/mochajs/mocha/pull/4647/files?file-filters%5B%5D=.json
 
 require('../lib/cli').main();
