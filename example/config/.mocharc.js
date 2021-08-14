757b85dd230079901b181c68f0be82b9a3de6407
Juerg B <44573692+juergba@users.noreply.github.com>
2021-08-14 18:07:05 +0200

    docs: improve 'grep()' and clarify docs (#4714)

--- example/config/.mocharc.js
+++ example/config/.mocharc.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // This is a JavaScript-based config file containing every Mocha option plus others.
-// If you need conditional logic, you might want to use this type of config.
+// If you need conditional logic, you might want to use this type of config,
+// e.g. set options via environment variables 'process.env'.
 // Otherwise, JSON or YAML is recommended.
 
 module.exports = {
