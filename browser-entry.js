552c20879853e3339812c58632ec7558f6f1c40c
华 <1395348685z@gmail.com>
2021-09-12 22:43:18 +0800

    fix(browser): stop using all global vars when init

--- browser-entry.js
+++ browser-entry.js
@@ -216,4 +216,29 @@ global.mocha = mocha;
 // this allows test/acceptance/required-tokens.js to pass; thus,
 // you can now do `const describe = require('mocha').describe` in a
 // browser context (assuming browserification).  should fix #880
-module.exports = Object.assign(mocha, global);
+[
+  'Mocha',
+  'after',
+  'afterEach',
+  'before',
+  'beforeEach',
+  'context',
+  'describe',
+  'it',
+  'mocha',
+  'run',
+  'setup',
+  'specify',
+  'suite',
+  'suiteSetup',
+  'suiteTeardown',
+  'teardown',
+  'test',
+  'xcontext',
+  'xdescribe',
+  'xit',
+  'xspecify'
+].forEach(function(key) {
+  if (key in global) mocha[key] = global[key];
+});
+module.exports = mocha;
