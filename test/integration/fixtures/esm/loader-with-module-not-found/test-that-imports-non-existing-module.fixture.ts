b37be07be54b519148db5dfc41369c146f3108d3
Gil Tayar <gil@tayar.org>
2022-01-10 11:25:28 +0200

    fix comment in test-that-imports-non-existing-module.ts to point to correct resolved file

--- test/integration/fixtures/esm/loader-with-module-not-found/test-that-imports-non-existing-module.fixture.ts
+++ test/integration/fixtures/esm/loader-with-module-not-found/test-that-imports-non-existing-module.fixture.ts
@@ -1,2 +1,2 @@
-// This file will be resolved to test.js by the loader
+// This file will be resolved to `test-that-imports-non-existing-module.fixture.mjs` by the loader
 import 'non-existent-package';
