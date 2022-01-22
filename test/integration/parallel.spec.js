1c52cdad44b2daac55649e0fb1c2ffe69749c64b
Quentin Barbe <forty@everteam.org>
2022-01-22 15:20:19 +0100

    feat(parallel): assign each worker a worker-id (#4813)
    This uses a new feature from the workerpool module, which allows
    overriding process parameters for each process, which is why the
    workerpool module has been updated to the latest version (6.2.0).

--- test/integration/parallel.spec.js
+++ test/integration/parallel.spec.js
@@ -18,4 +18,16 @@ describe('parallel run', () => {
     assert.strictEqual(result.stats.failures, 1);
     assert.strictEqual(result.stats.passes, 2);
   });
+
+  it('should correctly set worker ids for each process', async () => {
+    const result = await runMochaJSONAsync('parallel/testworkerid3.mjs', [
+      '--parallel',
+      '--jobs',
+      '2',
+      require.resolve('./fixtures/parallel/testworkerid1.mjs'),
+      require.resolve('./fixtures/parallel/testworkerid2.mjs')
+    ]);
+    assert.strictEqual(result.stats.failures, 0);
+    assert.strictEqual(result.stats.passes, 3);
+  });
 });
