785aeb1ff22793086543c559f93e5b482621e820
Juerg B <44573692+juergba@users.noreply.github.com>
2022-03-28 07:42:00 +0200

    chore(test): drop AMD/'requirejs' (#4857)

--- test/browser-specific/fixtures/requirejs/main.fixture.js
+++
@@ -1,9 +0,0 @@
-define(['lib'], function(lib) {
-  describe('lib', function() {
-    it('should equal "foo"', function() {
-      if (lib !== 'foo') {
-        throw new Error('should be "foo"');
-      }
-    });
-  });
-});
