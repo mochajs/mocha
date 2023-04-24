17b190e7ccebc3b02899d55d299111810a44d8c0
Manuel Thalmann <m@nuth.ch>
2023-04-24 21:15:08 +0200

    test(cli): test the functionality for rc files

--- test/integration/options.spec.js
+++ test/integration/options.spec.js
@@ -4,17 +4,36 @@ var path = require('path');
 var loadOptions = require('../../lib/cli/options').loadOptions;
 
 describe('options', function () {
-  it('Should support extended options', function () {
-    var configDir = path.join(
-      __dirname,
-      'fixtures',
-      'config',
-      'mocharc-extended'
-    );
+  var workingDirectory;
+  const workspaceDir = path.join(
+    __dirname,
+    'fixtures',
+    'config',
+    'mocharc-extended'
+  );
+
+  beforeEach(function () {
+    workingDirectory = process.cwd();
+    process.chdir(workspaceDir);
+  });
+
+  afterEach(function () {
+    process.chdir(workingDirectory);
+  });
+
+  it('Should support extended options using --config parameter', function () {
     var extended = loadOptions([
       '--config',
-      path.join(configDir, 'extends.json')
+      path.join(workspaceDir, 'extends.json')
     ]);
+    expect(extended.require, 'to equal', ['foo', 'bar']);
+    expect(extended.bail, 'to equal', true);
+    expect(extended.reporter, 'to equal', 'html');
+    expect(extended.slow, 'to equal', 30);
+  });
+
+  it('Should support extended options using rc file', function () {
+    var extended = loadOptions([]);
     expect(extended.require, 'to equal', ['foo', 'bar']);
     expect(extended.bail, 'to equal', true);
     expect(extended.reporter, 'to equal', 'html');
