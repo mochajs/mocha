5e25d512ef58df9b4a9f263c2ef42a850984c7ff
CheadleCheadle <gcheadle123@gmail.com>
2024-02-21 16:33:18 -0800

    spec-reporter prints retries

--- test/integration/retries.spec.js
+++ test/integration/retries.spec.js
@@ -134,4 +134,34 @@ describe('retries', function () {
       }
     );
   });
+
+  it('should return current retry count out of total retry count', function (done) {
+    helpers.runMocha(
+      'retries/early-pass.fixture.js',
+      ['--reporter', 'spec'],
+      function (err, res) {
+        var lines, expected;
+
+        if (err) {
+          done(err);
+          return;
+        }
+
+        lines = res.output
+          .split(helpers.SPLIT_DOT_REPORTER_REGEXP)
+          .map(function (line) {
+            return line.trim();
+          })
+          .filter(function (line) {
+            return line.length;
+          })
+          .slice(0, -1);
+
+        var expected = '(Succeeded after 1 / 1 retries)';
+
+        assert.equal(lines[2], expected);
+        done();
+      }
+    );
+  });
 });
