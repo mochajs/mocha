eed332c200783a817d9b372afaa74f83db03154b
Chengzhong Wu <cwu631@bloomberg.net>
2025-06-18 14:32:40 +0100

    Revert "feat: use require to load esm (#5366)"
    This reverts commit 41e24a242944da0cfc9d4d6989dede85f648cb40.

--- test/compiler-cjs/test.ts
+++
@@ -1,9 +0,0 @@
-const obj: unknown = { foo: 'bar' };
-
-describe('cts written in esm', () => {
-  it('should work', () => {
-    expect(obj, 'to equal', { foo: 'bar' });
-  });
-});
-
-export const foo = 'bar';
