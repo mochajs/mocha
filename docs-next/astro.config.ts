ec6c936d00d4c53e5385ef8441354908f6702b79
Mark Wiemer <markwiemer@outlook.com>
2025-08-18 18:37:16 -0700

    Alphabetize nav

--- docs-next/astro.config.ts
+++ docs-next/astro.config.ts
@@ -108,12 +108,12 @@ export default defineConfig({
             },
             { label: "Test duration", slug: "explainers/test-duration" },
             {
-              label: "Third party reporters",
-              slug: "explainers/third-party-reporters",
-            },
-            {
               label: "Test fixture decision tree",
               slug: "explainers/test-fixture-decision-tree",
+            },
+            {
+              label: "Third party reporters",
+              slug: "explainers/third-party-reporters",
             },
           ],
           label: "Explainers",
