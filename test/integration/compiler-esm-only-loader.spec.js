458f99b7e50d2680375a48d0dff8d566ae1663b5
Mark Wiemer <markwiemer@outlook.com>
2025-08-22 20:31:34 -0700

    Refactor compiler-esm-only-loader spec

--- test/integration/compiler-esm-only-loader.spec.js
+++ test/integration/compiler-esm-only-loader.spec.js
@@ -5,13 +5,18 @@ var path = require('node:path');
 
 // Regression test for https://github.com/mochajs/mocha/issues/5380
 describe('support ESM only module loader packages', function () {
-  it('should support ESM .js extension', function (done) {
+  /**
+   * Load the filename using an ESM-only loader and expect things to work
+   * @param {string} filename Name of the file to load
+   * @param {Function} done The done callback
+   */
+  function loadAndExpect(filename, done) {
     exec(
       '"' +
         process.execPath +
         '" "' +
         path.join('bin', 'mocha') +
-        '" -R json --require "@test/esm-only-loader" "test/compiler-esm/*.js"',
+        `" -R json --require "@test/esm-only-loader" "${filename}"`,
       {cwd: path.join(__dirname, '..', '..')},
       function (error, stdout) {
         if (error && !stdout) {
@@ -24,48 +29,22 @@ describe('support ESM only module loader packages', function () {
           expect(results.tests[index], 'to have property', 'fullTitle');
           titles.push(results.tests[index].fullTitle);
         }
-        expect(
-          titles,
-          'to contain',
-          'esm written in esm should work',
-        ).and(
-          'to contain',
-          'esm written in esm with top-level-await should work',
-        ).and('to have length', 2);
+        expect(titles, 'to contain', 'esm written in esm should work')
+          .and(
+            'to contain',
+            'esm written in esm with top-level-await should work'
+          )
+          .and('to have length', 2);
         done();
       }
     );
+  }
+
+  it('should support ESM .js extension', function (done) {
+    loadAndExpect('test/compiler-esm/*.js', done);
   });
 
   it('should support ESM .ts extension', function (done) {
-    exec(
-      '"' +
-        process.execPath +
-        '" "' +
-        path.join('bin', 'mocha') +
-        '" -R json --require "@test/esm-only-loader" "test/compiler-esm/*.ts"',
-      {cwd: path.join(__dirname, '..', '..')},
-      function (error, stdout) {
-        if (error && !stdout) {
-          return done(error);
-        }
-        var results = JSON.parse(stdout);
-        expect(results, 'to have property', 'tests');
-        var titles = [];
-        for (var index = 0; index < results.tests.length; index += 1) {
-          expect(results.tests[index], 'to have property', 'fullTitle');
-          titles.push(results.tests[index].fullTitle);
-        }
-        expect(
-          titles,
-          'to contain',
-          'esm written in esm should work',
-        ).and(
-          'to contain',
-          'esm written in esm with top-level-await should work',
-        ).and('to have length', 2);
-        done();
-      }
-    );
+    loadAndExpect('test/compiler-esm/*.ts', done);
   });
 });
