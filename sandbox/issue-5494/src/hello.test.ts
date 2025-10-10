53969c883573beda3edd7cf9b25ab1039f00ffd2
Mark Wiemer <markwiemer@outlook.com>
2025-10-10 06:19:04 -0700

    Delete sandbox repro

--- sandbox/issue-5494/src/hello.test.ts
+++
@@ -1,7 +0,0 @@
-import "./other.js";
-
-describe("the test", function () {
-  it("should work", function () {
-    console.log("ok");
-  });
-});
--- sandbox/issue-5494/src/other.ts
+++
@@ -1,9 +0,0 @@
-export const ok = () => {
-  console.log("ok");
-};
-
-// there must be an async involved somewhere or the error does not trigger
-await Promise.resolve("something");
-
-// simulate an error in a module
-throw new Error("stop");
