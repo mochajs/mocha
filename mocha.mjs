5a07936b07dff8f89338c7aa4a3865f98f8dc22a
Mark Wiemer <markwiemer@outlook.com>
2025-11-13 20:29:44 -0800

    Ignore missing import error, format file

--- mocha.mjs
+++ mocha.mjs
@@ -1,24 +1,35 @@
-import "./mocha.js"
+/* eslint-disable-next-line n/no-missing-import */
+import "./mocha.js";
 
 const { mocha } = globalThis;
-
-const describe = mocha.describe.bind(mocha);
-const context = mocha.context.bind(mocha);
-const it = mocha.it.bind(mocha);
-const specify = mocha.specify.bind(mocha);
-const xdescribe = mocha.xdescribe.bind(mocha);
-const xcontext = mocha.xcontext.bind(mocha);
-const xit = mocha.xit.bind(mocha);
-const xspecify = mocha.xspecify.bind(mocha);
+const after = mocha.after.bind(mocha);
+const afterEach = mocha.afterEach.bind(mocha);
 const before = mocha.before.bind(mocha);
 const beforeEach = mocha.beforeEach.bind(mocha);
-const afterEach = mocha.afterEach.bind(mocha);
-const after = mocha.after.bind(mocha);
-
-const setup = mocha.setup.bind(mocha);
+const context = mocha.context.bind(mocha);
+const describe = mocha.describe.bind(mocha);
+const it = mocha.it.bind(mocha);
 const run = mocha.run.bind(mocha);
+const setup = mocha.setup.bind(mocha);
+const specify = mocha.specify.bind(mocha);
+const xcontext = mocha.xcontext.bind(mocha);
+const xdescribe = mocha.xdescribe.bind(mocha);
+const xit = mocha.xit.bind(mocha);
+const xspecify = mocha.xspecify.bind(mocha);
 
 export {
-  describe, context, it, specify, xdescribe, xcontext,
-  xit, xspecify, before, beforeEach, afterEach, after, setup, run
+  after,
+  afterEach,
+  before,
+  beforeEach,
+  context,
+  describe,
+  it,
+  run,
+  setup,
+  specify,
+  xcontext,
+  xdescribe,
+  xit,
+  xspecify,
 };
