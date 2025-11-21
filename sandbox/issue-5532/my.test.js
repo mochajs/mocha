62021a3f10ff9ccf9e262dc8bc5064951c783ab3
Mark Wiemer <markwiemer@outlook.com>
2025-11-20 20:39:00 -0800

    Repro #5532

---
+++ sandbox/issue-5532/my.test.js
@@ -0,0 +1,15 @@
+"use strict";
+
+describe("fails `before` hook", function () {
+  it("test 1", function () {
+    // This should be reported as failed due to before hook failure
+  });
+  it("test 2", function () {
+    // This should be reported as failed due to before hook failure
+  });
+});
+describe("passes normally", function () {
+  it("test 3", function () {
+    // This should pass normally
+  });
+});
