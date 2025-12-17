b9d91500b529ae39391d148cf94f5098ef06613a
Nikolay Vitkov <nvitkov@chromium.org>
2025-12-17 13:01:44 +0100

    fix allow importing ESM reporter and

---
+++ test/integration/fixtures/simple-ui.fixture.mjs
@@ -0,0 +1,36 @@
+import Mocha from '../../../lib/mocha.js'
+import MochaInterface from '../../../lib/interfaces/common.js';
+
+const Test = Mocha.Test;
+const EVENT_FILE_PRE_REQUIRE = Mocha.Suite.constants.EVENT_FILE_PRE_REQUIRE;
+
+/**
+ * A simple UI that only exposes a single function: test
+ */
+export default Mocha.interfaces['simple-ui'] = function(suite) {
+  suite.on(EVENT_FILE_PRE_REQUIRE, function(
+    context,
+    file,
+    mocha
+  ) {
+    const common = MochaInterface(
+      [suite],
+      context
+    );
+
+    context.run = mocha.options.delay && common.runWithSuite(suite);
+
+    /**
+     * Describes a specification or test-case with the given `title`
+     * and callback `fn` acting as a thunk.
+     */
+    context.test = function(title, fn) {
+      const test = new Test(title, fn);
+      test.file = file;
+      suite.addTest(test);
+
+      return test;
+    };
+  });
+};
+
