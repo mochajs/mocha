a448731ae0256e0d79aca3c7fc68f78ad5520b43
Brett Zamir <brettz9@yahoo.com>
2025-12-18 06:43:40 -0700

    ensure methods are available

--- mocha.mjs
+++ mocha.mjs
@@ -3,20 +3,24 @@
 import "./mocha.js";
 
 const { mocha } = globalThis;
-const after = mocha.after.bind(mocha);
-const afterEach = mocha.afterEach.bind(mocha);
-const before = mocha.before.bind(mocha);
-const beforeEach = mocha.beforeEach.bind(mocha);
-const context = mocha.context.bind(mocha);
-const describe = mocha.describe.bind(mocha);
-const it = mocha.it.bind(mocha);
+
+mocha.ui();
+
+const after = globalThis.after.bind(mocha);
+const afterEach = globalThis.afterEach.bind(mocha);
+const before = globalThis.before.bind(mocha);
+const beforeEach = globalThis.beforeEach.bind(mocha);
+const context = globalThis.context.bind(mocha);
+const describe = globalThis.describe.bind(mocha);
+const it = globalThis.it.bind(mocha);
+const specify = globalThis.specify.bind(mocha);
+const xcontext = globalThis.xcontext.bind(mocha);
+const xdescribe = globalThis.xdescribe.bind(mocha);
+const xit = globalThis.xit.bind(mocha);
+const xspecify = globalThis.xspecify.bind(mocha);
+
 const run = mocha.run.bind(mocha);
 const setup = mocha.setup.bind(mocha);
-const specify = mocha.specify.bind(mocha);
-const xcontext = mocha.xcontext.bind(mocha);
-const xdescribe = mocha.xdescribe.bind(mocha);
-const xit = mocha.xit.bind(mocha);
-const xspecify = mocha.xspecify.bind(mocha);
 
 export {
   after,
