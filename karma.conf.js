595e2b70a8f4d9f9a91721005b49da67a5c24420
Mark Wiemer <markwiemer@outlook.com>
2026-01-04 10:06:47 -0800

    remove Sauce Labs!

--- karma.conf.js
+++ karma.conf.js
@@ -30,13 +30,6 @@ const env = process.env;
 const hostname = os.hostname();
 const BROWSER = env.BROWSER;
 
-const SAUCE_BROWSER_PLATFORM_MAP = {
-  "chrome@latest": "Windows 10",
-  "MicrosoftEdge@latest": "Windows 10",
-  "firefox@latest": "Windows 10",
-  "safari@latest": "macOS 10.15",
-};
-
 const baseConfig = {
   frameworks: ["rollup", "mocha"],
   files: [
@@ -47,7 +40,6 @@ const baseConfig = {
   plugins: [
     "karma-mocha",
     "karma-mocha-reporter",
-    "karma-sauce-launcher", // todo remove sauce from this file and package.json
     "karma-chrome-launcher",
     rollupPlugin,
   ],
@@ -79,10 +71,6 @@ module.exports = (config) => {
   let bundleDirPath = path.join(BASE_BUNDLE_DIR_PATH, hostname);
   let cfg = { ...baseConfig };
 
-  // TO RUN AGAINST SAUCELABS LOCALLY, execute:
-  // `CI=1 SAUCE_USERNAME=<user> SAUCE_ACCESS_KEY=<key> npm run test-browser`
-  let sauceConfig;
-
   // configuration for CI mode
   if (env.CI) {
     console.error("CI mode enabled");
@@ -90,29 +78,14 @@ module.exports = (config) => {
       console.error("Github Actions detected");
       const buildId = `github-${env.GITHUB_RUN_ID}_${env.GITHUB_RUN_NUMBER}`;
       bundleDirPath = path.join(BASE_BUNDLE_DIR_PATH, buildId);
-      sauceConfig = {
-        build: buildId,
-      };
     } else {
       console.error(`Local environment (${hostname}) detected`);
-      // don't need to run sauce from Windows CI b/c travis does it.
-      if (env.SAUCE_USERNAME || env.SAUCE_ACCESS_KEY) {
-        const id = `${hostname} (${Date.now()})`;
-        sauceConfig = {
-          build: id,
-          tunnelIdentifier: id,
-        };
-        console.error("Configured SauceLabs");
-      } else {
-        console.error(
-          "No SauceLabs credentials present; set SAUCE_USERNAME and SAUCE_ACCESS_KEY env vars",
-        );
-      }
+      console.error(`CI + local environment is not supported`);
+      console.error(`https://github.com/mochajs/mocha/issues/5616`);
     }
   }
 
   cfg = createBundleDir(cfg, bundleDirPath);
-  cfg = addSauceTests(cfg, sauceConfig);
   cfg = chooseTestSuite(cfg, env.MOCHA_TEST);
 
   // include sourcemap
@@ -161,60 +134,6 @@ const createBundleDir = (cfg, bundleDirPath) => {
 };
 
 /**
- * Adds Saucelabs-specific config to a Karma config object.
- *
- * If `sauceLabs` parameter is falsy, just return a clone of the `cfg` parameter.
- *
- * @see https://github.com/karma-runner/karma-sauce-launcher
- * @see https://github.com/bermi/sauce-connect-launcher#advanced-usage
- * @param {object} cfg - Karma config
- * @param {object} [sauceLabs] - SauceLabs config
- * @returns {object} Karma config
- */
-const addSauceTests = (cfg, sauceLabs) => {
-  if (sauceLabs) {
-    const sauceBrowsers = Object.keys(SAUCE_BROWSER_PLATFORM_MAP);
-
-    // creates Karma `customLauncher` configs from `SAUCE_BROWSER_PLATFORM_MAP`
-    const customLaunchers = sauceBrowsers.reduce((acc, sauceBrowser) => {
-      const platformName = SAUCE_BROWSER_PLATFORM_MAP[sauceBrowser];
-      const [browserName, browserVersion] = sauceBrowser.split("@");
-      const result = {
-        ...acc,
-        [sauceBrowser]: {
-          base: "SauceLabs",
-          browserName,
-          browserVersion,
-          platformName,
-          "sauce:options": sauceLabs,
-        },
-      };
-      if (browserName === "firefox") {
-        result[sauceBrowser]["sauce:options"]["moz:debuggerAddress"] = true;
-      }
-      return result;
-    }, {});
-
-    const result = {
-      ...cfg,
-      reporters: [...cfg.reporters, "saucelabs"],
-      browsers: [...cfg.browsers, ...sauceBrowsers],
-      customLaunchers: {
-        ...cfg.customLaunchers,
-        ...customLaunchers,
-      },
-      sauceLabs,
-      concurrency: Infinity,
-      retryLimit: 1,
-      captureTimeout: 120000,
-      browserNoActivityTimeout: 20000,
-    };
-    return result;
-  }
-  return { ...cfg };
-};
-
-/**
  * Returns a new Karma config containing standard dependencies for our tests.
  *
  * Most suites use this.
@@ -253,28 +172,6 @@ const addStandardDependencies = (cfg) => ({
 });
 
 /**
- * Adds a name for the tests, reflected in SauceLabs' UI. Returns new Karma
- * config.
- *
- * Does not add a test name if the `sauceLabs` prop of `cfg` is falsy (which
- * would imply that we're not running tests on SauceLabs).
- *
- * @param {string} testName - SauceLabs test name
- * @param {object} cfg - Karma config.
- * @returns {object} New Karma config
- */
-const addSauceLabsTestName = (testName, cfg) =>
-  cfg.sauceLabs
-    ? {
-        ...cfg,
-        sauceLabs: {
-          ...cfg.sauceLabs,
-          testName,
-        },
-      }
-    : { ...cfg };
-
-/**
  * Returns a new Karma config to run with specific configuration (which cannot
  * be run with other configurations) as specified by `value`. Known values:
  *
@@ -296,7 +193,6 @@ const chooseTestSuite = (cfg, value) => {
     case "qunit":
       return addStandardDependencies({
         ...cfg,
-        ...addSauceLabsTestName(`Interface "${value}" Integration Tests`, cfg),
         files: [`test/interfaces/${value}.spec.js`],
         client: {
           ...cfg.client,
@@ -308,7 +204,6 @@ const chooseTestSuite = (cfg, value) => {
       });
     case "esm":
       return addStandardDependencies({
-        ...addSauceLabsTestName("ESM Integration Tests", cfg),
         files: [
           {
             pattern: "test/browser-specific/fixtures/esm.fixture.mjs",
@@ -321,8 +216,6 @@ const chooseTestSuite = (cfg, value) => {
         ],
       });
     default:
-      return addStandardDependencies({
-        ...addSauceLabsTestName("Unit Tests", cfg),
-      });
+      return addStandardDependencies({});
   }
 };
