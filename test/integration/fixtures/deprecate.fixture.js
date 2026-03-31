29de573ba62bcee0ee46f5b5f6cfddcf4ecd9597
Pelle Wessman <pelle@kodfabrik.se>
2026-03-31 15:04:23 +0200

    refactor: remove unused `errors.js` functions
    - `warn()` is no longer used anywhere
    - `createInvalidPluginError()` is no longer used anywhere and has been deprecated since #4360, for 6 years now
    - `deprecate()` was only used by `createInvalidPluginError()`
    - `emitWarning()` was only used by `warn()` and `deprecate()` and contained an unnecessary polyfill for `process.emitWarning`, a function that has existed since Node 6.0.0 with us now requiring at least Node 20.19.0
    
    Ref: mochajs/mocha#5357 (v12 Release Plan)

--- test/integration/fixtures/deprecate.fixture.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var errors = require("../../../lib/errors");
-
-it('consolidates identical calls to deprecate', function() {
-  errors.deprecate("suite foo did a deprecated thing");
-  errors.deprecate("suite foo did a deprecated thing");
-  errors.deprecate("suite bar did a deprecated thing");
-});
