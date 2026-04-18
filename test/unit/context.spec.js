fa06dde40cdbcd1bc6e98af19112b1a68a059a29
Mostafa Nazari <93077724+mostafaNazari702@users.noreply.github.com>
2026-04-18 16:22:10 +0200

    test: migrate 5 unit test files from unexpected to chai (#5896)
    Co-authored-by: Mark Wiemer <markwiemer@outlook.com>

--- test/unit/context.spec.js
+++ test/unit/context.spec.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var { expect } = require("chai");
+
 describe("Context", function () {
   beforeEach(function () {
     this.calls = ["before"];
@@ -11,18 +13,18 @@ describe("Context", function () {
     });
 
     it("should work", function () {
-      expect(this.calls, "to equal", ["before", "before two"]);
+      expect(this.calls).to.deep.equal(["before", "before two"]);
       this.calls.push("test");
     });
 
     after(function () {
-      expect(this.calls, "to equal", ["before", "before two", "test"]);
+      expect(this.calls).to.deep.equal(["before", "before two", "test"]);
       this.calls.push("after two");
     });
   });
 
   after(function () {
-    expect(this.calls, "to equal", [
+    expect(this.calls).to.deep.equal([
       "before",
       "before two",
       "test",
@@ -43,7 +45,7 @@ describe("Context Siblings", function () {
     });
 
     it("should work", function () {
-      expect(this.hiddenFromSibling, "to equal", "This should be hidden");
+      expect(this.hiddenFromSibling).to.equal("This should be hidden");
     });
   });
 
@@ -53,25 +55,21 @@ describe("Context Siblings", function () {
     });
 
     it("should not have value set within a sibling describe", function () {
-      expect("This should be hidden", "not to equal", this.hiddenFromSibling);
+      expect("This should be hidden").to.not.equal(this.hiddenFromSibling);
       this.visibleFromTestSibling = "Visible from test sibling";
     });
 
     it("should allow test siblings to modify shared context", function () {
-      expect(
-        "Visible from test sibling",
-        "to equal",
-        this.visibleFromTestSibling,
-      );
+      expect("Visible from test sibling").to.equal(this.visibleFromTestSibling);
     });
 
     it("should have reset this.calls before describe", function () {
-      expect(this.calls, "to equal", ["before", "before sibling"]);
+      expect(this.calls).to.deep.equal(["before", "before sibling"]);
     });
   });
 
   after(function () {
-    expect(this.calls, "to equal", ["before", "before sibling"]);
+    expect(this.calls).to.deep.equal(["before", "before sibling"]);
   });
 });
 
@@ -80,19 +78,19 @@ describe("methods", function () {
     it("should return the timeout", function () {
       // set this explicitly because browser and node use diff settings
       this.timeout(1000);
-      expect(this.timeout(), "to be", 1000);
+      expect(this.timeout()).to.equal(1000);
     });
   });
 
   describe("slow()", function () {
     it("should return the slow", function () {
-      expect(this.slow(), "to be", 75);
+      expect(this.slow()).to.equal(75);
     });
   });
 
   describe("retries", function () {
     it("should return the number of retries", function () {
-      expect(this.retries(), "to be", -1);
+      expect(this.retries()).to.equal(-1);
     });
   });
 });
