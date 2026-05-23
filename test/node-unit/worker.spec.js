08174c808dbdfd167aed1affaf995e1225a0e909
Josh Goldberg <git@joshuakgoldberg.com>
2026-05-23 16:46:18 -0400

    fix: put worker back as .js

--- test/node-unit/worker.spec.js
+++ test/node-unit/worker.spec.js
@@ -2,10 +2,10 @@
 
 const serializeJavascript = require("serialize-javascript");
 const rewiremock = require("rewiremock/node");
-const { SerializableWorkerResult } = require("../../lib/nodejs/serializer");
+const { SerializableWorkerResult } = require("../../lib/nodejs/serializer.mjs");
 const sinon = require("sinon");
 
-const WORKER_PATH = require.resolve("../../lib/nodejs/worker.mjs");
+const WORKER_PATH = require.resolve("../../lib/nodejs/worker.js");
 
 describe("worker", function () {
   let worker;
@@ -65,9 +65,9 @@ describe("worker", function () {
       worker = rewiremock.proxy(WORKER_PATH, {
         workerpool: stubs.workerpool,
         "../../lib/mocha": stubs.Mocha,
-        "../../lib/nodejs/serializer": stubs.serializer,
+        "../../lib/nodejs/serializer.mjs": stubs.serializer,
         "../../lib/cli/run-helpers": stubs.runHelpers,
-        "../../lib/plugin-loader": stubs.plugin,
+        "../../lib/plugin-loader.mjs": stubs.plugin,
       });
     });
 
