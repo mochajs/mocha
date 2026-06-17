bbdeaf47c5255009ddf801322504160e978e1e28
Mark Wiemer <markwiemer@outlook.com>
2026-06-16 21:18:11 -0700

    Fixup more renames for unit tests and jsapi test

--- test/jsapi/index.js
+++ test/jsapi/index.js
@@ -11,12 +11,12 @@ import "../setup.cjs";
 mocha.addFile("test/unit/suite.spec.cjs");
 mocha.addFile("test/unit/runner.spec.cjs");
 mocha.addFile("test/unit/runnable.spec.cjs");
-mocha.addFile("test/unit/hook-sync.spec.js");
-mocha.addFile("test/unit/hook-sync-nested.spec.js");
-mocha.addFile("test/unit/hook-async.spec.js");
-mocha.addFile("test/unit/duration.spec.js");
-mocha.addFile("test/unit/globals.spec.js");
-mocha.addFile("test/unit/timeout.spec.js");
+mocha.addFile("test/unit/hook-sync.spec.cjs");
+mocha.addFile("test/unit/hook-sync-nested.spec.cjs");
+mocha.addFile("test/unit/hook-async.spec.cjs");
+mocha.addFile("test/unit/duration.spec.cjs");
+mocha.addFile("test/unit/globals.spec.cjs");
+mocha.addFile("test/unit/timeout.spec.cjs");
 
 mocha.run(function () {
   console.log("done");
