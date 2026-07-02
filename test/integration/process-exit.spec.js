9b943e81d8113aa22cb8b62b1ba60ff2e4aca193
Naveed Khan <dxbnaveed.k@gmail.com>
2026-07-02 21:09:01 +0530

    cover async and hook exit paths, restore process.exit on all run-end paths

--- test/integration/process-exit.spec.js
+++ test/integration/process-exit.spec.js
@@ -18,6 +18,11 @@ describe("process.exit() during test execution", function () {
           "should still run this test after process.exit(0)",
         );
 
+      expect(res.failures[0].err, "to satisfy", {
+        code: "ERR_MOCHA_PROCESS_EXIT_DURING_TEST",
+        exitCode: 0,
+      });
+
       done();
     });
   });
@@ -53,6 +58,47 @@ describe("process.exit() during test execution", function () {
         .and("to have failed test", "second test calls process.exit(0)")
         .and("to have passed test", "first test passes")
         .and("to have passed test", "third test should still run");
+
+      done();
+    });
+  });
+
+  it("should fail the test but continue when process.exit is called asynchronously", function (done) {
+    run("process-exit-async", [], function (err, res) {
+      if (err) {
+        return done(err);
+      }
+
+      expect(res, "to have failed")
+        .and("to have passed test count", 1)
+        .and("to have failed test count", 1)
+        .and(
+          "to have failed test",
+          "should fail when calling process.exit(0) in a setTimeout",
+        )
+        .and(
+          "to have passed test",
+          "should still run after async process.exit",
+        );
+
+      done();
+    });
+  });
+
+  it("should fail the hook when process.exit is called inside it", function (done) {
+    run("process-exit-hook", [], function (err, res) {
+      if (err) {
+        return done(err);
+      }
+
+      expect(res, "to have failed")
+        .and("to have passed test count", 0)
+        .and("to have failed test count", 1);
+
+      expect(res.failures[0].err, "to satisfy", {
+        code: "ERR_MOCHA_PROCESS_EXIT_DURING_TEST",
+        exitCode: 0,
+      });
 
       done();
     });
