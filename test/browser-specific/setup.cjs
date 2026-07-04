d1198bba396d1f80b5f9a9e0c8a34a9136a0c9ce
Josh Goldberg <git@joshuakgoldberg.com>
2026-07-04 12:43:02 -0400

    Revert "Oh hey unexpected-sinon is unused too"
    This reverts commit 42bf29e303ab4ab0e7ddd4612a6abae9af4b2df1.

--- test/browser-specific/setup.cjs
+++ test/browser-specific/setup.cjs
@@ -2,4 +2,4 @@
 
 process.stdout = require("browser-stdout")();
 
-global.expect = require("unexpected").clone();
+global.expect = require("unexpected").clone().use(require("unexpected-sinon"));
--- test/setup.cjs
+++ test/setup.cjs
@@ -4,4 +4,5 @@ const unexpected = require("unexpected");
 
 global.expect = unexpected
   .clone()
+  .use(require("unexpected-sinon"))
   .use(require("./assertions.cjs"));
