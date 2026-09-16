2bc4679d22ce0d6e4995119bf107ea90bf2f470d
mostafanazari702 <nazarimostafa2006@gmail.com>
2026-09-16 19:25:57 +0200

    fix: make --timeout 0 disable timeouts for the whole run

---
+++ test/integration/fixtures/options/timeout-in-file-override.fixture.js
@@ -0,0 +1,30 @@
+'use strict';
+
+describe('suite-level override', function() {
+  this.timeout(10);
+
+  it('should not time out an async test', function(done) {
+    setTimeout(done, 60);
+  });
+
+  it('should not time out a sync test', function() {
+    var start = Date.now();
+    while (Date.now() - start < 60);
+  });
+});
+
+describe('test-level override', function() {
+  it('should not time out', function(done) {
+    this.timeout(10);
+    setTimeout(done, 60);
+  });
+});
+
+describe('hook-level override', function() {
+  before(function(done) {
+    this.timeout(10);
+    setTimeout(done, 60);
+  });
+
+  it('should run once the hook is done', function() {});
+});
