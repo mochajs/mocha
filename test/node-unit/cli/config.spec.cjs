64a17bab0cc10b485207ac35ac44a54ffba7f0c8
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2026-09-16 05:30:48 -0700

    Revert "Switch to find-up-simple"
    This reverts commit f2a1ff70974a661f6088b3429d304d33ac21958a.

--- test/node-unit/cli/config.spec.cjs
+++ test/node-unit/cli/config.spec.cjs
@@ -142,12 +142,11 @@ describe("cli/config", function () {
     let CONFIG_FILES;
 
     beforeEach(function () {
-      findup = sinon.stub();
-      findup.onFirstCall().returns("/some/path/.mocharc.js");
+      findup = sinon.stub().returns("/some/path/.mocharc.js");
       const config = rewiremock.proxy(
         require.resolve("../../../lib/cli/config.cjs"),
         (r) => ({
-          "find-up-simple": r.by(() => ({ findUpSync: findup })),
+          "find-up": r.by(() => ({ findUpSync: findup })),
         }),
       );
       findConfig = config.findConfig;
@@ -157,7 +156,7 @@ describe("cli/config", function () {
     it("should look for one of the config files using findup-sync", function () {
       findConfig();
       expect(findup, "to have a call satisfying", {
-        args: [CONFIG_FILES[0], { cwd: process.cwd() }],
+        args: [CONFIG_FILES, { cwd: process.cwd() }],
         returned: "/some/path/.mocharc.js",
       });
     });
@@ -165,7 +164,7 @@ describe("cli/config", function () {
     it("should support an explicit `cwd`", function () {
       findConfig("/some/path/");
       expect(findup, "to have a call satisfying", {
-        args: [CONFIG_FILES[0], { cwd: "/some/path/" }],
+        args: [CONFIG_FILES, { cwd: "/some/path/" }],
         returned: "/some/path/.mocharc.js",
       });
     });
--- test/node-unit/cli/options.spec.js
+++ test/node-unit/cli/options.spec.js
@@ -20,7 +20,7 @@ const proxyLoadOptions = ({
   proxy(modulePath, (r) => ({
     "node:fs": r.with({ readFileSync }).directChildOnly(),
     [mocharcPath]: defaults,
-    "find-up-simple": r
+    "find-up": r
       .by(() => (findupSync ? { findUpSync: findupSync } : {}))
       .directChildOnly(),
     [configPath]: r.with({ findConfig, loadConfig }).directChildOnly(),
